9d2ef7d9c67e4605198003d37e9033899a09b1c5
Aiden Bai <mail.contraband@gmail.com>
2021-05-31 12:38:57 -0700

    fix: example and dev spin

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -14,7 +14,7 @@ index_html_content="<!DOCTYPE html>
     <!-- Your code here -->
   </body>
 </html>"
-script_ts_content="import { h, diff, element } from 'million';"
+script_ts_content="import { h, patch } from 'million';"
 style_css_content="body {
   font-size: 2em;
   display: flex;
