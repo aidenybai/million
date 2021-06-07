b9fe654f199ce6e863b28726d1f04a631997c341
Will Lane <williamlane923@gmail.com>
2021-06-07 13:14:28 -0700

    100% test coverage on createElement

--- src/__test__/createElement.spec.ts
+++ src/__test__/createElement.spec.ts
@@ -1,9 +1,20 @@
 // import { _ } from '../index';
-// import { m, style, className } from '../m';
-// import { createElement } from '../createElement';
+import { m } from '../m';
+import { createElement } from '../createElement';
 
 describe('.createElement', () => {
-  it('', () => {
-    expect(true).toEqual(true);
+  it('creates text element', () => {
+    expect(createElement('hello')).toEqual(document.createTextNode('hello'));
   });
+
+  it('creates HTMLElement from vNode', () => {
+    expect(createElement(m('div'))).toEqual(document.createElement('div') as HTMLElement);
+
+    let created = createElement(m('div', { id: 't' }, ['test element']));
+    let manual = document.createElement('div') as HTMLElement;
+    manual.id = 't';
+    manual.innerHTML = 'test element';
+
+    expect(created).toEqual(manual);
+  })
 });
