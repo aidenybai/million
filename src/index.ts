9fd276bfee8177249a3efb658141db32131307e5
Aiden Bai <mail.contraband@gmail.com>
2021-06-07 09:19:41 -0700

    iQIzBAABCAAdFiEEXIjdS586QkIAKiWg6Ds2uRFBoZEFAmC+Rx0ACgkQ6Ds2uRFB  oZHx3w/+JXu8dFDHoSPVoDjxs6wRKCdH/hu66CMUFHMkGFTSahxZVGOtsmgV3c7f  SgNO+EKkznXPG7XbuZE5/6/PP8+EbmWdu3ukjcN8h8Uo3dnUy/0ZGThGdS2H+5dw  Uga3AfMBnQWiG6h7JAjtmbtTw7GuoxvP00D72CRARl1aCdVccyAxt5Up8qQ3woF7  Y+likDubgpZc84rVx2O/o23bE5aQFRGafyn5iOzwG5zkngeiU0rpCN86Jbq+Kmn7  EMyZmlavazaEzmUBMy0M6fxV2mQkQkMS9Q1wiqy3VufcvEukkP1ErWF69RV+77JU  UFEpJR8GIybLCnznbre65VfnEJodm0x9Li4NvSQBsUVCTp5rlGh97ZBSUmBFPbYI  aZZpEMh/FNUK7QD7glr/Qv3NuYgogTvd7XLOUsefrlIDfhBjOsQOgKdgJL/GLzYD  q8edGlWLTOs5QoUGfE9XA1OM2U8fLO57kK+XvMBVTiaqYrK/uddJqA6HtyKT+nSD  xNsf4f6uUmeBs+QPqC5Ij7hjp9gb8BvYQGhtdohsBaWWBkKlsuKIhrIF5n4K8Uko  KW7xzFywPF1RMNEV5vKOUDDSD3tT8A2/Vf5NbSRaBHN8V6dH96o2OhztSv3Jr6XV  qJ4ef+2iyojowJPtsB3Pm119R29IhCWfCJxq2e9yGo4ixsHkMh0=  =Zsrj  -----END PGP SIGNATURE-----
    feat: default export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export const _ = undefined;
-export { m, style, className } from './m';
-export { patch } from './patch';
-export { createElement } from './createElement';
+import { m, style, className } from './m';
+import { patch } from './patch';
+import { createElement } from './createElement';
+const _ = undefined;
+
+export default {
+  _,
+  m,
+  style,
+  className,
+  patch,
+  createElement,
+};
