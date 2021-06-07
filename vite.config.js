4a6d34f6109712d3e9f48ba616430699ccd85e58
Aiden Bai <mail.contraband@gmail.com>
2021-06-07 11:13:39 -0700

    iQIzBAABCAAdFiEEXIjdS586QkIAKiWg6Ds2uRFBoZEFAmC+YdMACgkQ6Ds2uRFB  oZGIJQ//RTzi/5mu6BOKxXdfakr2ObKBcR+HQGFqFYInL9bphG2DmDnFYmmzo5R9  UE9bDei/aIUKKJtaLdraZhQsnLsS1dgLHwp7xMclpAWqweGI4c1yzBi+0KWvo3kN  jzmtY75M697a1E0B7TUrhAcCOTQTu3WPFqPdEpSUnQhHAFe4aq4ZaazMEb7CNbkn  aqVXp+BHFXQ1VXZOqGm4Qaol711aRJ1EDq095k0H5Wk4QV1MnoJd04U3H0S7Ri0y  5MYqNQNoXK/6tUWz2gMqLeo1Q5ft3MjByTSTX19/NVg97vVjuOFQm6I6cJx5YzAh  EHrMx4UlpMKPaaHLNeS/thuhGq+4t386IGldtIHN0KDSCcfudYnOAIUB4QS7uDzA  Ry+g6f/K0V4DFY9CdK01VuRA6EOTIA+2WP06gkiZ3QsR2YmVllSFl/bcHLiFocbC  nwbN1Wq/Hna5nsy6z4TPUXx/rkSDj9lHyj96mLL0upWosIC6Xq0ZD1zuUhzn2Wqo  zKKZjmbIAaxsXDkr1sufARPycbjwkAOOlWOHOYi5HV/fvJ7qOtCTBPa4UhZ8OE6S  R6zsyIpVtnF2rHr/4A1NRANZBLXrjl4zj9tjN12ETaisoDVr9NOJpTPAM0OB9ixI  Y5cx83rrRKocBI2+rcNkR3h5EPet7SGaRIlDW2PY8y8AVa11Hu4=  =xgWX  -----END PGP SIGNATURE-----
    chore: jsx for vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { resolve } from 'path';
-
 export default {
   root: 'dev',
-  resolve: {
-    alias: {
-      million: resolve(__dirname, 'src/index.ts'),
-    },
+  esbuild: {
+    jsxFactory: 'jsx',
   },
 };
