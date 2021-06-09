d6b913c43b347c6d7b4e0f81ce15e5a0271a935b
Aiden Bai <mail.contraband@gmail.com>
2021-06-08 17:41:46 -0700

    refactor: jsx compatible m function

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 export default {
   root: 'dev',
   esbuild: {
-    jsxInject: `
-      import { m as __million_hyperscript } from '../src/m';
-      const __jsx_factory = (tag, props = {}, ...children) => __million_hyperscript(tag, props, children);
-    `,
-    jsxFactory: '__jsx_factory',
+    jsxInject: `import { m } from '../src/m';`,
+    jsxFactory: 'm',
   },
 };
