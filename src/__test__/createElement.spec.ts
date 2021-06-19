de1653dc930b5b368c00933c1034b4c8a3c78c54
Felippe Regazio <felippe.moraes@zoho.com>
2021-06-19 02:05:36 -0300

    improvements in tests and readability of createElement fn

--- src/__test__/createElement.spec.ts
+++ src/__test__/createElement.spec.ts
@@ -3,6 +3,8 @@ import { OLD_VNODE_FIELD } from '../constants';
 import { m } from '../m';
 import { VNode, VProps } from '../structs';
 
+const createManual = (tag: string, props: unknown) => Object.assign(document.createElement(tag), props);
+
 const h = (tag: string, props?: VProps, ...children: VNode[]) =>
   m(
     tag,
@@ -19,9 +21,7 @@ describe('.createElement', () => {
     expect(createElement(h('div'))).toEqual(document.createElement('div') as HTMLElement);
 
     const created = createElement(h('div', { id: 'app' }, 'foo'));
-    const manual = document.createElement('div') as HTMLElement;
-    manual.id = 'app';
-    manual.innerHTML = 'foo';
+    const manual = createManual('div', { id: 'app', innerHTML: 'foo' }) as HTMLElement;
 
     expect(created).toEqual(manual);
   });
@@ -30,11 +30,20 @@ describe('.createElement', () => {
     expect(createElement(h('div'))).toEqual(<HTMLElement>document.createElement('div'));
 
     const created = createElement(h('div', { id: 'app' }, 'foo'));
-    const manual = <HTMLElement>document.createElement('div');
-    manual.id = 'app';
-    manual.innerHTML = 'foo';
+    const manual = <HTMLElement>createManual('div', { id: 'app', innerHTML: 'foo' });
 
     expect(created).toEqual(manual);
+  });
+
+  it('should create HTMLElement from vnode with VNode child', () => {
+    const child = h('section', { id: 'child' }, 'bar');
+    const created = createElement(h('div', { id: 'app' }, child));
+
+    const mCreated = <HTMLElement>createManual('div', { id: 'app' });
+    const mChild = <HTMLElement>createManual('section', { id: 'child', innerHTML: 'bar' });
+
+    mCreated.append(mChild);
+    expect(created).toEqual(mCreated);
   });
 
   it('should create HTMLElement from vnode', () => {
