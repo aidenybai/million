ca1df0e34723f186bc5fd423b757fbc85fcfa2a5
Aiden Bai <aiden.bai05@gmail.com>
2021-07-10 13:43:30 -0700

    fix: replace broken color codes

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -2,26 +2,21 @@
 
 export PATH="$(dirname "$0")/../node_modules/.bin:$PATH"
 
-YELLOW_COLOR="\u001b[33m"
-GREEN_COLOR="\033[0;32m"
-RED_COLOR='\033[0;31m'
-BLUE_COLOR='\033[0;34m'
-RESET_COLOR="\u001b[39m"
 SPACER='                    '
 
 function pending {
-  printf "${YELLOW_COLOR}⚠ $1${RESET_COLOR}"
+  printf "⚠ $1"
 }
 
 function info {
-  printf "${BLUE_COLOR}ⓘ $1${RESET_COLOR}\n"
+  printf "ⓘ $1\n"
 }
 
 function success {
-  printf "\r${GREEN_COLOR}✓ $1${RESET_COLOR}${SPACER}\n"
+  printf "\r✓ $1${SPACER}\n"
 }
 
 function fail {
-  printf "\r${RED_COLOR}✗ Error during $1. Run \`$2\`.${RESET_COLOR}${SPACER}\n";
+  printf "\r✗ Error during $1. Run \`$2\`.${SPACER}\n";
   exit 1;
 }
