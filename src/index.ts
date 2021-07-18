cba600b07c285f95a06a8d2baffbd2caacff8ae6
Aiden Bai <aiden.bai05@gmail.com>
2021-07-18 18:58:07 -0400

    fix(exports): VFlags export as nontype

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export type { VElement, VFlags, VNode, VProps } from './structs';
-export { OLD_VNODE_FIELD } from './structs';
+export type { VElement, VNode, VProps } from './structs';
+export { VFlags, OLD_VNODE_FIELD } from './structs';
 export { createElement } from './createElement';
 export { className, DELETE, INSERT, m, style, svg, UPDATE } from './m';
 export { patch, patchChildren, patchProps } from './patch';
