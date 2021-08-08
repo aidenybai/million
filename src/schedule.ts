393a49d3757cc123b2337d3946c67c465fd19c61
Aiden Bai <aiden.bai05@gmail.com>
2021-08-08 00:53:51 -0700

    refactor: remove rAF impl support

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,16 +1,12 @@
 import { VTask } from './structs';
 
-const queue: VTask[] = [];
+const stack: VTask[] = [];
 const DEADLINE_THRESHOLD = 1000 / 60; // Minimum time buffer for 60 FPS
 let deadline = 0;
 
-export const schedule = (callback: VTask, important = false): void => {
-  if (important) {
-    requestAnimationFrame(callback);
-  } else {
-    queue.push(callback);
-    postMessage();
-  }
+export const schedule = (callback: VTask): void => {
+  stack.push(callback);
+  postMessage();
 };
 
 const shouldYield = (): boolean =>
@@ -21,11 +17,11 @@ const shouldYield = (): boolean =>
 const flush = (): void => {
   deadline = performance.now() + DEADLINE_THRESHOLD;
   while (!shouldYield()) {
-    const task = queue.shift();
+    const task = stack.shift();
     if (task) task();
     else break;
   }
-  if (queue.length > 0) postMessage();
+  if (stack.length > 0) postMessage();
 };
 
 const { port1, port2 } = new MessageChannel();
