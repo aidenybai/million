468d92fe1950f3988772f5d60ff34f81b6b10872
Aiden Bai <aiden.bai05@gmail.com>
2021-08-20 16:09:52 -0700

    refactor(schedule): use promise-based debounce rather than MessageChannel

--- src/schedule.ts
+++ src/schedule.ts
@@ -2,29 +2,34 @@ import { VTask } from './structs';
 
 const stack: VTask[] = [];
 const DEADLINE_THRESHOLD = 1000 / 60; // Minimum time buffer for 60 FPS
+const promise = Promise.resolve();
 let deadline = 0;
+let queued = false;
 
 export const schedule = (callback: VTask): void => {
   stack.push(callback);
-  postMessage();
+  nextTick();
 };
 
-const shouldYield = (): boolean =>
+export const shouldYield = (): boolean =>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (<any>navigator)?.scheduling?.isInputPending({ includeContinuous: true }) ||
   performance.now() >= deadline;
 
-const flush = (): void => {
+export const flush = (): void => {
   deadline = performance.now() + DEADLINE_THRESHOLD;
   while (!shouldYield()) {
     const task = stack.shift();
     if (task) task();
     else break;
   }
-  if (stack.length > 0) postMessage();
+  queued = false;
+  if (stack.length > 0) nextTick();
 };
 
-const { port1, port2 } = new MessageChannel();
-port1.onmessage = flush;
-
-const postMessage = (): void => port2.postMessage(null);
+export const nextTick = (): void => {
+  if (!queued) {
+    promise.then(flush);
+    queued = true;
+  }
+};
