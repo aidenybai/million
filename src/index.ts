1d90b45bd4486e6c2bdea0bba138d024b715eefc
Aiden Bai <aiden.bai05@gmail.com>
2021-08-20 15:43:10 -0700

    refactor: export all functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { createElement } from './createElement';
 export { className, DELETE, INSERT, m, style, svg, UPDATE } from './m';
-export { patch } from './patch';
-export { schedule } from './schedule';
+export { patch, patchProps, patchChildren } from './patch';
+export { schedule, shouldYield, flush, nextTick } from './schedule';
 export { OLD_VNODE_FIELD, VFlags } from './structs';
 export type { VDelta, VElement, VNode, VProps } from './structs';
