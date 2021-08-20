4ed7561b3d585420ff35f4975e2368f6cd114266
Aiden Bai <aiden.bai05@gmail.com>
2021-08-20 16:07:48 -0700

    build(scripts): colors and labels for build scripts

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -2,21 +2,28 @@
 
 export PATH="$(dirname "$0")/../node_modules/.bin:$PATH"
 
+COLOR_RED=`tput setaf 1`
+COLOR_GREEN=`tput setaf 2`
+COLOR_YELLOW=`tput setaf 3`
+COLOR_BLUE=`tput setaf 4`
+START_STANDOUT=`tput smso`
+END_STANDOUT=`tput rmso`
+RESET=`tput sgr0`
 SPACER='                    '
 
 function pending {
-  printf "⚠ $1"
+  printf "${COLOR_YELLOW}${START_STANDOUT} PENDING ${END_STANDOUT} $1${RESET}"
 }
 
 function info {
-  printf "ⓘ $1\n"
+  printf "${COLOR_BLUE}${START_STANDOUT} INFO ${END_STANDOUT} $1${RESET}\n"
 }
 
 function success {
-  printf "\r✓ $1${SPACER}\n"
+  printf "\r${COLOR_GREEN}${START_STANDOUT} SUCCESS ${END_STANDOUT} $1${SPACER}${RESET}\n"
 }
 
 function fail {
-  printf "\r✗ Error during $1. Run \`$2\`.${SPACER}\n";
-  exit 1;
+  printf "\r${COLOR_RED}${START_STANDOUT} ERROR ${END_STANDOUT} Failed during $1. Run \`$2\`.${SPACER}${RESET}\n";
+  exit 0;
 }
