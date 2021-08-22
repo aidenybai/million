4cc26b430041e39cc69b69d602c0ee9a67ab2623
Aiden Bai <aiden.bai05@gmail.com>
2021-08-22 14:47:00 -0700

    build(vite): use defineConfig for types

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   root: 'dev',
   esbuild: {
     jsxFactory: 'jsx',
     jsxFragment: 'Fragment',
     jsxInject: `import { jsx, jsxs, Fragment } from '../src/jsx'`,
   },
-};
+});
