306e1d07eff012fbf5219dd83023553ec0a31aed
Aiden Bai <aiden.bai05@gmail.com>
2021-08-23 21:21:45 -0700

    refactor(scripts): less intrusive log messages

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,4 @@ cp dist/types/jsx-runtime.esm.d.ts dist/jsx-runtime.d.ts
 
 sed -i '' 's/export type/export/g' dist/million.d.ts
 
-info "Dist: `ls -xm -d dist/*`"
+info "\nDist: `ls -xm -d dist/*`\n"
