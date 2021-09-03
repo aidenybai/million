d4b9fd716a68f0a19ecf30ad1cf2066a95040ef8
Aiden Bai <aiden.bai05@gmail.com>
2021-09-03 16:51:18 -0700

    build: remove files inside dist

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 source $(dirname "$0")/helpers.sh
 
-rm -rf dist
+rm -rf dist/*
 rollup -c
 cp dist/types/million.esm.d.ts dist/million.d.ts
 cp dist/types/jsx-runtime.esm.d.ts dist/jsx-runtime.d.ts
