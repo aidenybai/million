5863f87fb708ad0d79df3e7bfe1c697041f857c6
Aiden Bai <aiden.bai05@gmail.com>
2021-09-08 12:03:54 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmE5CRoACgkQ03WEOIZ1  /zrWgQ//WLNCMc9D80VjYaFUuNfPQjDfy1xEwoodC9NXi7BGuhRcEZGtUc/YConf  77CYT1IjCy127ZWntjpin3ObGdrq0sSHNOduveRnGP31qpR6i5Sxwz9nRVjKQzph  V0g5gqb0fpjzjDPa1LKC+Bf9yW1pITW5CmwhCshPcgXlIQSl7haBAFKx7i6mGxT+  qfwwktKH/PGl7UbdsVciucGWrqklJdtmDk0Kg1oBSLyD+e5MzIt0JJqM/TePIgOa  V8aDxibZuLHvFg3Dn341SMLur6xkfGt7CxYdePlA9vJfnwSafTOcx3oPT/iVfPx8  PFNzJ7Htl+RxVt741dlRGRFMNAD1be4kWjB7LFwHoqK82pe5Wameod2GHpq79ZJC  rvJ2e3Ivo5C6Q6v5KcH5J7A8a8qwP/Sc+AqHyUtQCuV3W3Rs3x7TQbUH6f8ZMXzA  XPv3alFK5qywZkWMCYXjUvpzj8+Zg1uKfdYH58LzlyS8MEbUaQGh7aMXhmqAz8ir  rnR0f8Te3fiRcvqRzNMgeIBGHMe3CTugfO+MChNE3iF4ioyXLwNvqE1iaNwbGuAZ  Azd6jR/O2m1nmOCUe1IYO1lfmIWwQAYEYRp9frZgaAckxQjpATqoNK/2rRsTo7xY  U+QE/Rw4HiWFf1Dh5qOuht0XcE4itaapxKJ1HClgLm8nTrovb0g=  =73EL  -----END PGP SIGNATURE-----
    build(types): fix #118

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,7 @@ source $(dirname "$0")/helpers.sh
 
 rm -rf dist/*
 rollup -c
-cp dist/types/million.d.ts dist/million.d.ts
-cp dist/types/jsx-runtime.d.ts dist/jsx-runtime.d.ts
+cp dist/types/* dist/*
 
 sed -i '' 's/export type/export/g' dist/million.d.ts
 
