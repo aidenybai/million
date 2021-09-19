af7d4e643f496791de31f6ce1f503cc8c56b2dd9
Aiden Bai <aiden.bai05@gmail.com>
2021-09-19 12:09:06 -0700

    fix: export every exportable function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { createElement } from './createElement';
-export { childrenDriver } from './drivers/children';
-export { propsDriver } from './drivers/props';
-export { className, DELETE, INSERT, m, style, svg, UPDATE } from './m';
-export { flushWorkStack, init, patch } from './patch';
-export { flush, nextTick, schedule, shouldYield } from './schedule';
+export * from './createElement';
+export * from './drivers/children';
+export * from './drivers/props';
+export * from './m';
+export * from './patch';
+export * from './schedule';
 export { OLD_VNODE_FIELD, VFlags } from './types';
 export type { VDelta, VElement, VNode, VProps } from './types';
