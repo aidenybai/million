66925376dca25b0fc765caac4c3c7a3c4e530e45
Aiden Bai <aiden.bai05@gmail.com>
2021-09-18 17:32:22 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmFGhSAACgkQ03WEOIZ1  /zpcIA/+JW8DwkdqtcZfZPWeEvzgcig6u+GX3BgtNjIBCNni1OUq0Fjq2Yuni9TZ  Igg5l2BrZGUuaJrVPvUBqmgLDvl3p/1QOoq4Tr3uDSDoeUD61QzYKYSbyrCr6JkJ  MKkQNTlb1j6XG84AnLtktsj+4Qber6Kr7lzOY53XO8kLA9VqsH6I1t4CnlyjK7eY  pXuYK8WmbmZzhhrCQ703hMD+qPodlgyfrgAh7yzSLCOCQjsV3jxXseeEKuE8e2ZW  XpJTcB9fhMJ0cVdyTFG+6Wmv0Gj1WzCUsYeT1Rj28yF1Ulag6POC5E8EOBeHzLSV  hyg5wTheIrZKeP8WLTbq7G+5NJIBDnNV0qLAmMOpQ44pU1GuTk8CsHozIzOzitfe  oq9klaMYJo7ATZbbqr0X4Jk2iMit+QP1IXYp2fpegifJp5ig89SaiwDvnjDf3Nbh  yE+wG1cJpEnLT2xV3t/vsManPXztUhS3EPYMTV6RAJJaf5X8wSE5Wz4RnEXkq3cE  7FWomFPZ1WMW050QRVII7a0sXuSvYCLC/mGKEz1NmdwBSd3Kkv407y0K6XP95FIM  /bMormt4uAnzcZJoxmQ3p1kYWzcTzhTLKuxFlPqSa3dymv0oL4l7mBOjlVkoweYa  6Unsdfbv3LcjusQ8X1YGj6td+XpK03y0/7OXd2ZiK1+IPt4No6I=  =AVEN  -----END PGP SIGNATURE-----
    refactor(index): export flushWorkStack

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { createElement } from './createElement';
 export { childrenDriver } from './drivers/children';
 export { propsDriver } from './drivers/props';
 export { className, DELETE, INSERT, m, style, svg, UPDATE } from './m';
-export { init, patch } from './patch';
+export { flushWorkStack, init, patch } from './patch';
 export { flush, nextTick, schedule, shouldYield } from './schedule';
 export { OLD_VNODE_FIELD, VFlags } from './structs';
 export type { VDelta, VElement, VNode, VProps } from './structs';
