a4a544990efa10817325f13cc6e0128dc7dbdd48
Aiden Bai <aiden.bai05@gmail.com>
2021-10-07 19:21:55 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmFfq0MACgkQ03WEOIZ1  /zr9pg/+KLu3DPaUIzdlVQ4mCFuew8WGNbtrQVxrO21rABdo8UHnBGWvgcZJVGRO  ++i0lUyVeEmxMMZ05Ag/GNQXWBnaNi9zqjvjeaKWOmY24PW2VqCFOAyxyRvkLw3a  bjdRs6d5ldcR/B6kki3XpozlR77xH4ltprf3lJDf1ArXUmq5FWTpca72WiuDScUg  mEeyYXTbUolLsa6PEMCi6igABcQKJhoKwDnK9Ex5+7Fwqp4Q16oz9Cj/q3Hr0wrM  g/T7SCZ2/6YjDljNOMFn5dNiuAUM/0DJ53MmCPkg6+8wHNrK41cl0+GWJRFCQTfs  sM5/fA9HtIUk7OhG0loUhojqlgeR/uyKL3qgpdqJJzK27nHHjp+CdfLJ4bu+BTbz  QSnVCLdSulrnqmeHZBWl/F2vZjKi2WT+cQAmdxkzHC55sV1o2x6/CFIiw9mvxe4T  7c+9WjIIzMeIwHy5y/D2sBEzqsci7pX1FnmngrZo5iewFMj18cudw1nF5wwLBSji  gl2BhpSG2yX7mbI9o6VJ1pt7uUUpqG5YBgXaNWaSEDpuBO3aBgd0yiidBOytjgJh  slAVhNNb9QbOwsFI/oyd42cFVLlcYXQNyi9tgb5oZD9Gn36EniBvai7fAx0WAIxp  XXmF+dub2KGK2rX2Vp/w4Th2JwhaI8MBXBeg3wA+xOx3kmgm4dg=  =S+70  -----END PGP SIGNATURE-----
    build: path aliases

--- benchmarks/vite.config.ts
+++ benchmarks/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'benchmarks',
   esbuild: {
     jsxFactory: 'jsx',
     jsxFragment: 'Fragment',
-    jsxInject: `import { jsx, jsxs, Fragment } from './_jsx'`,
+    jsxInject: `import { jsx, jsxs, Fragment } from 'million/jsx'`,
+  },
+  resolve: {
+    alias: {
+      million: resolve(__dirname, '../src'),
+    },
   },
 });
