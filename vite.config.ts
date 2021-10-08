852839a006db7a7fba23b0deccc37d210dc9e382
Aiden Bai <aiden.bai05@gmail.com>
2021-10-07 21:58:04 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmFfz9wACgkQ03WEOIZ1  /zqMFhAAzYW/0iZARdkYx6m4u02FbU2dNxxa0gn1aSUdsi6tdZSoQ21dy7jqkdmI  iZvHY2c9cN4/Mb2xq0ZJTrss0JJcoMPuCEpIezjuw6QzeWRuEFa5fdhtfNtcjNlE  fPKI1V71EEKvT0wjOUuaT9SPxtSPBGKTfMK0su7iOfjSsOgDS7SAmUcN9m22iruw  Gtyo/0kUiXmYjkDJ5x0+cLffEvgNCeeskPhQqYWlAZP2avEVWhIVDIt4O0XKH5ve  6Q0kytPwaN80q/jEz34FosBJMWDnzXtCRbTlGZ0STsd4TXmpfGvT8Di79M6UeFPM  8f0x5YXVjU1FagqXhQHWrDYA2J/x1tDHagTBCnceWhAEJ3FQyu0hQvXL+lRdaWMx  lPMvnmptbFsQg64+yqdT9BnciQpfMkIMX01rV7haMM8UUNpSMsv7Gsv5BNRZzbRg  b2xAgDLOPvzcEWFlskASZ19Zh/VDXfy5rtK2u77qLobOE2DlAn96uKgZ0LQAvOUN  6CQY4aUIhwfV66PuSYjnjreGUp2g0pBboQb9pwvhhS/hQBBT+RxRq2WKK65R0VRx  NncDWZmJefFp5282b5Snl3tgbsyROmV4iy55wkXtA4x7SJeU36C76Jl3X38VcFP2  j445Uy66wYOA8wOkWoV49gMS7q6U9rxkDZHE+MXRQP3+3/zDgjw=  =5l90  -----END PGP SIGNATURE-----
    refactor(scripts): switch from shell to zx

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'dev',
   esbuild: {
     jsxFactory: 'jsx',
     jsxFragment: 'Fragment',
-    jsxInject: `import { jsx, jsxs, Fragment } from '../src/jsx'`,
+    jsxInject: `import { jsx, jsxs, Fragment } from 'million/jsx'`,
+  },
+  resolve: {
+    alias: {
+      million: resolve(__dirname, './src'),
+    },
   },
 });
