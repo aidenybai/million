62edd0bad265fc253b568972b4569581402a6cd3
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-10-09 22:03:34 -0400

    feat: add matt-esch/virtual-dom to benchmarks

--- benchmarks/suites/createManyRows.jsx
+++ benchmarks/suites/createManyRows.jsx
@@ -7,6 +7,7 @@ import { createElement, patch } from '../../src/index';
 import { Suite } from '../benchmark';
 import { buildData } from '../data';
 import * as tiny_vdom from '../tiny-vdom';
+import patch as virtual_dom from 'virtual-dom/patch';
 
 const data = buildData(10000);
 const oldVNode = <table></table>;
@@ -28,6 +29,9 @@ const suite = Suite('create many rows (creating 10,000 rows)', {
   },
   'tiny-vdom': () => {
     tiny_vdom.patch(el(), vnode, oldVNode);
+  },
+  'virtual-dom': () => {
+    virtual_dom.patch(el(), vnode, oldVNode)
   },
   DOM: () => {
     const elClone = el();
--- benchmarks/suites/swapRows.jsx
+++ benchmarks/suites/swapRows.jsx
@@ -7,6 +7,7 @@ import { createElement, patch, UPDATE } from '../../src/index';
 import { Suite } from '../benchmark';
 import { buildData } from '../data';
 import * as tiny_vdom from '../tiny-vdom';
+import patch as virtual_dom from 'virtual-dom/patch';
 
 const data = buildData(1000);
 const oldVNode = (
@@ -44,6 +45,9 @@ const suite = Suite('swap rows (swap 2 rows for table with 1,000 rows)', {
   },
   'tiny-vdom': () => {
     tiny_vdom.patch(el(), vnode, oldVNode);
+  },
+  'virtual-dom': () => {
+    virtual_dom.patch(el(), vnode, oldVNode);
   },
   DOM: () => {
     const elClone = el();
