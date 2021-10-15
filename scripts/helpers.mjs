e655ec5c83a965b75de302ccc25252c142c476cb
Aiden Bai <aiden.bai05@gmail.com>
2021-10-14 19:51:58 -0700

    feat(scripts): custom ascii icons

--- scripts/helpers.mjs
+++ scripts/helpers.mjs
@@ -4,27 +4,29 @@ const SPACER = ' '.repeat(20);
 
 export const load = (message) => {
   const loader = ora({
-    text: `${chalk.blue('info    ')}- ${message}`,
-    spinner: 'noise',
+    text: `${chalk.bold(chalk.blue('info    '))}- ${message}`,
+    spinner: 'arc',
     color: 'blue',
   });
   loader.start();
   return loader;
 };
 
-export const info = (message) => console.log(`${chalk.blue('▓ info    ')}- ${message}`);
+export const info = (message) => console.log(`${chalk.bold(chalk.blue('→ info    '))}- ${message}`);
 
 export const success = (message) =>
-  console.log(`${chalk.green('▓ success ')}- ${message}${SPACER}`);
+  console.log(`${chalk.bold(chalk.green('✔ success '))}- ${message}${SPACER}`);
 
 export const fail = async (message, fix) => {
-  console.log(`${chalk.red('▓ fail    ')}- Failed during ${message}. Run ${fix}.${SPACER}`);
+  console.log(
+    `${chalk.bold(chalk.red('✘ fail    '))}- Failed during ${message}. Run ${fix}.${SPACER}`,
+  );
   await $`exit 0`;
 };
 
 export const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
-export const say = (message) => console.log(`${chalk.red('▓')} ${message}`);
+export const say = (message) => console.log(`${chalk.bold(chalk.red('→'))} ${message}`);
 
 export const typing = (message) => {
   const loader = ora({
