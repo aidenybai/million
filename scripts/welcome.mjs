32189852dace6af9c017fb39f5b6e54f7a3f95b2
Aiden Bai <aiden.bai05@gmail.com>
2021-10-14 18:31:18 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmFo2e0ACgkQ03WEOIZ1  /zpGUA//Y3N17ftkuoVDNkKuBSkSF9K9cttbso6FH8xvKFvP9oz79SOQHjJCr9h+  yPsBj/MJV5euUGCwwputvM7w8KK1dEpEkGPssqotOsM2gkueDm8eF9MCxsren0L+  hw0ilbaQ/6uPDa7fodvUIGQ8IE6J5TcRlQiLVzXRZ06HR22QknQ9faGJ9jAFVorq  Nm/75+NmWBfSe7XbxrRRWyu/mTz6nYRaE3w8b+1whNwUB9Wig6ZsTSazbtnR9aka  0df1Wc/V3/Ips1simtV/lykBGzplhoRzRbp/90U28ncAEayqfh+cfNMrwja0B8mU  WWSjKkmUzqZrc0BNbaKRNasz+KvMvvdkCVxHimBkzgvbnmajJu4bNZCvKHlVQ6M4  +k2fzEzbNuZgy3ODoHffJhhLcoEiRnvEGdI5EaStWeWBOf7FSE9A08YaNNZN4wbm  Za/2kr4HxWAjP3FG3f+8jOlbUEV1XXqMCxXGipT3XWPws7w8M4qm2cHGZ6Iw3j8u  mQ7kAYV1WFiTKFzWIfxrfY5HM0k68vztLens3fTWIBE3ymPExN0ZYQfW0VmrABl5  ymloaKg1zbF9uaLZpQ5SYg+0q7HhAtN55mkpGjOF5uRrWobNoMBtW1wmi4qnGICo  rDxAz02xvogJEu4bc6sLgq+vY8k+MT1Ra2zYNCuwwcLyMHTL1+I=  =6URm  -----END PGP SIGNATURE-----
    feat: mil the lion

---
+++ scripts/welcome.mjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env zx
+import 'zx/globals';
+import { info, success, fail, load, sleep, say, typing } from './helpers.mjs';
+$.verbose = false;
+
+let current;
+
+const send = async (message, time) => {
+  current = typing('Mil is typing');
+  await sleep(time);
+  current.stop();
+  say(message);
+};
+
+await send(`Hi there! I'm ${chalk.red('Mil the Lion')}! Welcome to the Million codebase!`, 2000);
+await send(`Before we get started, make sure you have ${chalk.gray('`pnpm`')} installed.`, 3000);
+await send(
+  `You can install ${chalk.gray('`pnpm`')} by running ${chalk.gray('`npm i -g pnpm`')}.`,
+  3000,
+);
+const hasPnpmInstalled =
+  (await question(`${chalk.red('▓')} Do you have pnpm? ${chalk.gray('(yes/no)')} `)) === 'yes';
+if (!hasPnpmInstalled) throw new Error('Please install `pnpm`');
+await send(`Ok! Let me install the necessary packages for you.`, 3000);
+await $`pnpm i`;
+await send(
+  `Packages installed! Please read ${chalk.gray('`.github/CONTRIBUTING.md`')} && ${chalk.gray(
+    '`.github/CODE_OF_CONDUCT.md`',
+  )} to get started!`,
+  3000,
+);
+await send(`Tip: You can spin up a dev environment by running ${chalk.gray('`pnpm dev`')}!`, 3000);
+await send(`Alright! I'll see ya around!`, 3000);
