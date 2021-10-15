401de343a2a4fef864ed00668876a37a31be69a0
Aiden Bai <aiden.bai05@gmail.com>
2021-10-14 17:55:54 -0700

    feat: new loaders with ora

--- scripts/helpers.mjs
+++ scripts/helpers.mjs
@@ -1,10 +1,25 @@
+import ora from 'ora';
+
 const SPACER = ' '.repeat(20);
 
-export const info = (message) => console.log(`${chalk.blue('info    ')}- ${message}`);
+export const load = (message) => {
+  const loader = ora({
+    text: `${chalk.blue('info    ')}- ${message}`,
+    spinner: 'noise',
+    color: 'blue',
+  });
+  loader.start();
+  return loader;
+};
 
-export const success = (message) => console.log(`${chalk.green('success ')}- ${message}${SPACER}`);
+export const info = (message) => {
+  console.log(`${chalk.blue('▓ info    ')}- ${message}`);
+};
+
+export const success = (message) =>
+  console.log(`${chalk.green('▓ success ')}- ${message}${SPACER}`);
 
 export const fail = async (message, fix) => {
-  console.log(`${chalk.red('fail    ')}- Failed during ${message}. Run ${fix}.${SPACER}`);
+  console.log(`${chalk.red('▓ fail    ')}- Failed during ${message}. Run ${fix}.${SPACER}`);
   await $`exit 0`;
 };
--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,21 +1,23 @@
 #!/usr/bin/env zx
 import 'zx/globals';
-import { info, success, fail } from './helpers.mjs';
+import { info, success, fail, load } from './helpers.mjs';
 $.verbose = false;
 
-info('Checking for errors...');
+const loader1 = load('Checking for errors...');
 try {
   await $`zx scripts/check.mjs`;
 } catch (_err) {
   fail('checks', 'zx scripts/check.mjs');
 }
+loader1.stop();
 success('No errors found.');
-info('Building distribution bundles...');
+const loader2 = load('Building distribution bundles...');
 try {
-  await $`zx scripts/build.mjs`;
+  await $`pnpm zx scripts/build.mjs`;
 } catch (_err) {
   fail('build', 'pnpm build');
 }
+loader2.stop();
 success('Built distribution bundles.');
-info('Please change the version number in `CITATIONS.cff`');
-info('Run `np` to publish.');
+info(`Please change the version number in ${chalk.gray('`CITATIONS.cff`')}`);
+info(`Run ${chalk.gray('`np`')} to publish.`);
