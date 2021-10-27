cd9bd38e04f3824d518429591cd6523f1d5638aa
Aiden Bai <aiden.bai05@gmail.com>
2021-10-27 09:32:54 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmF5f0MACgkQ03WEOIZ1  /zohYw//Z0PlmSkH7DQ6X7QzhjQT/In7OuI2FgLVbIWGP1NNwOR7PyahsMYMCTjq  WFoA0b3jFtZCfYqmUhY7RVUJU0FSLW2/1WMX7uxHIYgfEQHje7uF/0E5yOCx05OG  naa3ZvezXOc/wInhW5wjkSuuVjFFzEqt5e2rvDEWivjIWDVKEuw3baTaVT1aYsrn  tIqbn3W/J2STTKB6LdHL+q7r/iVJ2JHS7N/1uWrKJTtZwDG240CK8jbQ/3BqvRDC  Ze9ZoaRN/UgM/F10bqD4gz53+5D0Vhx1I9tUt0DMEgPbye1B2Uu0Z+66QhSQYqKb  c7HaqrUhnUfmF+e6LxGAk5XP5lMqCTZ1moaiz86QYhTmN+c7sIUU6AJHbhlG165i  RkF0d9iehwS2GAJiq5KpQF2W4iLJT7Kmhj/lPxqlH8ObmhTQjdR3PYh6r5zELZQT  pKFELl2jWcd/Cog9FonoaA0c3Nk8iuPuo0Ihgfxj4KIPEMrwkAKJjqAO023k5oZZ  Sj7ppfBQOYWDp22t6IBh1UazcalH1GIa80kUhEVXX20JNSYyI6Gx+oSExtlvg+2o  yji7NF1Xu41p21uziuItF8P5lfGcwo6NPOtRnjChA5nZGr3SjL6GvFcST9Bw33Tl  zV1lMBPjJy1AnP32nVEOElUQQiw+PO7J5HHK61sgMi91XTIZ4lA=  =MrQJ  -----END PGP SIGNATURE-----
    refactor(createElement.test.ts): standardized unit tests

---
+++ src/__test__/createElement.test.ts
@@ -0,0 +1,42 @@
+import { createElement } from '../createElement';
+import { m } from '../m';
+import { OLD_VNODE_FIELD } from '../types';
+
+describe('.createElement', () => {
+  it('should create Text', () => {
+    expect(createElement('foo')).toEqual(document.createTextNode('foo'));
+  });
+
+  it('should create HTMLElement from vnode', () => {
+    expect(createElement(m('div'))).toEqual(document.createElement('div') as HTMLElement);
+
+    const el = document.createElement('div');
+    el.id = 'app';
+    el.appendChild(document.createTextNode('foo'));
+    expect(createElement(m('div', { id: 'app' }, ['foo']))).toEqual(el);
+  });
+
+  it('should create HTMLElement from vnode with VNode child', () => {
+    const el = document.createElement('div');
+    el.id = 'app';
+    const child = document.createElement('section');
+    child.innerHTML = 'bar';
+    child.id = 'foo';
+    el.appendChild(child);
+
+    expect(createElement(m('div', { id: 'app' }, [m('section', { id: 'foo' }, ['bar'])]))).toEqual(
+      el,
+    );
+  });
+
+  it('should create HTMLElement from vnode', () => {
+    expect(createElement(m('div'), true)[OLD_VNODE_FIELD]).toEqual(m('div'));
+    expect(createElement(m('div'), false)[OLD_VNODE_FIELD]).toBeUndefined();
+  });
+
+  it('should createElementNS when svg', () => {
+    const svg = <HTMLElement>createElement(m('svg', { ns: 'http://www.w3.org/2000/svg' }));
+    expect(svg.namespaceURI).toEqual('http://www.w3.org/2000/svg');
+    expect(svg instanceof SVGElement).toBeTruthy();
+  });
+});
