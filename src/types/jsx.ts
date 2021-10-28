a5b0ba8a6c940c1edb5e765b430ed1b8bafac540
Aiden Bai <aiden.bai05@gmail.com>
2021-10-27 17:40:32 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmF58ZUACgkQ03WEOIZ1  /zrHEA//etFPCa2spAFbsAJbZrbqUfFsl7oSq+/aaVIUJfJ9A28cP9fdxUOeDYdy  mnnilFjdOokZoJ/Vc3ROPoVI9vCrxBDogy5nZl2Rtv5/xJxXOSm5BrLGNssdCPQL  /fA0Qgapmn9bA83qGb2BDs7sGqfs9JXaRu3l5UgVNj8oyRyNhYpHJLhhCR1Cn81g  NWSVB7GoBgWcASo6FdDIMf2dg0Gb6/gwxBKdKu/9qbH91lmQqQnbexBfDnT6VA+h  I+s5na1bjvSJU9FLepvoKarr1jHwMdbK/Wttf9RIqczUcb4FqA+bxAqY1EZ1PWSl  5g7KacMFXpKsNd0jSP7QwkeWodKmhEvNPhSrwC4KVlZDGB0Df9n5fyVR9ouCGnxA  nF98DYGLK1JhgLHhk+qYqVbfvUW8/6JcR/nWYs/0W4G1KYNH9m1hJUSlaTW+itM6  ZcsYy3ihSQZi1Q6ijVIkqV5uhdgDRIc2ACzl1077ys/LyOCK651gXiM6Ttl0hvnG  s+8RzUQWlXBNNqBA/NdsRZvRTFpPwkgpeY5Fjm6c35oOF1o83Yk60Cmo7ayt4G1i  MxK6l7kRjkMcSqGLKZamOIYdEnM53i7LO+iw2BmSqQxNnXdHftPjENmjAMfmHNvT  BwclT9B/7aQ6U/NRc4TfptSvTnXDfUS43LbJ/dbITHp4rHU+DVI=  =ATzF  -----END PGP SIGNATURE-----
    fix(jsx): add null case

--- src/types/jsx.ts
+++ src/types/jsx.ts
@@ -1,7 +1,7 @@
 import type { VDelta, VElement, VNode, VProps } from './base';
 
 export type JSXVNode = VNode | number | boolean | undefined | null;
-export type FC = (props?: VProps, key?: string) => VElement;
+export type FC = (props?: VProps, key?: string | null) => VElement;
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace JSX {
