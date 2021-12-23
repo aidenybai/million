0eae22e83cd996157d0cee43c7efd359625beb8c
Aiden Bai <aiden.bai05@gmail.com>
2021-12-22 22:19:08 -0800

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmHEFOgACgkQ03WEOIZ1  /zqrBxAAgqI86aBUotqXVSgALp7qaOEkWy/VRW0pv1IfO/JIAlgRYrQMx/xZHruc  AmOLf99u0UqT/eysEK8ZLScTd6kFudjMxUMngTNp3fpaOmWz7Wc6SvSDD/k4yxML  92leWQY6fWfqX73T/e3ELZZ4ZgQiBQCVrA3tTCpqzTWadgamnKPAH1JFKi3s+FTl  BRI892/gVbteVN6OsRGZk3lH+TA7TA2OULd+Xbo7kEEy5b3PhT4c/22nz8x9rHG8  avBaO3F81Mfd2shTDwQBIDzMrSR2dj1gNjhMUgl8STLhhfiEfJuPT2FTIeRJyzPj  FjExWo/xO6Xgc+LKeoNs67KDJn8MpQiIRkckBWLN8/3aeIyRDDFJsnK9tV3M+whY  vfWupRcT7k3fJjFCg8WezsFx3mTonX1i3msnJPVvIFuPEesrvTNJ13XX1/Man1ur  3yIcPlEU/NQ8/YLPPDlB8W1LC4El2ePrK2jJwMnRKHU53IlDbkIERqTlfu2ElKQH  Px3+TVQoZLn11anHBir+jaROBJtn6KxOujHEcPbb6p4jkDmLKKfDUhO4BMCEYgqD  aOa7paY09QIq/ecKpTGC3Xtcm7ibBPoIty5ciGcjf8i9C7j4UF4EgKJWffmSkz7G  6BJyv+t36Oyt31GR0UJwBtrHZ7jhvgX3pex3Per3OiRkVHsil/A=  =UUXB  -----END PGP SIGNATURE-----
    build: switch from rollup to tsup

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,11 +3,10 @@ import 'zx/globals';
 import { info, success } from './helpers.mjs';
 
 await $`rm -rf dist/*`;
-await $`rollup -c`;
+await $`tsup`;
+await $`esbuild src/jsx-runtime.ts --minify --bundle --outfile=dist/code-size-measurement.js`;
 
 const files = await $`ls -xm -d dist/*`;
 
 info(files);
 success('Successfully built distribution files.');
-
-await $`node scripts/fix-jsx-runtime.mjs`;
