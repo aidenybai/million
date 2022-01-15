a3e96cc25249654b1066aa3b5465cb92e9d99a31
Aiden Bai <aiden.bai05@gmail.com>
2022-01-15 00:45:50 -0800

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmHiic0ACgkQ03WEOIZ1  /zpKBg/7B6tKWKytrK1cyFqFq1Z1aAzI8NlOA7DJCVYMWbCEmMK8noT8U0Q3ExtT  Y5rbGrNbEopA4Q9TBDzjtq0N0btsAouC7a90W1Fd64n/yJq1/zxG/YPlfVVkoPUi  oilOFNG7q00YegF4gANtCtWKf/Jz22Mk1HkrYx5/6uJjtBYRvooUj5UVtZ7mxiDy  Tvfc3CjPIRoMklHUs21xXXvzpKBtpMbR6osS3dSRvel0vL8TIWmx1mnOAkUlauPF  BjOG/wl21DFoYsdYJVcH2BczUdmxB62vOv7j5HZRUHtMFLuUx/YvQaUVEIsAtHDd  WI57n2u4Kb303QVLKJE11t35N+v704iWvr5K95NhvanU8R2YZHTlNKYNV6Ll+N5/  Fpn0PBZXkhpd2RnXIJWyeY/gCgCP7SrI/UXYvKTdMJElHijorSoIIaa6QqgaUr+j  UUmqLws9Mdg24HZLu8KLxVhZMkrTnOLu9+Qat6+UaLIHc3eQnzk5d//UKs2dDuDh  AUpoXLaIBAZMc1ef1Irf28iuT0VpTL4fSt5q7sQ04ZVdIw1Ox8XP3BGn6HqgvOHT  fFr2kaVYdAC0GvuCdetPhUaiScq8xLu3LaFTF/XTdMRtwlGPVNrnUm5FgboIrWkf  X9fH7GtQDr9KKzKSlDPKGiGTZOY1FcWayciBcGDLzx0j9pVKsvw=  =kJ1Q  -----END PGP SIGNATURE-----
    chore: organize imports

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ export * from './drivers/useNode';
 export * from './drivers/useProps';
 export * from './m';
 export * from './patch';
-export { Flags, OLD_VNODE_FIELD } from './types/base';
+export { DeltaTypes, Flags, OLD_VNODE_FIELD } from './types/base';
 export type {
+  Commit,
   DeltaOperation,
   DOMNode,
   DOMOperation,
