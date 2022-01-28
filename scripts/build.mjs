5002ed253bc60e91811854c0573d48e98f2e07d0
Aiden Bai <aiden.bai05@gmail.com>
2022-01-27 17:07:02 -0800

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmHzQb0ACgkQ03WEOIZ1  /zqbExAAqT+rCRsY+6uPW1DYn+og9zigc6zM/MRbaziBbvB5NPMhC86mMNjXGFeS  QBvRZTK3vBup/zTo16FrSg38fkq3M1EJr7ZZA0xDU/jNVq+Bho4qhPy6GWhiVaNL  l1ox9M+h0riD1qFYos22fKQMSBdLTZ9upPcFj21ufdfCPKAt/K1xLEd54lb2qBzm  FT7k3fgQm+Wh9gCfwf7DO6mP51vZS/kZrcrkHPmE8ZwdVOK5yu3Fp8Ec5IeK8VF6  N7Mcoe0LRORx0fM8dNRHfTkIfvljxMhlBZytwURBoY/OFRDCGMelgWVChgxIg9bO  hY2JChvWo5oj++Mkfam9pXaLegBGTnL615p/Fe/ijPRYSaYY4UZN2uEVbMAXMhkf  6AtEHNnoxW6p6AeFMxeqqbd5F/6cI1LFT0ZDoTAivPcoOf56qhPlQX+KHM1AazkJ  YAxcbe319WctDDhjT+tmlw46DzLEAzVL9nZNeZkxVWyeeTWFm357oDPMzq5BuhTP  atRAqQPIvlOvxTweDlmUmXYiLsvLTGo31rfvxn0+F1oYHYhOIL7QRoiKmvYtVjXB  YCov89X58J3731FQbVb9WWhTyoiXpM7ixAyfty+VgOC2Pj226uE7z6uKMmu+S3FU  kV28GfaRi5LZtZO6fYnfK/SP8asxpfpBe4CnYO/YuEnkcdBa+xY=  =5z23  -----END PGP SIGNATURE-----
    build: generate declaration files at build time

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,9 +1,13 @@
 import { $ } from 'zx';
+import { write } from 'fsxx';
 import { info, success } from './helpers.mjs';
 
 await $`rm -rf dist/*`;
 await $`unbuild`;
 await $`esbuild src/jsx-runtime.ts --minify --bundle --outfile=dist/code-size-measurement.js`;
+
+await write('./jsx-runtime.d.ts', `export * from './dist/jsx-runtime';`);
+await write('./refresh.d.ts', `export * from './dist/refresh';`);
 
 const files = await $`ls -xm -d dist/*`;
 
