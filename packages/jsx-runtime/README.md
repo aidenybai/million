e5f56d202153220394d198440329d23b6ebcccf0
Aiden Bai <aiden.bai05@gmail.com>
2022-01-30 15:01:18 -0800

    refactor: paths

---
+++ packages/jsx-runtime/README.md
@@ -0,0 +1,7 @@
+# `million/jsx-runtime`
+
+Adds JSX support for [Million](https://github.com/aidenybai/million).
+
+## License
+
+Million is [MIT-licensed](../../../LICENSE) open-source software and [research](https://github.com/aidenybai/million/blob/main/.github/RESEARCH.md) by [Aiden Bai](https://github.com/aidenybai).
--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,11 @@ export default defineConfig({
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
-    jsxInject: `import { h, jsxs, Fragment } from 'million/jsx-runtime'`,
+    jsxInject: `import { h, jsxs, Fragment } from 'packages/jsx-runtime'`,
   },
   resolve: {
     alias: {
-      '@million': resolve(__dirname, '../packages'),
-      million: resolve(__dirname, '../packages/million'),
+      packages: resolve(__dirname, '../packages'),
     },
   },
   test: {
