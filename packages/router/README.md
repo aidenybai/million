5ad16330a9e1dbf543a4fe6a6cc0d5dbabaaeda6
Aiden Bai <aiden.bai05@gmail.com>
2022-02-25 11:17:50 -0800

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmIZK2MACgkQ03WEOIZ1  /zpSgBAAjse+E2Zafq4rQqjroPFErgfbwt4gCI5v+nPc6d8fqB4gmhU+r/68Lvl1  wJgIpZcmOe0fIecaHz9GdIzaINeBvLBygC7w0PxxXkhXbs9rgRxiuSw0IETlOH+3  WhOB0VlAgHStGO0wrbbPP5/EdaLyhdhyJnVsr7Tko0zHosWOefga1J01E014Tim/  zhiVJQmdbV8S3wZoFhoP2jOUB96Tk0JGwm+HN+GwapLl61jAvig/8o6gkv3qnlvW  yESd9No7KIldWFI0AddRnfqhvM+jQ894Gp5ysWfvXZP5jq2aeGkxWDi8Wv+tBp21  Ljuphgv3qc3WmxjFTDcmJMRIHLsTTJ2AqtFGYaTTNOFDRXsiOjNB0+VZnjbhvrXA  QBVjjOr2vIj9sTqgkck8XQSvIP2hmG281CWeIGVEd6eahQK9trNS+MPRfVPl5ACT  o3GgJJDlHWF9hqW9mSIW/zNcGUZwySUs7aithLsJWGvBfXhAqhPRfSJL/pZkANQA  G+eELcCAqKenoUBpXBKKcg1o9JYkDLuRk6sHhJwKCoKjGItIkJNnEqyoRA1rVhvQ  vmTYMY9V8AX4NAEValW9VLWYzJEYMfYnPX8MaJt/J8gWmmL3CL32dnKMEQAjUM5Z  UgIjsi/Uad6Q/iyoK/ybqds6tKr7Q1u0MvBw2NAbJ6Z0QlX4fQ4=  =TIUQ  -----END PGP SIGNATURE-----
    feat(router): add ability for spa to route

---
+++ packages/router/README.md
@@ -0,0 +1,7 @@
+# `million/router`
+
+Adds a SPA router for [Million](https://github.com/aidenybai/million), adapted from [`micromorph/spa`](https://github.com/natemoo-re/micromorph/blob/main/src/spa.ts).
+
+## License
+
+Million is [MIT-licensed](../../../LICENSE) open-source software and [research](https://arxiv.org/abs/2202.08409) by [Aiden Bai](https://aidenybai.com).
---
+++ packages/router/utils.ts
@@ -0,0 +1,34 @@
+export const setAttribute = (el: Element, attr: string, base: string | URL) => {
+  el.setAttribute(attr, new URL(el.getAttribute(attr)!, base).pathname);
+};
+
+export const normalizeRelativeURLs = (el: Element | Document, base: string | URL) => {
+  const hrefs = el.querySelectorAll(':is([href^="./"], [href^="../"])');
+  const srcs = el.querySelectorAll(':is([src^="./"], [src^="../"])');
+  for (let i = 0; i < hrefs.length; i++) {
+    setAttribute(hrefs[i], 'href', base);
+  }
+  for (let i = 0; i < srcs.length; i++) {
+    setAttribute(srcs[i], 'src', base);
+  }
+};
+
+export const isLocalURL = (href: string) => {
+  try {
+    const url = new URL(href);
+    if (window.location.origin === url.origin) {
+      if (url.pathname === window.location.pathname) {
+        return !url.hash;
+      }
+      return true;
+    }
+    // eslint-disable-next-line no-empty
+  } catch (e) {}
+  return false;
+};
+
+export const getURL = ({ target }: Event): URL | undefined => {
+  const a = (<HTMLElement>target).closest('a');
+  if (!a || !isLocalURL(a.href)) return undefined;
+  else return new URL(a.href);
+};
