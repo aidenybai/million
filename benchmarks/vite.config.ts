10db1779f33aae9723eb1d5692583b5f4a319d9a
Aiden Bai <aiden.bai05@gmail.com>
2022-03-31 11:26:45 -0700

    feat(bench): add plugin for vnodes

--- benchmarks/vite.config.ts
+++ benchmarks/vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import legacy from '@vitejs/plugin-legacy';
+import million from '../packages/vite-plugin-million';
 
 export default defineConfig({
   root: 'benchmarks',
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-    jsxInject: `import { h, jsxs, Fragment } from 'packages/jsx-runtime'`,
-  },
   resolve: {
     alias: {
       packages: resolve(__dirname, '../packages'),
     },
   },
   plugins: [
+    million({ importSource: 'packages/jsx-runtime' }),
     legacy({
       targets: ['> 0.25%', 'last 2 versions', 'Firefox ESR', 'not dead'],
     }),
