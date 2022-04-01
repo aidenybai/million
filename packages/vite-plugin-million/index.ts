5e703e80848ca13e635608a449555b7e4615f8b4
Aiden Bai <aiden.bai05@gmail.com>
2022-04-01 11:12:36 -0700

    fix: types not being exported correctly

--- packages/vite-plugin-million/index.ts
+++ packages/vite-plugin-million/index.ts
@@ -1,5 +1,4 @@
-import { plugin } from './plugin';
+export * from './plugin';
 
 export const seed = Math.floor(Math.random() * 1e5);
 export const jsxFactory = `__MILLION_JSX_${seed}`;
-export default plugin;
