cc94d7a6cca437dcbb0647d8aa5ab3a783977b49
Aiden Bai <aiden.bai05@gmail.com>
2022-04-09 15:51:31 -0700

    refactor(scheduler): separate to individual file

---
+++ packages/million/schedule.ts
@@ -0,0 +1,26 @@
+const workQueue: (() => void)[] = [];
+let isFlushing = false;
+
+export const schedule = (work: () => void): void => {
+  workQueue.push(work);
+  if (!isFlushing) requestIdleCallback(flushQueue);
+};
+
+export const flushQueue = (
+  deadline: IdleDeadline = {
+    didTimeout: false,
+    timeRemaining: () => Number.MAX_VALUE,
+  },
+): void => {
+  isFlushing = true;
+  while (
+    !(<any>navigator)?.scheduling?.isInputPending({ includeContinuous: true }) &&
+    deadline.timeRemaining() > 0 &&
+    workQueue.length > 0
+  ) {
+    const work = workQueue.shift();
+    if (work) work();
+  }
+  isFlushing = false;
+  if (workQueue.length > 0) requestIdleCallback(flushQueue);
+};
