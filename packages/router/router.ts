9014a2bc63a059231b642dde764d37ba44e4fc54
Aiden Bai <aiden.bai05@gmail.com>
2022-04-08 19:27:21 -0700

    refactor(router): vnode based routing

--- packages/router/router.ts
+++ packages/router/router.ts
@@ -1,37 +1,43 @@
-import { normalizeRelativeURLs } from './utils';
+import { patch } from '../million/render';
+import { VElement } from '../million/types';
 import { morph } from '../morph/morph';
-import { getURL } from './utils';
+import { getURL, normalizeRelativeURLs } from './utils';
 
 const parser: DOMParser = new DOMParser();
 
-export const navigate = async (url: URL, isBack = false, htmlString?: string) => {
+export const navigate = async (url: URL, isBack = false, vnode?: VElement) => {
   if (!isBack) {
     history.pushState({}, '', url);
     window.scrollTo({ top: 0 });
   }
-  const newPageHtmlString =
-    htmlString ??
-    (await fetch(String(url))
+
+  if (vnode) {
+    const [head, body] = vnode.children ?? [];
+    if (head) patch(document.head, head);
+    if (body) patch(document.body, body);
+  } else {
+    const newPageHtmlString = await fetch(String(url))
       .then((res) => res.text())
       .catch(() => {
         window.location.assign(url);
-      }));
+      });
 
-  if (!newPageHtmlString) return;
+    if (!newPageHtmlString) return;
 
-  const html = parser.parseFromString(newPageHtmlString, 'text/html');
-  normalizeRelativeURLs(html, url);
+    const html = parser.parseFromString(newPageHtmlString, 'text/html');
+    normalizeRelativeURLs(html, url);
 
-  const title = html.querySelector('title');
-  if (title) {
-    document.title = title.text;
+    const title = html.querySelector('title');
+    if (title) {
+      document.title = title.text;
+    }
+
+    morph(html.head, document.head);
+    morph(html.body, document.body);
   }
-
-  morph(html.head, document.head);
-  morph(html.body, document.body);
 };
 
-export const router = (routes?: Record<string, string>) => {
+export const router = (routes?: Record<string, VElement>) => {
   if (typeof window !== 'undefined') {
     const routesMap = new Map();
     if (routes) {
