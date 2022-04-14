abf67c865a1366121379cceff4dadf83a61b340e
Aiden Bai <aiden.bai05@gmail.com>
2022-04-14 13:24:55 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmJYgyAACgkQ03WEOIZ1  /zrpShAAwF4mU+aLRW9TjZxeBryQvVhoQyQ3L1Mrh7MMFGKe/xS/RNpJ4vit1V5I  18UhAYbQDWmMlAaQnOTzmizEnYU08/yrE7JMgch0fUB8jjE3un5LwFE62FPc6Oml  iJScBGbnnYynU6Wxci+yZnuZmT3yK4SKSjD/de+k7371ywamA+y04PsCxMiUsHQE  3fRfMHiRZPnr3TZ9sIrQ4qW83gR+VRGXRBUXZSeGNUyeR3nWXlrOE9Udgt6defrM  BAzy41eMSEkdr2ncovS6iLtO5hQKWjNB6J/sPXhRJ76J0y4vT5T9a4Pwb1WLM5Jk  Dz5jU0TQPKmFNCn3XG9OOhgd2Tzq8fEnQpCU/y+GY6nl4Sz1cyDCKNZTV5SLxcpJ  jro6nyfV030gxtkDExaOUirpRqf7DIkn60ZO2CFaC2meQzRAtPThy5vcPhMmurat  PTmMAytP0fZRHv1l+MIXBVA/7WtXQO32UdHABiCaFuZK4WGWUyY5HFUgY5TqR8Ut  gP8SfY8kGqUMxuqOIhxCyJMky8znlTSBtQVVUCXTQOF4Kz+ik0woR+gAUohcagCZ  gLEWtQIAXyrtAWAF7SkkwtFLQIGEqUbw0E6afX2SxcnUiCEdrPmdi/I3wJLLWGpa  rhRBUOkkYrBO9QuWsC2Ye/sIDW+C6Cvvj+YN7w3PBX7lbR+qiz0=  =gxl0  -----END PGP SIGNATURE-----
    refactor(router): reduce unnecesary complexity

--- packages/router/router.ts
+++ packages/router/router.ts
@@ -1,13 +1,20 @@
+import { m } from '../million/m';
 import { patch } from '../million/render';
 import { VElement } from '../million/types';
-import { m } from '../million/m';
 import { morph } from '../morph/morph';
 import { fromDomNodeToVNode } from '../shared/convert';
 import { getURL, normalizeRelativeURLs } from './utils';
 
-const parser: DOMParser = new DOMParser();
+const parser = new DOMParser();
+const routesMap = new Map<string, VElement>();
 
-export const fetchPage = async (url: URL): Promise<string | void> => {
+export const parsePageContent = (content: string, url: URL): Document => {
+  const html = parser.parseFromString(content, 'text/html');
+  normalizeRelativeURLs(html, url);
+  return html;
+};
+
+export const getPageContent = async (url: URL): Promise<string | void> => {
   return fetch(String(url))
     .then((res) => res.text())
     .catch(() => {
@@ -15,26 +22,24 @@ export const fetchPage = async (url: URL): Promise<string | void> => {
     });
 };
 
-export const navigate = async (url: URL, isBack = false, vnode?: VElement) => {
-  if (!isBack) {
+export const navigate = async (url: URL, goBack = false): Promise<void> => {
+  if (!goBack) {
     history.pushState({}, '', url);
     window.scrollTo({ top: 0 });
   }
 
-  if (vnode) {
-    const [head, body] = vnode.children ?? [];
+  if (routesMap.has(url.pathname)) {
+    const vnode = routesMap.get(url.pathname)!;
+    if (!vnode.children) return;
+    const [head, body] = vnode.children;
+
     if (head) patch(document.head, head);
     if (body) patch(document.body, body);
   } else {
-    const newPageHtmlString = await fetchPage(url);
+    const content = await getPageContent(url);
+    if (!content) return;
 
-    if (!newPageHtmlString) return;
-
-    const html = parser.parseFromString(newPageHtmlString, 'text/html');
-    normalizeRelativeURLs(html, url);
-
-    const title = html.head.querySelector('title');
-    if (title) document.title = title.text;
+    const html = parsePageContent(content, url);
 
     morph(html.head, document.head);
     morph(html.body, document.body);
@@ -43,11 +48,8 @@ export const navigate = async (url: URL, isBack = false, vnode?: VElement) => {
 
 export const router = (routes?: Record<string, VElement>) => {
   if (typeof window !== 'undefined') {
-    const routesMap = new Map();
-    if (routes) {
-      for (const route in routes) {
-        routesMap.set(route, routes[route]);
-      }
+    for (const route in routes) {
+      routesMap.set(route, routes[route]);
     }
 
     window.addEventListener('click', (event) => {
@@ -55,8 +57,8 @@ export const router = (routes?: Record<string, VElement>) => {
       if (!url) return;
       event.preventDefault();
       try {
-        navigate(url, false, routesMap.get(url.pathname));
-      } catch (e) {
+        navigate(url);
+      } catch (_err) {
         window.location.assign(url);
       }
     });
@@ -66,9 +68,9 @@ export const router = (routes?: Record<string, VElement>) => {
       if (!url) return;
       event.preventDefault();
       if (routesMap.has(url.pathname)) return;
-      const newPageHtmlString = await fetchPage(url);
-      if (newPageHtmlString) {
-        const html = parser.parseFromString(newPageHtmlString, 'text/html');
+      const content = await getPageContent(url);
+      if (content) {
+        const html = parsePageContent(content, url);
         const vnode = m('html', undefined, [
           fromDomNodeToVNode(html.head)!,
           fromDomNodeToVNode(html.body)!,
@@ -81,7 +83,7 @@ export const router = (routes?: Record<string, VElement>) => {
       if (window.location.hash) return;
       const url = new URL(window.location.toString());
       try {
-        navigate(url, true, routesMap.get(url.pathname));
+        navigate(url, true);
       } catch (e) {
         window.location.reload();
       }
