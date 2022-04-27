36c24c2c23563890d024010b44effbeaebcae986
Aiden Bai <aiden.bai05@gmail.com>
2022-04-26 17:06:17 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmJoiQMACgkQ03WEOIZ1  /zqqeA//YWOfoSY5XxvZa48naXlhnuDo3/ntkfsI1iqpcwir3A9DTm+MTYan3O9z  Vr6QTZtQRJExVOKxCIxOtMY+CuZxBEmmwV/lrMOOPuS0nwj4bn2Xz2bydE7jK5AB  BUXmi8snUOzl0I/t1QZpaMFQiManEPlOf73T+hyBN114D0DVWb6qOdx52xJBxIU/  WjqrPiruTXZtjhAz/jLBq8HiDnGZtFKWaxsVCFPMVJChUukKUv8AM4jkIFO+rOnp  XKePWsl3aDOubNPNpmpTjlHdef9p6iaPf9diJg/nVwVA7y48VFJ687i9b7ee6Xtw  c7OEwHQ/GqfdR3DlP8TeaTT5iRy8BQib7UtgsKYS5cA/q28vt4dC7/erEhKB3JK6  /cJOxknuPkJ+wP0lE0n9IKb7g5wp9zrnVQx38joaOwCu6pTXo5dLV7T/kw/7XwlM  +3AYnC+rfKPG11bgBc7SkzaSl2YgFoM+pfPlHeu66/piX2kFThY867KFkLAWyAOk  CUjwSDjtH4/CZ3MOOdg3qdXR/Bk96JJMoH3R4tduwBfkcZYvTFXiRN3t9dyB0Hw8  U75KQ97oImgpewMvYwafAd+FmwSLlTylnbzN/B8o0kpFFWphO6X3zWtz2DlZztyB  0ZqDoIlnKY4qW83OwqWH0tZlx77JLtreOGiti6XS66GoYTLWim8=  =NzIR  -----END PGP SIGNATURE-----
    feat(router): add listener type

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -1,8 +1,10 @@
 import { VElement } from '../million/types';
 
+export type Listener = (data: { url: URL; opts?: RequestInit; goBack: boolean }) => void;
+
 export interface Controller {
-  on: (path: string, listener: () => any) => Controller;
-  off: (path: string, listener: () => any) => Controller;
+  on: (path: string, listener: Listener) => Controller;
+  off: (path: string, listener: Listener) => Controller;
   add: (path: string, vnode: VElement) => Controller;
   remove: (path: string) => Controller;
 }
