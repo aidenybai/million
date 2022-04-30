25f6f8679180d962e0e4c1d4dfac0a230b5cb187
Aiden Bai <aiden.bai05@gmail.com>
2022-04-29 21:48:11 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmJsv5YACgkQ03WEOIZ1  /zpQUxAAg1LrcA7rKS/rodh08+17cRWNwYcI0AG6AdRnVK0F9sU60ykdyxEUM/1w  yjmFQsjdAAEmIZwf8gfKpa8Xwh+HnO4wH4epSwsOKgebGxFnmT2jq9kyteTa91s6  RnXdWWtBqiWEFnbMTy0Ini84rIL1kTCYoh3nO+5OHxveCn3BrvagFg+x9KEGWxj4  CM4jA80HEoQ+D1GOcsLFDuRqo9fcocfHlJ2oGHeN87++L6g0qp76T09/MphDqxx/  0cZhHf8ErfZO9GT4qrxOKUfj8arKmxd1SQ0/HRlA8blLA/1DrJsqyiX477DCZS6f  RqszDtC0/jq8tNUI6mTGIZTQCK+tfL01UDpNgMSqtHT5f1vH1Q1RNLiw3p+kUpV0  zAeWSVCWE8IDFZeoObQN8/oOz3NLfziLpIei5ZZcq9cJPOXWSo/iXvF7rtz3zgKe  Ko1LULberd93VbCMzJq9djLfWtAvgboCFl6TVzS03F0c3SC9O/OCjoWiamsYr+sq  YYTN6YvVJmcXHfbfgn5ROPa31fY96p/yveVN29Z35AY0MIanrE25BOvFBOlXlgZI  FUeya/JKOLGwRWRfXfCHL5ahrQc/9ZXurTihT3esh43nVVVdybN4ZzffjkuJ8aY3  98zODOX9dpQuAStq5R8EvuIuetx5L20cym81yarnjd8gCnhD3MM=  =hOPQ  -----END PGP SIGNATURE-----
    feat(ssr): add void element support to renderToString

--- packages/ssr/renderToString.ts
+++ packages/ssr/renderToString.ts
@@ -13,9 +13,40 @@ export const renderToString = (vnode: VNode): string => {
       attributes += ` ${prop}="${vnode.props[prop]}"`;
     }
   }
+
+  if (htmlVoidElements.includes(vnode.tag)) {
+    return `<${vnode.tag}${attributes} />`;
+  }
+
   for (const child of vnode.children || []) {
     children += renderToString(child);
   }
 
   return `<${vnode.tag}${attributes}>${children}</${vnode.tag}>`;
 };
+
+export const htmlVoidElements = [
+  'area',
+  'base',
+  'basefont',
+  'bgsound',
+  'br',
+  'col',
+  'command',
+  'embed',
+  'frame',
+  'hr',
+  'image',
+  'img',
+  'input',
+  'isindex',
+  'keygen',
+  'link',
+  'menuitem',
+  'meta',
+  'nextid',
+  'param',
+  'source',
+  'track',
+  'wbr',
+];
