b5eaa7ae976f40aa9e2ba6e46c1e2af90e4a57c6
Aiden Bai <aiden.bai05@gmail.com>
2022-05-31 11:07:02 -0700

    feat: initial support for react

--- src/react/index.ts
+++ src/react/index.ts
@@ -3,13 +3,17 @@ import { VNode } from '../million/types';
 import { augmentor } from './hooks';
 
 export * from './hooks';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const compat = (fn: Function) => augmentor(fn)();
+
 export const createRoot = (root: HTMLElement) => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const renderer = (fn: Function) => {
+    return (vnode: VNode) => fn(root, vnode);
+  };
   return {
-    render(vnode: VNode) {
-      augmentor(() => render(root, vnode))();
-    },
-    hydrate(vnode: VNode) {
-      augmentor(() => hydrate(root, vnode))();
-    },
+    render: renderer(render),
+    hydrate: renderer(hydrate),
   };
 };
