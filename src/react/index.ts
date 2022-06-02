c30f823501aa55b1af5cc204a9c641fb741428da
Aiden Bai <aiden.bai05@gmail.com>
2022-06-02 13:51:05 -0700

    feat(react): add default export

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,3 +1,11 @@
+import * as React from './react';
+import * as ReactDOM from './react-dom';
+
 export * from './react';
 export * from './react-dom';
 export const version = '18.1.0';
+export default {
+  version,
+  ...React,
+  ...ReactDOM,
+};
