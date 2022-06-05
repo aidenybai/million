add554515d8344ce0be9a94d9444f1f1f6df9fbf
Aiden Bai <aiden.bai05@gmail.com>
2022-06-05 14:38:02 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmKdIkQACgkQ03WEOIZ1  /zpibw//T7v4YyCjZfT3kIpSP5O0eUvFKVqcbe3g6tAEsXLrtiAy6BmAdMgA+jHT  lFcW6Q6MzkCtkfO8IFRFb04LmZYw8x8/+zPyq8rQUOLlKZLFdzK753XFIY98mpAY  aaRMJHxUTjR/zlZMcpYHB3hRTlUInmL1E1J8udcVmtWrEWzAFegnnYk+BOayj5MO  P32MptFoZVcSP5PXOHXSBzbbEq0fE2nvlFIoA3+P/0yL69Yuta9sVrSWOJFPuiW+  rBwX6ED9Y/e5oW1cg+OuiMAXWlQRXtnP5hHQE77yiUB8v4gWmS0lCoMuiSfI/99S  DM/NkOGIVo7lszMfvERIsfrx72/vMKOg1sDDfWKNCyEMW48yowzqvWguQaze2qKO  TxvPI9W2BUNvDhShcKoW6KjdyBuaGvQHLZkq1JaPZBfIoexohzEWH5dy0KBnfQd/  gKVBFUGxs+ke1U/wiREDQoGEPsw5fyzik6b7iauVwfyL32Wng2DBSfRYNahx9ypV  +POZzXbkVCEA+YtKEh8G/dkJdeURwCQYF8OAKYqFPmhyUjX4NDPr1K5+YJc1AVk7  eaIBxsPIYXQHuXoHDNLCz0QAm8tfa0VYuXdUMYMa3/eDg1lg50lWVtrerG/xOxc2  /A4LPW2FNJVh7YeJmdJu9hElykcF8iIBir6IW7SrprqLJ4HvE1I=  =CWE+  -----END PGP SIGNATURE-----
    fix(react): runtime-based jsxFactory

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,11 +1,14 @@
 import * as React from './react';
 import * as ReactDOM from './react-dom';
+import * as ReactCompat from './compat';
 
 export * from './react';
 export * from './react-dom';
+export * from './compat';
 export const version = '18.1.0';
 export default {
   version,
   ...React,
   ...ReactDOM,
+  ...ReactCompat,
 };
