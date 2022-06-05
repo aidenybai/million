18f8c232ec466e6f3f032e7c00d3d7964a00b6af
Aiden Bai <aiden.bai05@gmail.com>
2022-06-05 14:41:23 -0700

    chore: format

--- src/vite-plugin-million/dedent.ts
+++ src/vite-plugin-million/dedent.ts
@@ -4,21 +4,18 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 
-export default function dedent(
-  strings: TemplateStringsArray,
-  ...values: Array<string>
-) {
+export default function dedent(strings: TemplateStringsArray, ...values: Array<string>) {
   // $FlowFixMe: Flow doesn't undestand .raw
-  const raw = typeof strings === "string" ? [strings] : strings.raw;
+  const raw = typeof strings === 'string' ? [strings] : strings.raw;
 
   // first, perform interpolation
-  let result = "";
+  let result = '';
   for (let i = 0; i < raw.length; i++) {
     result += raw[i]
       // join lines when there is a suppressed newline
-      .replace(/\\\n[ \t]*/g, "")
+      .replace(/\\\n[ \t]*/g, '')
       // handle escaped backticks
-      .replace(/\\`/g, "`");
+      .replace(/\\`/g, '`');
 
     if (i < values.length) {
       result += values[i];
@@ -26,9 +23,9 @@ export default function dedent(
   }
 
   // now strip indentation
-  const lines = result.split("\n");
+  const lines = result.split('\n');
   let mindent: number | null = null;
-  lines.forEach(l => {
+  lines.forEach((l) => {
     const m = l.match(/^(\s+)\S+/);
     if (m) {
       const indent = m[1].length;
@@ -43,12 +40,14 @@ export default function dedent(
 
   if (mindent !== null) {
     const m = mindent; // appease Flow
-    result = lines.map(l => l[0] === " " ? l.slice(m) : l).join("\n");
+    result = lines.map((l) => (l[0] === ' ' ? l.slice(m) : l)).join('\n');
   }
 
-  return result
-    // dedent eats leading and trailing whitespace too
-    .trim()
-    // handle escaped newlines at the end to ensure they don't get stripped too
-    .replace(/\\n/g, "\n");
+  return (
+    result
+      // dedent eats leading and trailing whitespace too
+      .trim()
+      // handle escaped newlines at the end to ensure they don't get stripped too
+      .replace(/\\n/g, '\n')
+  );
 }
