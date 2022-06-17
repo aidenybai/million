022edb9120705f254122314eeea69afb8a6e2d5b
Aiden Bai <aiden.bai05@gmail.com>
2022-06-17 11:47:12 -0700

    refactor(morph): allow memo of dom elements

--- src/morph/memo.ts
+++ src/morph/memo.ts
@@ -3,11 +3,11 @@ import { fromDomNodeToVNode, fromStringToDomNode } from '../utils';
 
 const cache = new Map<DOMNode | string, VNode>();
 
-export const memo = (key: string): VNode | undefined => {
+export const memo = (key: DOMNode | string): VNode | undefined => {
   if (cache.has(key)) {
     return cache.get(key);
   } else {
-    const vnode = fromDomNodeToVNode(fromStringToDomNode(key));
+    const vnode = fromDomNodeToVNode(typeof key === 'string' ? fromStringToDomNode(key) : key);
     cache.set(key, vnode!);
     return vnode;
   }
--- src/morph/morph.ts
+++ src/morph/morph.ts
@@ -1,7 +1,6 @@
 import { patch } from '../million/render';
 import { DOMNode, Hook } from '../million/types';
 import { memo } from './memo';
-import { fromDomNodeToVNode } from '../utils';
 
 /**
  * Patches two DOM nodes and modifies the DOM node based on the necessary changes
@@ -10,10 +9,6 @@ export const morph = (
   newDOMNode: DOMNode | string,
   oldDOMNode: DOMNode,
   hook: Hook = () => true,
-): DOMNode =>
-  patch(
-    oldDOMNode,
-    typeof newDOMNode === 'string' ? memo(newDOMNode) : fromDomNodeToVNode(newDOMNode),
-    fromDomNodeToVNode(oldDOMNode),
-    hook,
-  );
+): DOMNode => {
+  return patch(oldDOMNode, memo(newDOMNode), memo(oldDOMNode), hook);
+};
