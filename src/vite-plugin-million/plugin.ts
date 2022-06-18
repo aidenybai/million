72bbaf563f2016dbab128aca0ea14cb8a21094bc
Aiden Bai <aiden.bai05@gmail.com>
2022-06-17 18:29:12 -0700

    feat(vite-plugin): error handling

--- src/vite-plugin-million/plugin.ts
+++ src/vite-plugin-million/plugin.ts
@@ -2,7 +2,11 @@ import { parse, print, visit } from 'recast';
 import { compile } from './compile';
 import { jsxCompat, jsxFactory, jsxFragment, jsxFactoryRaw, JSX_FILTER } from './constants';
 
-export const million = (options?: { importSource?: string; react?: boolean }): any[] => [
+export const million = (options?: {
+  importSource?: string;
+  react?: boolean;
+  skipOptimize?: boolean;
+}): any[] => [
   {
     name: 'vite:million-jsx',
     enforce: 'pre',
@@ -48,27 +52,32 @@ export const million = (options?: { importSource?: string; react?: boolean }): a
     async transform(code: string, id: string) {
       if (id.includes('node_modules') || !JSX_FILTER.test(id)) return;
 
-      if (code.includes(`${jsxFactory}(`)) {
-        const ast = parse(code);
-        const astNodes: any[] = [];
+      try {
+        if (code.includes(`${jsxFactory}(`) && !options?.skipOptimize) {
+          const ast = parse(code);
+          const astNodes: any[] = [];
 
-        visit(ast, {
-          visitCallExpression(path) {
-            if (path.value.callee.name === jsxFactory) {
-              astNodes.push(path);
-            }
-            this.traverse(path);
-          },
-        });
+          visit(ast, {
+            visitCallExpression(path) {
+              if (path.value.callee.name === jsxFactory) {
+                astNodes.push(path);
+              }
+              this.traverse(path);
+            },
+          });
 
-        for (let i = 0; i < astNodes.length; i++) {
-          astNodes[i].replace(compile(astNodes[i].value));
+          for (let i = 0; i < astNodes.length; i++) {
+            astNodes[i].replace(compile(astNodes[i].value));
+          }
+
+          const result = print(ast);
+
+          return { code: result.code, map: result.map };
+        } else {
+          return { code };
         }
-
-        const result = print(ast);
-
-        return { code: result.code, map: result.map };
-      } else {
+      } catch (_err) {
+        console.error(_err);
         return { code };
       }
     },
