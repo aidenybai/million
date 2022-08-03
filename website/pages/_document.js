7c34924df753a7f88274361b96419de8481e065a
38elements <38elements@users.noreply.github.com>
2022-08-04 01:38:04 +0900

    chore: summary_large_image -> summary

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -22,7 +22,7 @@ class MyDocument extends Document {
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:card" content="summary" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
           <meta name="twitter:image" content={meta.image} />
