8cb1d8a5e823857005bc118c00683a49d6b60dea
Aiden Bai <aiden.bai05@gmail.com>
2023-04-09 18:45:43 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmQzakcACgkQ03WEOIZ1  /zqHGw/7Bew6NvzKyW3MsCzlX2/N2Hq3AdCMzs8Kl7Zn4A3GCfI18T+VMwbXCMZJ  wHtUnxaISF8pahAjmPoaWHvDz7xlQdTmaaH8hNcbYSUT8ZlwC4K0rdHvxfY+e5xN  EGprZtUe6EJxB79dHS1xtS1xbPwECBMqFK349T+rZkOgJ1+xxvEZgLHrlNsH9vms  DZOfS7heT9il2vqcEaPqbt7Nmyu0uvAos40Ro4LOXjDpOx9fvXY9reQmay2ebQfm  QSCbjO9b/aMWzKpnldyddTEUQczD0BTOIaQ2Pe9I0rKPpvSq/8COgGG3ZcAAQ0rj  yreF5ncdqFLxkNXlyJQ3vmtLFeU9MLoodrR6DAZr01SpZXTNSmAv8qo4U08ihC3b  L661U2b4eBiB3zYR3eD3e4H6EV45ayqJ27LR7N2b27hemkN7HPCQCXzrNCwvpDaG  pDUq8MAhZxnaZCZzyU+QedPthVK8SxUb5ai+cohzViEpu7uMe0CjGwg6+wJlyyl6  DjmYgahMWH3pPMMmuN2dhYIx4k9qP+HULZXnj2qazR9DXdfcJbUeLFToI93JfYfR  w9aqCFkSM3tGwDhJiiyeu8zrsvjoHkIuOFes1RY/WPozdHRpn5NTxeq+Qq7ECIXy  g2Xu3yeSvW4/2yXrk3nZozEkdn7jZYJgejmVJZgCfAl665gThUA=  =T3M3  -----END PGP SIGNATURE-----
    fix: docsRepositoryBase

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -103,7 +103,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.gg/X9yFbcV2rF',
   },
-  docsRepositoryBase: 'https://github.com/aidenybai/million/website',
+  docsRepositoryBase: 'https://github.com/aidenybai/million/tree/main/website/',
   footer: {
     text: (
       <p>
