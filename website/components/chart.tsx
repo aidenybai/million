e905540e8ea364a1ac0f4bcadb900afebd79ee32
Aiden Bai <aiden.bai05@gmail.com>
2023-04-10 13:24:32 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmQ0cIAACgkQ03WEOIZ1  /zqsaw//UGbLdXKT8it2H46a7AR063zqD+0PqovVCwgWviJdR24IsRnEpGL7XWhy  s+vl4RXgim/F7DA//l6Jr9LMb6ibpPthKEElYa45AxOIyRAhOHTW23EoZIpqHMJf  jHsH1k/G5MwMXWh6d3amKZhOgTJ5G6diNMIBpkNpDvBga9AwUcS92zzCdzavjSwz  bO7BNnCB9Yf5k6LEll8kRgFpDLU1BOo30ajG3xx3HX+tu5WPjz4XTbXjcE1sVY0L  iSE5cmU1lLKh7vUQlUMsjdFkk+Th0i0LHWlj1Ek6CUo4M/1pQ5/ZdNh6IrqQgxaw  Ls3IqGMkIGPGR3xdxacEwHJ3f9TJJoZl0/5ePrdPM3dYI6DiqvPfvr028/irFSk8  eZN3az2tyPXDcFRKCEdsBsaemQv63qXuHB/FMV9aU+9HMyDgX8QHNPdVyKvtQrf4  +7aEFKk/owAKFxJq3q7T4b43N9ldVL+CIYKgkPTM9F03lwsMyVnM10NbFvIa+7iQ  A2KKW2+27XxZWamG9jBlMEgD0Q3pEWkXsTW9/ys7S3C/Cg6MHJacIM4cckwRYJTe  vaSQdnMBjEDv4uVU+VyxfDMXIleYe7iSE+a4eml3lMslrkmbVt8cL+XoKi5d4mCX  uOZMdy50wPAZI2UCSBFZv/gbSEo5w4STGxWMwvZsEX+SvLKItr0=  =ZW3F  -----END PGP SIGNATURE-----
    fix: reduce benchmark

--- website/components/chart.tsx
+++ website/components/chart.tsx
@@ -23,8 +23,6 @@ ChartJS.register(
 const data = [
   { framework: 'JavaScript', val: 0.96 },
   { framework: 'Million.js', val: 0.92 },
-  { framework: 'Inferno', val: 0.89 },
-  { framework: 'Solid', val: 0.89 },
   { framework: 'Preact', val: 0.59 },
   { framework: 'React', val: 0.26 },
 ];
@@ -51,22 +49,35 @@ const options = {
 export function Chart() {
   const [darkMode, setDarkMode] = useState<boolean | null>(null);
   useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains('dark');
-    if (isDarkMode) {
-      defaults.borderColor = '#545864';
-      defaults.color = '#bdbfc7';
-    } else {
-      defaults.borderColor = '#bdbfc7';
-      defaults.color = '#545864';
-    }
-    setDarkMode(isDarkMode);
+    const updateColors = () => {
+      const isDarkMode = document.documentElement.classList.contains('dark');
+      if (isDarkMode) {
+        defaults.borderColor = '#545864';
+        defaults.color = '#bdbfc7';
+      } else {
+        defaults.borderColor = '#bdbfc7';
+        defaults.color = '#545864';
+      }
+      setDarkMode(isDarkMode);
+    };
+    updateColors();
+
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        if (mutation.attributeName === 'class') {
+          updateColors();
+        }
+      }
+    });
+    observer.observe(document.documentElement, { attributes: true });
   }, []);
   const color = darkMode ? '#54527b' : '#dcc9e8';
-  const backgroundColor = [color, '#b073d9', color, color, color, color];
+  const backgroundColor = [color, '#b073d9', color, color];
   return (
     <div className="p-4 rounded-lg w-auto min-h-[270px]">
       {darkMode !== null && (
         <Bar
+          redraw
           options={options}
           data={{
             labels: data.map((row) => row.framework),
