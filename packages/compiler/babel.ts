31b88952d0eeb75bd0e203583e42d9bb99c84946
Aiden Bai <aiden.bai05@gmail.com>
2023-04-12 10:04:23 -0700

    fix: add warnings for conditionals

--- packages/compiler/babel.ts
+++ packages/compiler/babel.ts
@@ -1,13 +1,13 @@
 import { declare } from '@babel/helper-plugin-utils';
 import { optimize } from './optimize';
-import { react } from './react';
+import { transformReact } from './react';
 
 export default declare((api, options) => {
   api.assertVersion(7);
   return {
     name: 'million',
     visitor: {
-      CallExpression: options.mode === 'optimize' ? optimize : react,
+      CallExpression: options.mode === 'optimize' ? optimize : transformReact,
     },
   };
 });
