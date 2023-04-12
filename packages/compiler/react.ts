524f60c88bf6fec2f066c852b0cf4111883b7c19
Aiden Bai <aiden.bai05@gmail.com>
2023-04-11 18:38:42 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmQ2C6MACgkQ03WEOIZ1  /zrqTg/9GN3nP+FcVoTEN9bGTtVtdYaJGHXV2Sela6IOmDkjIofQbDA97rAu540M  6sV55HlKgBDxW4o/jZDCS4ibiu59C2LTAbIAyydMPTiVgJwf4uxJJr8jllYOCBS6  AzKF5bWOp56nAHjc2t2O3sYwHbxd/vcEy006ruEcOpD+MqZj7YaAQoralNjUCqTC  Cjcq24Lbo+9Mm+0jyBS0H9wgDi6jeD+Sp0SuGwG/Sn+6sZBjZpEZ3Go92wqneiD2  jpSe2Fb7ngOGnIpSEhMlCmG1yed23XAS1dx6s26iClZDvKAtoRUW5WGBD8/XPdYX  nSsTw/L5H7Mwb9qpDzf/B0pGuZzCqFHPyurequsjeEUahK/F/zpuXRWHn0I32YaG  Wwy+Pq03BE1IdzspnLQzdTqLDzAHF7en6C/u/uuCOns0wKOM5PiI4A9P9kPaIbpA  PP38SwxzMZ+jwqN0KXEsQoR8vqt1n04KzWdXDr60k6FgsrMhMAeK7s5usL8z1jWl  oQGg4PKrTP/oiaXXDOSE6Mtjx8jL9fBlTTtWgttr7iBiafgEI8M7NbK9d+8KC/14  klq5wnRnJJJMHxm0Bz/9N50eYH8GpXqHt20RR+M8VWw33RRBpuH7wkot3bkwK9Ig  r9kNqgXnkZS+K0EiuNctNCYfyVrn7CDR3Xa2OmcLPpXYsHUPfB4=  =hANv  -----END PGP SIGNATURE-----
    docs: update README

---
+++ packages/compiler/react.ts
@@ -0,0 +1,149 @@
+import * as t from '@babel/types';
+import type { NodePath } from '@babel/core';
+
+export const react = (path: NodePath<t.CallExpression>) => {
+  // TODO: allow aliasing (block as newBlock)
+  if (t.isIdentifier(path.node.callee, { name: 'block' })) {
+    // gets the binding for the "Component" in "block(Component)"
+    const blockFunction = path.scope.getBinding(path.node.callee.name);
+    if (!blockFunction) return;
+    const importSource = blockFunction.path.parent;
+
+    // Check if import is from million/react
+    if (
+      !t.isVariableDeclarator(path.parentPath.node) ||
+      !t.isImportDeclaration(importSource) ||
+      !importSource.source.value.includes('million/react')
+    ) {
+      return;
+    }
+
+    // Get the name of the component
+    const componentId = path.node.arguments[0] as t.Identifier;
+    if (!t.isIdentifier(componentId)) {
+      throw new Error('Unsupported argument for block');
+    }
+    const componentBinding = path.scope.getBinding(componentId.name);
+    const component = componentBinding?.path.node;
+
+    if (t.isFunctionDeclaration(component)) {
+      handleComponent(path, component.body, component);
+    } else if (
+      t.isVariableDeclarator(component) &&
+      t.isArrowFunctionExpression(component.init)
+    ) {
+      handleComponent(path, component.init.body, component);
+    }
+  }
+};
+
+const handleComponent = (
+  path: NodePath<t.CallExpression>,
+  componentFunction: object | null | undefined,
+  component: any,
+) => {
+  if (!t.isBlockStatement(componentFunction)) return;
+
+  const bodyLength = componentFunction.body.length;
+  // Gets the returned JSX element
+  const view = componentFunction.body[bodyLength - 1];
+
+  if (t.isReturnStatement(view) && t.isJSXElement(view.argument)) {
+    const jsx = view.argument;
+    // Extracts all expressions / identifiers from the JSX element
+    const dynamics = getDynamicsFromJSX(path, jsx);
+    const blockVariable = path.scope.generateUidIdentifier('block$');
+
+    // Creates a new function that will be wrapped in block() instead
+    const blockFunction = t.functionDeclaration(
+      blockVariable,
+      // Destructures props
+      [t.objectPattern(dynamics.map(({ id }) => t.objectProperty(id, id)))],
+      t.blockStatement([view]),
+    );
+
+    // Replaces the return statement with a call to the new block() function
+    componentFunction.body[bodyLength - 1] = t.returnStatement(
+      t.callExpression(component.id, [
+        t.objectExpression(
+          // Creates an object that passes expression values down
+          dynamics.map(({ id, value }) => t.objectProperty(id, value || id)),
+        ),
+      ]),
+    );
+
+    // Swaps the names of the functions so that the component that wraps the
+    // block is called instead
+    const blockComponent = path.parentPath.node as any;
+    const temp = blockComponent.id;
+    blockComponent.id = component.id;
+    component.id = temp;
+    path.node.arguments[0] = blockVariable;
+
+    path.parentPath.parentPath?.insertBefore(blockFunction);
+  }
+};
+
+const getDynamicsFromJSX = (
+  path: NodePath<t.CallExpression>,
+  jsx: t.JSXElement,
+  dynamics: { id: t.Identifier; value: t.Expression | null }[] = [],
+  count = 0,
+) => {
+  const { openingElement } = jsx;
+  const { attributes } = openingElement;
+
+  const createDynamic = (
+    identifier: t.Identifier | null,
+    expression: t.Expression | null,
+  ) => {
+    // We need to create identifiers for expressions
+    const id = identifier || t.identifier(`__${count}`);
+    dynamics.push({ value: expression, id });
+    count++;
+    return id;
+  };
+
+  for (let i = 0, j = attributes.length; i < j; i++) {
+    const attribute = attributes[i]!;
+
+    if (
+      t.isJSXAttribute(attribute) &&
+      t.isJSXExpressionContainer(attribute.value)
+    ) {
+      const { expression } = attribute.value;
+
+      if (t.isIdentifier(expression)) {
+        createDynamic(expression, null);
+      } else if (t.isExpression(expression)) {
+        const id = createDynamic(null, expression);
+        attribute.value.expression = id;
+      }
+    }
+
+    if (t.isJSXSpreadAttribute(attribute)) {
+      throw new Error('Spread attributes are not supported. ');
+    }
+  }
+
+  for (let i = 0, j = jsx.children.length; i < j; i++) {
+    const child = jsx.children[i]!;
+
+    if (t.isJSXExpressionContainer(child)) {
+      const { expression } = child;
+      if (t.isIdentifier(expression)) {
+        createDynamic(expression, null);
+      } else if (t.isJSXElement(expression)) {
+        throw new Error(
+          'JSX elements cannot be used as expressions. Please wrap them in a block.',
+        );
+      } else if (t.isExpression(expression)) {
+        const id = createDynamic(null, expression);
+        child.expression = id;
+      }
+    } else if (t.isJSXElement(child)) {
+      getDynamicsFromJSX(path, child, dynamics, count);
+    }
+  }
+  return dynamics;
+};
