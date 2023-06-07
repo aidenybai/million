7c64be4538c9c3323ce3c9b423aaa365500f6121
Nathan <nathancai1031@gmail.com>
2023-06-07 19:48:36 -0400

    added block to types because it will also be used by other packages

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,6 +1,8 @@
-import { Block } from "packages/million";
+import { Block } from 'packages/million';
 
 export type MillionProps = Record<
   string,
   string | number | boolean | null | undefined | Block | Symbol | BigInt
 >;
+
+export { Block };
