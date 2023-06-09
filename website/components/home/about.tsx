0691a516634a05249247cd283a76f9f040857d95
Aiden Bai <aiden.bai05@gmail.com>
2023-06-08 18:05:25 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmSCetUACgkQ03WEOIZ1  /zo4cg/8CYGusPXFdpQo5jazMWaRzPXLraSiwfjc/oWodbRE0VARxjNlDxN69shO  D4VYIfp56ZlsVCHDTFg1ZpU145M/lO14+ItbFruFpSlxoAjjoI68EYH/eaPdE7sl  FYhFjMMHexQj5pOE7B+Ps9m/xU927GD6H7Yk283sgmw8cP/PosDVQeWSylTqxiMF  DBu497Bkmvt2PWzEH9CUtKMhS8GgzifBaO0vwO8Wt0bIBY+aJ3e2hDywZZF1DEDM  +ESGhrnD85hx5DJca6O8aduZS2puupcsANWW08KD75CbMBgG0aTz8aMFWR1x26y4  u/C6W+/pGDp4d2q+nxYfLA7v9h72pbAqnXz/ba1+zVCsLla7VeoFpSnNGH1A0x6q  mjh0yv+kSc+TTZZBYJn5qjhlLC//Mwj2+4A+LuI3BnAL2Nu1GswyTJ/n6ybjuYVY  /aJ4d6jCEqzikcoqlbSiYme7w56ef42Pu0sSPRknu/mSQ2iDvsJgvFkCyw+NYIqV  hd2174wf2zUO9KJgpzZKnkobgdCpNX9W9RTobYjkoCBR6XpVs4Myd6fhLKtx0UKH  aM1ozDBI95eCSQD8ZilxweunlD+p7sYba40RySY0pXR34TqCQ3As4bSuYlrHmpik  eImqvy2mM8JhRSETlLIMYjhGPjLu+AZM0gR+U9260hFMgHP6qSU=  =t3F2  -----END PGP SIGNATURE-----
    docs(home): fancy animations

--- website/components/home/about.tsx
+++ website/components/home/about.tsx
@@ -57,20 +57,7 @@ export function About() {
               <div className="divide-y space-y-4 divide-zinc-100 dark:divide-zinc-800">
                 <div className="mt-8 flex gap-4 md:items-center">
                   <div className="w-12 h-12 flex gap-4 rounded-full bg-purple-100 dark:bg-purple-900/20">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6 text-purple-400 m-auto"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
-                      />
-                    </svg>
+                    <LightningIcon />
                   </div>
                   <div className="w-5/6">
                     <h4 className="font-semibold text-lg text-zinc-700 dark:text-purple-300">
@@ -82,21 +69,8 @@ export function About() {
                   </div>
                 </div>
                 <div className="pt-4 flex gap-4 md:items-center">
-                  <div className="w-12 h-12 flex gap-4 rounded-full bg-purple-100 dark:bg-teal-900/20">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6 m-auto text-purple-600 dark:text-purple-400"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
+                  <div className="w-12 h-12 flex gap-4 rounded-full bg-purple-100 dark:bg-purple-900/20">
+                    <ClockIcon />
                   </div>
                   <div className="w-5/6">
                     <h4 className="font-semibold text-lg text-zinc-700 dark:text-purple-300">
@@ -118,95 +92,146 @@ export function About() {
             Sluggish to snappy
           </h3>
           <div className="relative mt-12 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            <div className="relative overflow-hidden rounded-2xl bg-gradient-to-b from-zinc-200 to-white p-px dark:from-zinc-700 dark:via-zinc-800 dark:to-darker">
-              <div className="relative flex h-full flex-col gap-6 rounded-2xl bg-zinc-100 p-8 dark:bg-zinc-900">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6 text-purple-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-                  />
-                </svg>
-
-                <div>
-                  <h4 className="text-xl font-semibold text-zinc-900 dark:text-white">
-                    Block Virtual DOM
-                  </h4>
-                  <p className="mt-3 text-zinc-600 dark:text-zinc-400">
-                    Million.js introduces a novel "block" virtual DOM. It's
-                    significantly faster than React's virtual DOM, as it diffs
-                    data instead of the DOM.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="relative overflow-hidden rounded-2xl bg-gradient-to-b from-zinc-200 to-white p-px dark:from-zinc-700 dark:via-zinc-800 dark:to-darker">
-              <div className="relative flex h-full flex-col gap-6 rounded-2xl bg-zinc-100 p-8 dark:bg-zinc-900">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 text-purple-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-                  />
-                </svg>
-
-                <div>
-                  <h4 className="text-xl font-semibold text-zinc-900 dark:text-white">
-                    Supercharged Compiler
-                  </h4>
-                  <p className="mt-3 text-zinc-600 dark:text-zinc-400">
-                    Million.js uses a custom compiler that automatically
-                    optimizes your React components on the server.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="relative overflow-hidden rounded-2xl bg-gradient-to-b from-zinc-200 to-white p-px dark:from-zinc-700 dark:via-zinc-800 dark:to-darker">
-              <div className="relative flex h-full flex-col gap-6 rounded-2xl bg-zinc-100 p-8 dark:bg-zinc-900">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 text-purple-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
-                  />
-                </svg>
-
-                <div>
-                  <h4 className="text-xl font-semibold text-zinc-900 dark:text-white">
-                    Just one function
-                  </h4>
-                  <p className="mt-3 text-zinc-600 dark:text-zinc-400">
-                    Tired of learning new frameworks and big migrations?
-                    Million.js ships one powerful function that you can use to
-                    optimize any component.
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Card
+              title="Block Virtual DOM"
+              icon={<BoxIcon />}
+              description={`Million.js introduces a novel "block" virtual DOM. It's significantly faster than React's virtual DOM, as it diffs data instead of the DOM.`}
+            />
+            <Card
+              title="Supercharged Compiler"
+              icon={<LightBulbIcon />}
+              description={
+                'Million.js uses a custom compiler that automatically optimizes your React components on the server.'
+              }
+            />
+            <Card
+              title="Just one function"
+              icon={<ThumbsUpIcon />}
+              description={
+                'Tired of learning new frameworks and big migrations? Million.js ships one powerful function that you can use to optimize any component.'
+              }
+            />
           </div>
         </Container>
       </div>
     </>
+  );
+}
+
+function Card({ title, description, icon }) {
+  return (
+    <Tilt
+      tiltMaxAngleX={2.5}
+      tiltMaxAngleY={5}
+      glareEnable
+      tiltAngleYInitial={0}
+      glareMaxOpacity={0.1}
+      className="relative overflow-hidden rounded-2xl bg-gradient-to-b from-zinc-200 to-white p-px dark:from-zinc-700 dark:via-zinc-800 dark:to-darker"
+    >
+      <div className="relative flex h-full flex-col gap-6 rounded-2xl bg-zinc-100 p-8 dark:bg-zinc-900">
+        {icon}
+        <div>
+          <h4 className="text-xl font-semibold text-zinc-900 dark:text-white">
+            {title}
+          </h4>
+          <p className="mt-3 text-zinc-600 dark:text-zinc-400">{description}</p>
+        </div>
+      </div>
+    </Tilt>
+  );
+}
+
+function ThumbsUpIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6 text-purple-500 hover:animate-spin"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+      />
+    </svg>
+  );
+}
+
+function LightBulbIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6 text-purple-400 hover:animate-spin"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+      />
+    </svg>
+  );
+}
+
+function BoxIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6 text-purple-400 hover:animate-spin"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+      />
+    </svg>
+  );
+}
+
+function LightningIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6 text-purple-400 m-auto hover:animate-spin"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+      />
+    </svg>
+  );
+}
+
+function ClockIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6 m-auto text-purple-600 dark:text-purple-400 hover:animate-spin"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
   );
 }
