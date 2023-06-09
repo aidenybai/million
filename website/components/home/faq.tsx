554b5bd25d3e7fb30ecd0a94d9fae809c6bb7cbc
Aiden Bai <aiden.bai05@gmail.com>
2023-06-09 14:12:22 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmSDlbYACgkQ03WEOIZ1  /zrMdA//RWsMbupU/ofLvT4r/BhDNz/YneclFqF/PAa6KLb2iM6GvU7Cs2gqGnuM  Ak1+REKdTxns+0lQqfLvHiC8h1UnSdfUGAI01NB4GsfWxlmBGN+092bOpGoJ/jOn  LzR5b5ZukfA/FEOEUGkkqbvBXOsr6gzTHrtPiEDZZXLCn0AMiFJNvcTv/u8no4Rz  03OF49CTcsKsrrMWuYHJbL9XJxDAQbyMstJqIvWcBFwYsES4rG73i/z43/+u6PvE  HeTI2UfKvZSYQ5lQtoGDI9+7g/7o0CpVd7Ub6aBTWP83e906BvITQDBJjGHScgtU  /4BFNoa9J3FMPOO+cmIAf8FueKlTjdsqfzQF1XWpYC5tKqUBMqRh2t1h7pFsPK6m  3rcqQpI4Z0OENdgw02wGt+GSr5UiYSmYhmUy/VCz4krUh4ag7ngGkVZlctK0qaYa  PI7hcLAbjBJ+B8K+aWgXRBs/TvWHlelx+VCkYzGX674qE6IdSQkuNQiZF7mKlIU9  LEYN6X5tF7/mk++oyTdpKFgQFmdwzXKlW3VVPc0Ke34/leeYecFMbvwo81lCEfVp  6gbTgzAWom4YTtXDtKjjxYqqxLBT0cdZrkEE0jHAm5XTTWVOFsBanvrbiu1cs8r5  1hYldYreXrKsmkxg7Tnrpsttdw2LoFDyC1fpwdyroJv7l7i/hyk=  =w4wg  -----END PGP SIGNATURE-----
    docs: add FAQ

--- website/components/home/faq.tsx
+++ website/components/home/faq.tsx
@@ -128,67 +128,75 @@ export function FAQ() {
               digging into the documentation and reading our blog articles.
             </p>
           </div>
-          <div className="divide-y divide-gray-200 border-y border-gray-200 dark:divide-gray-800 dark:border-gray-800 lg:w-7/12">
-            {faq.map((item, i) => (
-              <Disclosure
-                as="div"
-                key={i}
-                className="mx-auto max-w-2xl text-lg"
-              >
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button
-                      type="button"
-                      className="flex w-full items-start justify-between py-6 text-left text-gray-400"
-                    >
-                      <span className="font-medium text-gray-900 dark:text-white">
-                        {item.question}
-                      </span>
-                      <span className="ml-6 flex h-7 items-center">
-                        <svg
-                          className={`arrow-down h-6 w-6 transform duration-300 ${
-                            open ? 'rotate-180' : 'rotate-0'
-                          }`}
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                          ></path>
-                        </svg>
-                      </span>
-                    </Disclosure.Button>
-                    <Transition
-                      enter="transition duration-100 ease-out"
-                      enterFrom="transform scale-95 opacity-0"
-                      enterTo="transform scale-100 opacity-100"
-                      leave="transition duration-75 ease-out"
-                      leaveFrom="transform scale-100 opacity-100"
-                      leaveTo="transform scale-95 opacity-0"
-                    >
-                      <Disclosure.Panel
-                        className={`pr-12 duration-300 ease-in-out ${
-                          open ? '' : 'hidden'
-                        }`}
-                      >
-                        <p className="pb-6 text-base text-gray-600 dark:text-gray-400">
-                          {item.answer}
-                        </p>
-                      </Disclosure.Panel>
-                    </Transition>
-                  </>
-                )}
-              </Disclosure>
-            ))}
+          <div className="lg:w-7/12">
+            <Disclosures />
           </div>
         </div>
       </Container>
+    </div>
+  );
+}
+
+export function Disclosures({ full = false }) {
+  return (
+    <div className="divide-y divide-gray-200 border-y border-gray-200 dark:divide-gray-800 dark:border-gray-800">
+      {faq.map((item, i) => (
+        <Disclosure
+          as="div"
+          key={i}
+          className={`mx-auto text-lg ${full ? '' : 'max-w-2xl'}`}
+        >
+          {({ open }) => (
+            <>
+              <Disclosure.Button
+                type="button"
+                className="flex w-full items-start justify-between py-6 text-left text-gray-400"
+              >
+                <span className="font-medium text-gray-900 dark:text-white">
+                  {item.question}
+                </span>
+                <span className="ml-6 flex h-7 items-center">
+                  <svg
+                    className={`arrow-down h-6 w-6 transform duration-300 ${
+                      open ? 'rotate-180' : 'rotate-0'
+                    }`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    ></path>
+                  </svg>
+                </span>
+              </Disclosure.Button>
+              <Transition
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+              >
+                <Disclosure.Panel
+                  className={`pr-12 duration-300 ease-in-out ${
+                    open ? '' : 'hidden'
+                  }`}
+                >
+                  <p className="pb-6 text-base text-gray-600 dark:text-gray-400">
+                    {item.answer}
+                  </p>
+                </Disclosure.Panel>
+              </Transition>
+            </>
+          )}
+        </Disclosure>
+      ))}
     </div>
   );
 }
