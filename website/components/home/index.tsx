2973a6c5308cfbe349c5cab6afcc1376bb4d5dec
Aiden Bai <aiden.bai05@gmail.com>
2023-06-09 00:24:34 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmSC07MACgkQ03WEOIZ1  /zq4Pw/+LaNFz6N6LjeXkdkjS/mkf8yxHf0ElbPWR1s2vBMnu1f+QmxLeMP3SSbD  bMXAS072tGtwUEiZS/pSxzZMwFJaGhxh3AWUs+BgdtOLU0iGDMd5725kXfBZCtCQ  xLgv4WDzPSidJW9FIub9ilBv6EDAU6f5HfrWmSSg383huvSTPclA4Ik+hynX1H71  JGmqry5irNqil78VMY8tIEYAWpv6qB1Hs8Mo6/Hso5wwVLCRErOT8hdWAU17bSQx  dsZpSEdgRJA60fmbCwc7ykzH8anD2u0h2RGZjK4mOSiWlxk2Rkq5THTgSAG1UI0/  SJ0DaOSFBY8HM42qW+7lod94CVJmgHbLwmNRnwHRSXpKi9aEDn+Z5S+B4wBs6e5S  oWgEcq2aXmqRECagyux+1ALL1f0oWtMIwuW2E+SfRdG+HimYd6Gkxz1Qf9BVHVNU  K3OyKxs0vJx8Kb04UauEgid81zUlfed93i29ok0ZEKo1BHkXC4jaopi8H8Z1QgKB  W/7y4w7WhS8BR869IjiVwIOUsZ9mQMzKYmsQP+7FYk6PNB8x8iKcq4/gkUhE5042  7iTawGAsbMGOmAl60MUjX6OoMP7iweL0ZpoEQ7I19PzpnDCjShgC5Qu1PGa3LKRI  mAkCEEugSU+zIBd4UBJ7jAIAKDZc+BL0Va98yLD1k2VsvBx1gM8=  =D/Yl  -----END PGP SIGNATURE-----
    docs: add faq

--- website/components/home/index.tsx
+++ website/components/home/index.tsx
@@ -1,12 +1,14 @@
 import { About } from './about';
 import { CTA } from './cta';
 import { Hero } from './hero';
+import { FAQ } from './faq';
 
 export function Home() {
   return (
     <main className="space-y-40 mb-40">
       <Hero />
       <About />
+      <FAQ />
       <CTA />
     </main>
   );
