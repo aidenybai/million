2076bb8809b3c5a6c5427bc37234119f6d164693
poteboy <poteboy.dev@gmail.com>
2023-06-13 14:06:02 +0900

    rm snapshots for the compiler test

--- test/compiler.test.ts
+++ test/compiler.test.ts
@@ -13,20 +13,6 @@ describe.concurrent('compiler', () => {
       warnSpy.mockRestore();
     });
 
-    it('should transforms a valid block correctly', () => {
-      const inputCode = `
-        import { block } from "million/react";
-        function Lion() {
-            return (
-                <img src="https://million.dev/lion.svg" />
-            );
-        }
-        const LionBlock = block(Lion)
-        `;
-      const result = babelTransform(inputCode);
-      expect(result.code).toMatchSnapshot();
-    });
-
     it('should warns when an invalid block is used', () => {
       const inputCode = `
         import { block } from "million/react";
