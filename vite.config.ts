7e92a602a285766727166b16e1adb7d7fa27dae8
poteboy <59927325+poteboy@users.noreply.github.com>
2023-06-16 10:07:49 +0900

    wsBcBAABCAAQBQJki7XlCRBK7hj4Ov3rIwAACLoIAIwS89qzkjVgBKDY3sI4iA4n  2ccz4VroCPACvRzleDm+pGMFF5KspN+FMBzQhqnQeLzXMBCdZK+ye0xZ+I2iqB2J  tocf6XlxzuYjOj6yUD8zTtDEr94oD+GmDBf3qgoOzTsdoJhA7CO8CKp6oRJl860Q  74NVgKFshveycw0BKn7qqbVKeK29A9CY9WkZse6uTeEI5nBm2EjoZePnbey57k+u  OznoHOUWJ7lGqlLGjcjYBo8LIoZ1N3yw8cr2FPqYSxPKnMRlB1g9xu6DJQqRjbiY  jvFyegTwkKUVvbJ21YeUTnZ9fg1d3I8G2++bjvfJ181qGmazRQcsJkMtdf1dvS8=  =5yEj  -----END PGP SIGNATURE-----
    Upgrade Vitest to the latest version (#423)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
-
+import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   esbuild: {
     jsxFactory: 'h',
     jsxInject: `import { h } from 'packages/jsx-runtime';`,
