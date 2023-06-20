1a465062b2f05933c8fb1bd03813ff797c0f6aa3
Aiden Bai <aiden.bai05@gmail.com>
2023-06-20 01:37:33 -0700

    fix: accessing raw component and props

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -6,7 +6,7 @@ export type MillionProps = Record<string, any>;
 export interface Options {
   shouldUpdate?: (oldProps: Props, newProps: Props) => boolean;
   block?: any;
-  originalComponent?: ComponentType<any>;
+  component?: ComponentType<any>;
 }
 
 export interface MillionArrayProps<T> {
