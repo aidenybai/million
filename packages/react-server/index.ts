ca0276a20bfbbe24dcab0966cf827a7c7b946084
Aiden Bai <aiden.bai05@gmail.com>
2023-06-25 00:17:39 -0700

    fix: remove unnecessary stuff

--- packages/react-server/index.ts
+++ packages/react-server/index.ts
@@ -35,7 +35,7 @@ export const block = <P extends MillionProps>(
     }, []);
 
     if (!ready || !blockFactory) {
-      return createElement(
+      return createElement<P>(
         RENDER_SCOPE,
         null,
         // During compilation we will attach a .raw for the component and
