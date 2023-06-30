5f7277583ce535064b2c9a7858c401e263512e95
Aiden Bai <aiden.bai05@gmail.com>
2023-06-29 23:41:28 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmSeeRgACgkQ03WEOIZ1  /zp/Aw//fM7nXL4AaKoGx1WCKPgqNxA2OhywDcQRw1OuT1UU2A5xUitgRsokio4x  GSat6f89M1jNFa7wBc3P3o99SOf744Thp5vdbFuYECQNhBQmZm3TCEMIQgg7VJLr  WwhZ8M6CTueNkOjyXgbmPPFcL/WHWMy7yvEBFuoQlmiupww5LtNMd1T4hSaFJwhq  CkNgEZM97T2x9uwiCc1cozYqg6iMB6LwkM/nB3V7zBDti1nO6bhSSfKh/UOfsrek  lcGlJM1Ox9YO/92ArhrBJ3bbHd4uVeErBwmkZJ8QEUKzmis3peQNTBXF/DYcjseA  NFKPFefRbwm/aSjBP3KaTrKbeJkF2D58afUiy6buHwAtAzA/fShgX9CPoWWBv/CT  iXJhua6vm1v1cqXMpPL1M0fDVUg3kZX3ikWuN/qYDfQCflIlsAzzbMgEVmTYwfZ0  62SQsP9NfddI8OayLvJ3/dUWz3hG4tdEoqtXMUkPtl5QzkpdhdkYLpJ+0GsnCWh8  uVpJmTf3RacmdR2pZhAQt/0YTbU0eC8cc20aXqq//8U40Lymdvm8iXJ1ytls7MKl  9arrk3R5RnkstG+BzD4k5jlXcBJo+cZBufgzwQkaCI/qKlvhDUeBkpjSu+WWFhiT  IvMch1Z/yvmytYqDPcw/Qtjp+MyhSeFKtdc4GakccRnc0S00SnU=  =Ws7D  -----END PGP SIGNATURE-----
    docs: fix sponsor button for mobile

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -164,7 +164,11 @@ const config: DocsThemeConfig = {
             <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
           </svg>
         </a>
-        <a href="https://github.com/sponsors/aidenybai" target="_blank">
+        <a
+          href="https://github.com/sponsors/aidenybai"
+          target="_blank"
+          className="hidden sm:block"
+        >
           <iframe
             src="https://github.com/sponsors/aidenybai/button"
             title="Sponsor aidenybai"
