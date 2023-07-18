9e78648108977f7b0cdbf07193a22f42fcff2cd4
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-07-18 01:30:14 +0100

    write rules for block declaration warnings

---
+++ packages/eslint-plugin-million/lib/rules/warn-block-declaration.js
@@ -0,0 +1,34 @@
+module.exports = {
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "Block needs to be defined as a variable declaration.",
+      category: "Best Practices",
+      fixable: "code",
+      recommended: true,
+    },
+    schema: [],
+  },
+ create(context) {
+    return {
+      CallExpression(node) {
+
+        const { type, name } = node.callee; 
+        const parent = node.parent; 
+        
+        if (
+          type === 'Identifier' &&
+          name === 'block' && (!parent || parent.type !== "VariableDeclarator")
+      ){
+          context.report({
+          node: node.callee, // Updated to "node.callee" instead of "callee"
+          message:
+              '[Million.js] Block needs to be defined as a variable declaration.',
+          });
+        }
+      },
+    };
+  }
+};
+
