a25b43f84d90ed7d1df275dcfdf8c022cbc2c353
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-07-18 01:29:56 +0100

    write rules for block call warnings

---
+++ packages/eslint-plugin-million/lib/rules/warn-block-call.js
@@ -0,0 +1,32 @@
+module.exports = {
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "Found unsupported argument for block. Make sure blocks consume a reference to a component function or the direct declaration",
+      category: "Best Practices",
+      fixable: "code",
+      recommended: true,
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      CallExpression(node) {
+        const { callee, arguments: args } = node;
+
+        const { type, name } = callee;
+        if (
+          type === 'Identifier' &&
+          name === 'block' && args[0].type === 'JSXElement'
+        ){
+          context.report({
+            node: node,
+            message:
+              '[Million.js] Found unsupported argument for block. Make sure blocks consume a reference to a component function or the direct declaration',
+          });
+        }
+      },
+    };
+  }
+};
