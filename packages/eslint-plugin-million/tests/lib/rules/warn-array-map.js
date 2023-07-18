4216a9e8050a139281634cbac327d89f8fd00dbf
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-07-18 01:32:37 +0100

    write test for array map rules

---
+++ packages/eslint-plugin-million/tests/lib/rules/warn-array-map.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const rule = require("../../../lib/rules/warn-array-maps"),
+    RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("warn-array-maps", rule, {
+    valid: [],
+    invalid: [{
+        code: "Array.map(item => <li>{item}</li>)",
+        errors: [{
+            suggestions: [{
+                desc: "Array.map() will degrade performance. We recommend removing the block on the current component and using a <For /> component instead.",
+                output: "<For each={items}>{(item) => <li>{item}</li>}</For>"
+            }]
+        }]
+    }]
+})
