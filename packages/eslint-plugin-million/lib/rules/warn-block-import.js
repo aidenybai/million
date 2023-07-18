74fc6255beef477fc0b2fde973ee95a599203a60
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-07-18 01:30:28 +0100

    write rules for block import warnings

---
+++ packages/eslint-plugin-million/lib/rules/warn-block-import.js
@@ -0,0 +1,30 @@
+module.exports = {
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "Found unsupported import for block. Make sure blocks are imported from million/react.",
+      category: "Best Practices",
+      fixable: "code",
+      recommended: true,
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      ImportDeclaration(node) {
+        const { type, specifiers, source } = node;
+        if (
+          type === 'ImportDeclaration' &&
+          specifiers[0].type === 'ImportSpecifier' && specifiers[0].imported.name === 'block' && source.value !== 'million/react'
+        ){
+          context.report({
+            node: type,
+            message:
+              '[Million.js] Found unsupported import for block. Make sure blocks are imported from million/react.',
+          });
+        }
+      },
+    };
+  }
+};
