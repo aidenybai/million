d8a2035701f37d1a9b6ca1769ee9ec6e370b9b30
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-07-18 07:30:42 +0100

    extend million recommended config

--- packages/eslint-plugin-million/lib/index.js
+++ packages/eslint-plugin-million/lib/index.js
@@ -18,5 +18,20 @@ const requireIndex = require("requireindex");
 // import all rules in lib/rules
 module.exports.rules = requireIndex(__dirname + "/rules");
 
+//extend million recommended rules, this allows the recommended config rules to be loaded automatically
+
+module.exports.config = {
+    recommended: {
+        rules: {
+        "million/warn-block-import": "error",
+        "million/warn-block-declaration": "error",
+        "million/warn-block-call": "error",
+        "million/warn-array-map": "error",
+        "million/warn-conditional-expression": "error",
+        "million/warn-ui-components": "error",
+    }
+    }
+}
+
 
 
