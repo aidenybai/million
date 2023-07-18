a2228fd552e8e41a9fa790450b932cd612efb73c
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-07-18 01:31:08 +0100

    write rules for ui component warnings

---
+++ packages/eslint-plugin-million/lib/rules/warn-ui-components.js
@@ -0,0 +1,24 @@
+module.exports = {
+  meta: {
+    type: "suggestion",
+    docs: {
+      description:
+        "Components will cause degraded performance. Ideally, you should use DOM elements instead.",
+      category: "Best Practices",
+      recommended: true,
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      JSXElement(node) {
+        context.report({
+            node,
+            message:
+            '[Million.js] Components will cause degraded performance. Ideally, you should use DOM elements instead.',
+              });
+        },
+    };
+  }
+};
+
