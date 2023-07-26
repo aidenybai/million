bf46a37b05e5f282a2d0f2a471ac7e5f0e019be9
Jovi De Croock <decroockjovi@gmail.com>
2023-07-26 09:09:17 +0200

    Update constants.ts

--- packages/preact/constants.ts
+++ packages/preact/constants.ts
@@ -1,13 +1,5 @@
-import { useEffect } from 'preact/hooks';
-import type { ComponentType, FunctionComponent as FC } from 'preact';
+import type { FunctionComponent as FC } from 'preact';
 
 export const RENDER_SCOPE = 'slot';
 export const SVG_RENDER_SCOPE = 'g';
 export const REACT_ROOT = '__react_root';
-
-export const Effect: FC<{ effect: () => void }> = ({ effect }) => {
-  useEffect(effect, []);
-  return null;
-};
-
-export const REGISTRY = new Map<ComponentType, any>();
