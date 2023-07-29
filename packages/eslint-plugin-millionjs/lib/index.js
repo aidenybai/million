22882cb870ff675ec25d1bb749bea61b5fdb43e5
Drex <davidokunoye003@gmail.com>
2023-07-29 20:04:48 +0100

    made tweaks to the plugin

---
+++ packages/eslint-plugin-millionjs/lib/index.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Custom rules for Kent
+ * @author Kent
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const requireIndex = require("requireindex");
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+
+
+// import all rules in lib/rules
+module.exports.rules = requireIndex(__dirname + "/rules");
+
+
+
