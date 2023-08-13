2b672e5bb47d1d8ec556a56cf1e001905941ecd7
Aiden Bai <aiden.bai05@gmail.com>
2023-08-13 15:01:16 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmTZUqwACgkQ03WEOIZ1  /zqlWw//aGjLTAJUqmGHVk8Ko9iBw8IvGXy7pz5mSr+vXwhPloNOSdVUiBzui+Yv  WyYHwqNQPk9b/dAbcJjKqJ80InNEPDg6Le1xEl1eERbNqx3YzdKZF8H5MMENpx3A  dI/FhTuX8/V17ik6Q4MgHCpoHYbqTUlQ8dz1Sq35a4BcYDfq+jW3f5RMP3q9rEfD  7xDgqEnTmJwYJmRWkVYffD4aBd8gsgTzNEnuZD4zhnF1njAA3PeJdca4U1Qqvw9x  EMk47HFTaqBehIqMMloQ5z/cTI+JVGS/QAUVZCEWfkSHOqyQBk8nvOIMEhTn/xJD  1/+/a4B4w5JScNa4OHxRK8USqj8Nw42evsjxxATtQjec07q/yHfdz4sghLyBziRV  z1p9GCuHSpDdc44ik2h1u2KUjBj0GumRz2cKmQmVifgdo0ausZ+c4jUvVTby2odB  8p+mOTu57+KcorSAD4XbD+AKv955dIiINXqv9KdLzcIlH9wCVNp1J3xekmZg98B4  SJ4oQcDcn8Qg//WgAVAW4FOeHiJKSGs1nf4KtXn2+x8lC4LbvuoHJ7jr2WbOmZcS  anaaBd7fOAroSB0NS/Zb7MEyd6I5DaB1L+EceNQTfbv+PHML3YU1rSQI82R5DFQp  nebGJhOsmF5FrxhcUydJhR7JRHhrdrKhjif/Po665ipqY1z3+C0=  =IDTk  -----END PGP SIGNATURE-----
    feat: update sink

--- packages/kitchen-sink/src/main.tsx
+++ packages/kitchen-sink/src/main.tsx
@@ -5,85 +5,89 @@ import { ErrorBoundary } from 'react-error-boundary';
 
 type Module = Record<string, ComponentType>;
 
-const modules = await Promise.all(
-  Object.entries(import.meta.glob('./examples/*.tsx')).map(
-    async ([key, mod]) =>
-      [
-        key.replace('./examples/', '').replace('.tsx', ''),
-        mod as () => Promise<Module>,
-      ] as const,
-  ),
-);
-const loadedModules: Module[] = [];
-
-function App() {
-  const [selected, setSelected] = useState<number>(-1);
-  const hasSelected = selected >= 0;
-
-  const Mod = hasSelected ? loadedModules[selected]!.default : null;
-
-  return (
-    <div>
-      <h1>Million.js Kitchen Sink</h1>
-      <p>
-        Hey! We're actively recruiting cooks 🧑‍🍳 to help assemble a list of
-        examples of Million + your favorite React library. Go to{' '}
-        <a href="https://million.dev/kitchen-sink">million.dev/kitchen-sink</a>{' '}
-        for details.
-      </p>
-      <div
-        style={{
-          display: 'flex',
-          gap: '0.5rem',
-          flexWrap: 'wrap',
-        }}
-      >
-        {modules.map(([key], index) => {
-          return (
-            <button
-              onClick={async () => {
-                loadedModules[index] = await modules[index]![1]();
-                setSelected(index);
-              }}
-              key={key}
-              disabled={hasSelected && selected === index}
-            >
-              {key.replace('./examples/', '').replace('.tsx', '')}
-            </button>
-          );
-        })}{' '}
-      </div>
-      <details open>
-        <summary>
-          Currently Selected:{' '}
-          <strong>{hasSelected ? modules[selected]![0] : 'None'}</strong>
-        </summary>
-        <div style={{ padding: '1rem' }}>
-          <ErrorBoundary
-            fallbackRender={({ error }) => (
-              <div
-                style={{
-                  color: '#ff8080',
-                  background: '#290000',
-                  padding: '1rem',
-                  borderRadius: '1rem',
-                }}
-              >
-                <h2>💥 {error.message}</h2>
-                <pre>{error.stack}</pre>
-              </div>
-            )}
-          >
-            {hasSelected && Mod && <Mod />}
-          </ErrorBoundary>
-        </div>
-      </details>
-    </div>
+(async () => {
+  const modules = await Promise.all(
+    Object.entries(import.meta.glob('./examples/*.tsx')).map(
+      async ([key, mod]) =>
+        [
+          key.replace('./examples/', '').replace('.tsx', ''),
+          mod as () => Promise<Module>,
+        ] as const,
+    ),
   );
-}
+  const loadedModules: Module[] = [];
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+  function App() {
+    const [selected, setSelected] = useState<number>(-1);
+    const hasSelected = selected >= 0;
+
+    const Mod = hasSelected ? loadedModules[selected]!.default : null;
+
+    return (
+      <div>
+        <h1>Million.js Kitchen Sink</h1>
+        <p>
+          Hey! We're actively recruiting cooks 🧑‍🍳 to help assemble a list of
+          examples of Million + your favorite React library. Go to{' '}
+          <a href="https://million.dev/kitchen-sink">
+            million.dev/kitchen-sink
+          </a>{' '}
+          for details.
+        </p>
+        <div
+          style={{
+            display: 'flex',
+            gap: '0.5rem',
+            flexWrap: 'wrap',
+          }}
+        >
+          {modules.map(([key], index) => {
+            return (
+              <button
+                onClick={async () => {
+                  loadedModules[index] = await modules[index]![1]();
+                  setSelected(index);
+                }}
+                key={key}
+                disabled={hasSelected && selected === index}
+              >
+                {key.replace('./examples/', '').replace('.tsx', '')}
+              </button>
+            );
+          })}{' '}
+        </div>
+        <details open>
+          <summary>
+            Currently Selected:{' '}
+            <strong>{hasSelected ? modules[selected]![0] : 'None'}</strong>
+          </summary>
+          <div style={{ padding: '1rem' }}>
+            <ErrorBoundary
+              fallbackRender={({ error }) => (
+                <div
+                  style={{
+                    color: '#ff8080',
+                    background: '#290000',
+                    padding: '1rem',
+                    borderRadius: '1rem',
+                  }}
+                >
+                  <h2>💥 {error.message}</h2>
+                  <pre>{error.stack}</pre>
+                </div>
+              )}
+            >
+              {hasSelected && Mod && <Mod />}
+            </ErrorBoundary>
+          </div>
+        </details>
+      </div>
+    );
+  }
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+})();
