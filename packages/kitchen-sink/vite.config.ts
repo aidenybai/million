df20bbe02e8a9135974fb7321eeef4e0bc63451d
Aiden Bai <aiden.bai05@gmail.com>
2023-08-13 14:50:21 -0700

    feat: use latest

--- packages/kitchen-sink/vite.config.ts
+++ packages/kitchen-sink/vite.config.ts
@@ -1,15 +1,17 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import million from 'million/compiler';
+// @ts-ignore
+import million from '../compiler';
 import Inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite(), react(), Inspect()],
+  // @ts-ignore
+  plugins: [million.vite({ mute: 'info' }), react(), Inspect()],
   resolve: {
     alias: {
-      'millions/react': path.resolve(__dirname, '../react'),
+      'million/react': path.resolve(__dirname, '../react'),
     },
   },
 });
