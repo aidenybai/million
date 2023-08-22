c66ce531d33ac4408b96a6ab981f613c5d7a8ebe
Aiden Bai <aiden.bai05@gmail.com>
2023-08-21 23:02:07 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmTkT18ACgkQ03WEOIZ1  /zp7mxAAkl4ayt6EBPy3qgjASnV47K1UlV4oOu7esex1k1cXzDawPQEOHdZLmcp+  pndmioFH0cz/ANdvUiFwtHxY6DwPPhO/wWKBdYSXORnxn4tmY2yn4hsMTN5grJ7T  H3jSibT3VdgLd3LpN6/A+Z8Vfbr/f31JzDYsBuYGGA2vNRQT0WXl2fTkhJxlbsVi  Khby/cwgxUuahpyPtnU8puMhavXK+9ZCG0lxCWpmHgANlXsxtaudZK6xMqYAGTfO  AWtTyDzmzkTeXnUNDa1nwGouUCKibfAgd4yO0WNRshcvlTJ4rtYts78wLULWwx5h  fxAHgQnpjpmcA/qFrrHXwzCnReffJF4/AbFAqef38HTw8We0o3aHRlEYbvRvtl+Z  iS3NvUiUmz9PkZOuXb7TVKJdgIv6kUx+c/VTkWp4MPNXhyYWXNLwvxe9RlX9Xp7D  yy59WHdwlUGmRZDPZJGJIbEdxkqquLPpsF7pJFxwul1QCeHMx3mdVJRGAta4VDF5  gqvabTQG01vY1lACASTHdBAhTn/FsIgYAhjr3XTaX0AftLJIXWU1/02IQLVwbs38  ygD5VcPajKE7YUHqa2KZ0WEDOkB/wYVx1btcgY2O/UV5H/YItGeCc4gXDaM+4AXu  m87w5lCj9l7Of5HTbXU1yVhZ7HvfA9ycu3B2kyNDwGZSMwlTXGA=  =udOr  -----END PGP SIGNATURE-----
    fix: eslint

--- test/block.test.ts
+++ test/block.test.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { describe, it } from 'vitest';
 import { block as createBlock } from '../packages/million';
 import type { MillionProps } from '../packages/types';
