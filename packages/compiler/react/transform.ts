a50ae01a203d80accd10f79712a77135f4640bc1
Aiden Bai <aiden.bai05@gmail.com>
2023-08-24 03:16:10 -0700

    fix: rendering of for inside block

--- packages/compiler/react/transform.ts
+++ packages/compiler/react/transform.ts
@@ -560,7 +560,7 @@ export const transformJSX = (
   },
   SHARED: Shared,
 ) => {
-  const { file, imports, unstable, isReact, globalPath } = SHARED;
+  const { file, imports, unstable, isReact } = SHARED;
 
   /**
    * Populates `dynamics` with a new entry.
