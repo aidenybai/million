e0f7427e5969b2b542bd9b689ca39eb75734b495
Aiden Bai <aiden.bai05@gmail.com>
2023-08-26 22:37:24 -0700

    iQIzBAABCAAdFiEEmAqbhBaMcboBVHtE03WEOIZ1/zoFAmTq4RQACgkQ03WEOIZ1  /zpWNg/+KRpjZGuJLGVZRBCc4H5qL8EDatusULK9pxOmW7xePFM+g6K3tZQCK3UA  XIwS6ap4Ar5xWm95AOXWYa5xmN5uvJcI1yZSc/x1nK7/0IIvCXqXPu4dZ+byv/+u  K7Rh202H2K6g7fSbJhyYhX8UOS3KhHoaDNchkptzVUNR5l+NasseBTF15X/FurOP  TPLWtBTUhlJl8XjhCDI4RkTE/M5nBLR2fczsVRegWp6VByE3aJbeiizZjk+PLuH5  aj+UVdszIvjywAgtNxxcxxCNGwo7UXDAWegnsmDIr16JlycpQJT3vAgUAOhR16vS  Pn/n4eCrb25CYhyFJ3oLLXDQu8io5oelzple90AwXVOAp+BqX6JLSt5dDjtx2wif  EPmpISJ81yG39vVC3eVTYWpgIhF9qhhNlHNAzokWUn+4n+mxFrBxQE7lO2sRCi89  AN5BA1bHhI/QYs9574/5sW8Oo6GBLkEMuqi44hdukl9c3wDrTr7M0NXrZpWMg7zU  HLE2h24a28AB9yjhNGIYVmpwjdIhwwvNI1xuzTsdfbZbDoGlb7HUvDdgTX6Qu5+A  44BjCxZy4v0PxEfRHZcSfJyXKMjfQsUPTgXK95nfrqmauP6wOY6LzS5LIVLLnuwj  ABG/RWe8qDRWgjpPfZIkHRW5emX1VyZt4T0Fed3qN2mnYwjA2T0=  =t4bh  -----END PGP SIGNATURE-----
    docs: update

--- website/components/home/showcase.tsx
+++ website/components/home/showcase.tsx
@@ -7,6 +7,7 @@ import LLMReport from '../../pages/showcase/llm-report.png';
 import VeganCheck from '../../pages/showcase/vegancheck.png';
 import Windows11Web from '../../pages/showcase/windows-11-web.jpeg';
 import StudioFreight from '../../pages/showcase/studio-freight.jpeg';
+import Texts from '../../pages/showcase/texts.png';
 import { Container } from './container';
 
 export function Showcase() {
@@ -37,6 +38,11 @@ export function Showcase() {
       url: 'studiofreight.com',
     },
     {
+      image: Texts,
+      name: 'Texts',
+      url: 'texts.com',
+    },
+    {
       image: VeganCheck,
       name: 'Vegan Check',
       url: 'vegancheck.me',
@@ -61,29 +67,29 @@ export function Showcase() {
         </div>
         <div className="slider">
           <div className="slide-track-10 hover:pause mt-6 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5">
-            {[...entries, ...entries, ...entries].map(
-              ({ image, name, url }) => (
-                <a
-                  href={`https://${url}`}
-                  target="_blank"
-                  className=" pr-10 space-y-6 text-center slide grayscale-[50%] opacity-90 hover:opacity-100 transition duration-200 hover:grayscale-0"
-                >
-                  <Image
-                    src={image}
-                    alt={name}
-                    width={256}
-                    height={288}
-                    className="mx-auto h-32 w-72 md:h-40 md:w-24 lg:h-32 lg:w-72 rounded-lg object-cover object-top hover:rotate-[-1deg] hover:scale-110 hover:shadow-lg lg:hover:shadow-2xl transition"
-                  />
-                  <div>
-                    <h4 className="text-2xl text-gray-700 dark:text-white">
-                      {name}
-                    </h4>
-                    <span className="block text-sm text-gray-500">{url}</span>
-                  </div>
-                </a>
-              ),
-            )}
+            {[...entries, ...entries].map(({ image, name, url }, i) => (
+              <a
+                href={`https://${url}`}
+                target="_blank"
+                className="pr-10 space-y-6 text-center w-[24rem] relative grayscale-[50%] opacity-90 hover:opacity-100 transition duration-200 hover:grayscale-0"
+              >
+                <Image
+                  src={image}
+                  alt={name}
+                  width={256}
+                  height={288}
+                  className={`mx-auto h-32 w-72 md:h-40 md:w-24 lg:h-32 lg:w-72 rounded-lg object-cover object-top hover:rotate-0 ${
+                    i % 2 === 0 ? '-rotate-1' : 'rotate-1'
+                  } hover:scale-110 hover:shadow-lg lg:hover:shadow-2xl transition`}
+                />
+                <div>
+                  <h4 className="text-2xl text-gray-700 dark:text-white">
+                    {name}
+                  </h4>
+                  <span className="block text-sm text-gray-500">{url}</span>
+                </div>
+              </a>
+            ))}
           </div>
         </div>
 
