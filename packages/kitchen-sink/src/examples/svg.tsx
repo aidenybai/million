dd8efcbc789f48f2b72551e66c6b536972c0a008
Aiden Bai <aiden.bai05@gmail.com>
2023-08-26 23:47:22 -0700

    hotfix: undefined portals

---
+++ packages/kitchen-sink/src/examples/svg.tsx
@@ -0,0 +1,24 @@
+import { block } from 'million/react';
+
+const Svg = block(() => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.5 12.75l6 6 9-13.5"
+        />
+      </svg>
+    </div>
+  );
+});
+
+export default Svg;
