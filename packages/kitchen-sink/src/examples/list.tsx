2b84d85159e64f6358f2945ac601e14f7442c562
Aiden Bai <aiden.bai05@gmail.com>
2023-08-31 14:21:12 -0700

    fix: spread attributes (#565)

--- packages/kitchen-sink/src/examples/list.tsx
+++ packages/kitchen-sink/src/examples/list.tsx
@@ -1,10 +1,15 @@
 import { block, For } from 'million/react';
 
 const List = block(() => {
+  const items = { id: 'test' };
   return (
     <ul>
       <For each={[1, 2, 3, 4, 5]}>
-        {(item) => <li key={item}>Item {item}</li>}
+        {(item) => (
+          <li key={item} {...items}>
+            Item {item}
+          </li>
+        )}
       </For>
     </ul>
   );
