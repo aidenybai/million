6f80f257823e05d8c30392cf96bf3c2555767fd9
Aiden Bai <aiden.bai05@gmail.com>
2023-09-02 17:01:56 -0700

    docs: update showcase

--- website/components/home/hero.tsx
+++ website/components/home/hero.tsx
@@ -2,6 +2,8 @@ import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import Image from 'next/image';
 import TextsLogo from '../../public/texts.webp';
+import HackClubLogo from '../../public/hackclub.svg';
+import MetamaskLogo from '../../public/metamask.svg';
 import { Container } from './container';
 import { ShimmerButton } from './shimmer-button';
 
@@ -84,15 +86,28 @@ export function Companies() {
       component: <WyzeLogo />,
     },
     {
-      url: 'https://vercel.com',
-      component: <VercelLogo />,
-    },
-    {
-      url: 'https://dimension.dev',
+      url: 'https://metamask.io',
       component: (
         <div className="flex items-center gap-3 text-xl font-semibold">
-          <DimensionLogo /> Dimension
+          <Image
+            src={MetamaskLogo as string}
+            width={30}
+            height={30}
+            alt="Metamask"
+          />{' '}
+          Metamask
         </div>
+      ),
+    },
+    {
+      url: 'https://hackclub.com',
+      component: (
+        <Image
+          src={HackClubLogo as string}
+          width={90}
+          height={30}
+          alt="Hack Club"
+        />
       ),
     },
     {
@@ -123,7 +138,11 @@ export function Companies() {
         <div className="slide-track-5 hover:pause mt-6 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 justify-around items-center">
           {[...entries, ...entries].map(({ component, url }) => (
             <div className="w-[12rem] relative grayscale opacity-60 hover:opacity-100 transition duration-200 hover:grayscale-0">
-              <a href={url} target="_blank">
+              <a
+                href={url}
+                target="_blank"
+                className="flex justify-center w-full"
+              >
                 {component}
               </a>
             </div>
@@ -185,18 +204,6 @@ function WyzeLogo() {
   );
 }
 
-function DimensionLogo() {
-  return (
-    <Image
-      src="https://beta.dimension.dev/logo.svg"
-      alt="Dimension"
-      className="dark:invert"
-      width={25}
-      height={33.13}
-    />
-  );
-}
-
 function LLMReportLogo() {
   return (
     <svg
@@ -210,23 +217,6 @@ function LLMReportLogo() {
         d="M3 6a3 3 0 013-3h12a3 3 0 013 3v12a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm4.5 7.5a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0v-2.25a.75.75 0 01.75-.75zm3.75-1.5a.75.75 0 00-1.5 0v4.5a.75.75 0 001.5 0V12zm2.25-3a.75.75 0 01.75.75v6.75a.75.75 0 01-1.5 0V9.75A.75.75 0 0113.5 9zm3.75-1.5a.75.75 0 00-1.5 0v9a.75.75 0 001.5 0v-9z"
         clipRule="evenodd"
       />
-    </svg>
-  );
-}
-
-function VercelLogo() {
-  return (
-    <svg
-      aria-label="Vercel logotype"
-      height="64"
-      role="img"
-      className="h-6 dark:invert"
-      viewBox="0 0 283 64"
-    >
-      <path
-        d="M141.68 16.25c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zm117.14-14.5c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zm-39.03 3.5c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9v-46h9zM37.59.25l36.95 64H.64l36.95-64zm92.38 5l-27.71 48-27.71-48h10.39l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10v14.8h-9v-34h9v9.2c0-5.08 5.91-9.2 13.2-9.2z"
-        fill="black"
-      ></path>
     </svg>
   );
 }
