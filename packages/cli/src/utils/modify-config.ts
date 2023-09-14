93f64cb357e3db004e5ff4e24916f82cf6030943
KunalVerma2468 <92316166+kunal00000@users.noreply.github.com>
2023-09-15 01:00:19 +0530

    wsBcBAABCAAQBQJlA19LCRBK7hj4Ov3rIwAAFK8IAC2OF9+lTc3EziGpg00ggVOL  JLLOFhAq2xoqvCdK99+noBk3ctwmHrpD/vEvQBZQ1im9WseWzfm+lEgd44FSy5kV  MPavyhAkC1MvTjQDowVNmA8mzqfvwl5eoKhapzsCRQpgK3n1PhL1AvkesopJCjsx  nPARfz+oqA//Q6W6hnxqaUEuFcj7TfJWGAoQs2LHFcIhQXjmVLg+RmjrqutHSgMs  XaqwyodKrZCiXJTeBk6DvKmrU0LgdFejBeGg5D8RVWRxQpZ365gm6yViSs+ZYw+6  VbByLhLjwJoo277DOw2VQkGRZwdezA1gHrk7y/kYDtiWS7pnGcjAPRiZSCln1KU=  =aYxq  -----END PGP SIGNATURE-----
    feat: million cli (#572)

---
+++ packages/cli/src/utils/modify-config.ts
@@ -0,0 +1,231 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import chalk from 'chalk';
+import { abort, getNextRouter, highlightCodeDifferences } from './utils';
+import type { BuildTool } from '../types';
+
+export async function modifyConfigFile(detectedBuildTool: BuildTool) {
+  try {
+    let configFileContent = await getExistingConfigContent(detectedBuildTool);
+    const oldCode = configFileContent;
+    const filePath = path.join(process.cwd(), detectedBuildTool.configFilePath);
+    /**
+     * If the config file already has million js configured, abort
+     */
+    configFileContent.includes('million') ||
+    configFileContent.includes('million/compiler')
+      ? abort(
+          `${chalk.red(
+            `Million js already configured in ${detectedBuildTool.configFilePath}.`,
+          )}.\n Please refer docs: https://million.dev/docs/install#use-the-compiler if facing any errors.`,
+        )
+      : '';
+
+    const detectedModuleType: 'esm' | 'cjs' | 'unknown' =
+      detectModuleType(configFileContent);
+    /**
+     * 1. Add import or require command for million to the top of the file
+     * 2. Update 'export default' or 'module.exports'
+     * 3. Update file with new code
+     */
+    if (detectedModuleType === 'cjs') {
+      // 1.
+      const importStatement = `const million = require('million/compiler')\n`;
+      configFileContent = importStatement + configFileContent;
+
+      // 2.
+      const regex = /module\.exports\s*=\s*(?<exportContent>[^;]+)/;
+      const match = regex.exec(configFileContent);
+
+      if (match) {
+        const oldExportExpression = match[1];
+        const newExportExpression = await wrapExportCode(
+          detectedBuildTool,
+          oldExportExpression!,
+        );
+
+        // 3.
+        const newCode = configFileContent.replace(
+          regex,
+          `module.exports = ${newExportExpression}`,
+        );
+
+        await fs.promises.writeFile(filePath, newCode, {
+          encoding: 'utf-8',
+          flag: 'w',
+        });
+
+        highlightCodeDifferences(oldCode, newCode, detectedBuildTool);
+      }
+    } else if (detectedModuleType === 'esm') {
+      // 1.
+      const importStatement = `import million from 'million/compiler'\n`;
+      configFileContent = importStatement + configFileContent;
+
+      // 2.
+      const regex = /export\s+default\s+(?<exportContent>[^;]+)/;
+      const match = regex.exec(configFileContent);
+
+      if (match) {
+        const oldExportExpression = match[1];
+        const newExportExpression = await wrapExportCode(
+          detectedBuildTool,
+          oldExportExpression!,
+        );
+
+        // 3.
+        const newCode = configFileContent.replace(
+          regex,
+          `export default ${newExportExpression}`,
+        );
+
+        await fs.promises.writeFile(filePath, newCode, {
+          encoding: 'utf-8',
+          flag: 'w',
+        });
+
+        highlightCodeDifferences(oldCode, newCode, detectedBuildTool);
+      }
+    } else {
+      /**
+       *  If we can't detect the module type, we can't modify the config file
+       *  Refer user to read installation docs
+       * */
+
+      abort(
+        `${chalk.yellow(
+          `Could not detect module type for ${detectedBuildTool.configFilePath}.`,
+        )}\nPlease refer docs:${chalk.cyan(
+          'https://million.dev/docs/install#use-the-compiler',
+        )} to setup manually.`,
+      );
+    }
+  } catch (e) {
+    return abort();
+  }
+}
+
+export async function getExistingConfigContent(
+  detectedBuildTool: BuildTool,
+): Promise<string> {
+  const configFilePath = path.join(
+    process.cwd(),
+    detectedBuildTool.configFilePath,
+  );
+
+  // Read the config file
+  const configContent = await fs.promises.readFile(configFilePath, {
+    encoding: 'utf-8',
+  });
+  return configContent;
+}
+
+function detectModuleType(fileContent: string): 'cjs' | 'esm' | 'unknown' {
+  // Check for CommonJS require/module.exports patterns
+  if (fileContent.includes('module.exports =')) {
+    return 'cjs';
+  }
+
+  // Check for ESM import/export statements
+  if (fileContent.includes('export default')) {
+    return 'esm';
+  }
+
+  return 'unknown';
+}
+
+async function wrapExportCode(
+  buildtool: BuildTool,
+  oldExportExpression: string,
+): Promise<string> {
+  let [firstPart, ...rest]: string[] = [];
+
+  switch (buildtool.name) {
+    case 'next':
+      /**
+       * million.next(nextConfig, millionConfig);
+       * */
+      return handleNextCase(oldExportExpression);
+    case 'vite':
+      /**
+       * defineConfig({
+       *   plugins: [million.vite({ auto: true }), react(), ],
+       * });
+       */
+      [firstPart, ...rest] = oldExportExpression.split('plugins: [');
+      return `${firstPart!}plugins: [million.vite({ auto: true }), ${rest.join(
+        'plugins: [',
+      )}`;
+
+    case 'astro':
+      /**
+       * defineConfig({
+       *   vite: {
+       *     plugins: [million.vite({ mode: 'react', server: true, auto: true }), ]
+       *   }
+       * });
+       */
+      [firstPart, ...rest] = oldExportExpression.split('plugins: [');
+      return `${firstPart!}plugins: [million.vite({ mode: 'react', server: true, auto: true }), ${rest.join(
+        'plugins: [',
+      )}`;
+
+    case 'gatsby':
+      /**
+       * ({ actions }) => {
+       *   actions.setWebpackConfig({
+       *     plugins: [million.webpack({ mode: 'react', server: true, auto: true }), ],
+       *   })
+       * }
+       */
+      [firstPart, ...rest] = oldExportExpression.split('plugins: [');
+      return `${firstPart!}[plugins: million.webpack({ mode: 'react', server: true, auto: true }), ${rest.join(
+        'plugins: [',
+      )}`;
+    case 'craco':
+      /**
+       * {
+       *   webpack: {
+       *     plugins: { add: [million.webpack({ auto: true }), ] }
+       *   },
+       * }
+       */
+      [firstPart, ...rest] = oldExportExpression.split('plugins: [');
+      return `${firstPart!}plugins: [million.webpack({ auto: true }), ${rest.join(
+        'plugins: [',
+      )}`;
+
+    case 'webpack':
+      /**
+       * {
+       *   plugins: [million.webpack({ auto: true }), ],
+       * }
+       */
+      [firstPart, ...rest] = oldExportExpression.split('plugins: [');
+      return `${firstPart!}plugins: [million.webpack({ auto: true }), ${rest.join(
+        'plugins: [',
+      )}`;
+
+    case 'rollup':
+      /**
+       * {
+       *   plugins: [million.rollup({ auto: true }), ],
+       * }
+       */
+      [firstPart, ...rest] = oldExportExpression.split('plugins: [');
+      return `${firstPart!}plugins: [million.rollup({ auto: true }), ${rest.join(
+        'plugins: [',
+      )}`;
+    default:
+      return '';
+  }
+}
+
+async function handleNextCase(oldExportExpression: string): Promise<string> {
+  const nextRouter: 'app' | 'pages' = await getNextRouter();
+  return `million.next(
+  ${oldExportExpression}, { auto: ${
+    nextRouter === 'app' ? '{ rsc: true }' : 'true'
+  }}
+)`;
+}
---
+++ packages/cli/src/utils/package-json.ts
@@ -0,0 +1,43 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as clack from '@clack/prompts';
+import { abort } from './utils';
+import type { PackageDotJson } from '../types';
+
+/**
+ * Get the package.json dependencies and devDependencies
+ */
+export async function getPackageDotJson(): Promise<PackageDotJson> {
+  const packageJsonFileContents = await fs.promises
+    .readFile(path.join(process.cwd(), 'package.json'), 'utf8')
+    .catch(() => {
+      clack.log.error(
+        'Could not find package.json. Make sure to run the wizard in the root of your app!',
+      );
+      return abort();
+    });
+
+  let packageJson: PackageDotJson | undefined;
+
+  try {
+    packageJson = JSON.parse(packageJsonFileContents);
+  } catch {
+    clack.log.error(
+      'Unable to parse your package.json. Make sure it has a valid format!',
+    );
+    abort();
+  }
+
+  return packageJson || {};
+}
+
+/**
+ * Check if the million package is installed (either present in dependencies or devDependencies)
+ */
+export async function isPackageInstalled(): Promise<boolean> {
+  const packageJson = await getPackageDotJson();
+  if (packageJson.dependencies?.million) {
+    return true;
+  }
+  return false;
+}
