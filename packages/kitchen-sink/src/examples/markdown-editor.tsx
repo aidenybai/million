c325926e7ce4916f070fae0ae776f8ed1b3d5a3e
cyberviking5 <aviral.hatwal@gmail.com>
2023-10-03 00:54:07 +0530

    changes

--- packages/kitchen-sink/src/examples/markdown-editor.tsx
+++ packages/kitchen-sink/src/examples/markdown-editor.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react';
 import styled from 'styled-components';
 import { block } from 'million/react';
-import {marked} from 'marked'
+import { marked } from 'marked';
 
 export const ColumnFlex = styled.div`
   display: flex;
@@ -13,34 +13,32 @@ export const RowFlex = styled.div`
   flex-direction: row;
 `;
 
+type Editing = {
+  markdownContent: string;
+};
 
-type Editing={
-    markdownContent: string;
-}
-
-const Preview=block(({markdownContent}:Editing)=>{
+const Preview = block(({ markdownContent }: Editing) => {
   const mardownFormattedContent = marked(markdownContent);
-    return(
-        <>
-        <ColumnFlex
-            id="preview"
-            style={{
-            flex: "1",
-            padding: "16px",
-            }}
-        >
-            <h2>Preview</h2>
-            <div
-            dangerouslySetInnerHTML={{__html: mardownFormattedContent}}
-            >
-            </div>
-        </ColumnFlex>
-      </>
-    )
-})
+  return (
+    <>
+      <ColumnFlex
+        id="preview"
+        style={{
+          flex: '1',
+          padding: '16px',
+        }}
+      >
+        <h2>Preview</h2>
+        <div
+          dangerouslySetInnerHTML={{ __html: mardownFormattedContent }}
+        ></div>
+      </ColumnFlex>
+    </>
+  );
+});
 
 export default function MarkdownEditor() {
-    const [markdownContent, setMarkdownContent] = useState<string>(`
+  const [markdownContent, setMarkdownContent] = useState<string>(`
     # H1
     ## H2
     ### H3
@@ -51,30 +49,42 @@ export default function MarkdownEditor() {
     **bold**
     _italic_
     `);
-    const handleChange=(e: React.ChangeEvent<HTMLTextAreaElement>) => {setMarkdownContent(e.target.value)}
-  return( <div>
-    <RowFlex
-      style={{
-        padding: "32px",
-        paddingTop: "0px",
-        height: "60vh",
-        background:"black",
-        color:"white",
-        }}>
-        <ColumnFlex
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdownContent(e.target.value);
+  };
+  return (
+    <div>
+      <RowFlex
         style={{
-            flex: "1",
-            padding: "16px",
-          }}>
-        <h2>
-        Editor
-        </h2>
-        <textarea style={{height:"50vh",borderRadius:"10px",border:"2px grey",color:"black",
-        fontSize:"15px",padding:"20px"}}
-          onChange={handleChange} value={markdownContent}
+          padding: '32px',
+          paddingTop: '0px',
+          height: '60vh',
+          background: 'black',
+          color: 'white',
+        }}
+      >
+        <ColumnFlex
+          style={{
+            flex: '1',
+            padding: '16px',
+          }}
+        >
+          <h2>Editor</h2>
+          <textarea
+            style={{
+              height: '50vh',
+              borderRadius: '10px',
+              border: '2px grey',
+              color: 'black',
+              fontSize: '15px',
+              padding: '20px',
+            }}
+            onChange={handleChange}
+            value={markdownContent}
           />
-          </ColumnFlex>
-      <Preview markdownContent={markdownContent}/> 
-    </RowFlex>
-  </div>);
+        </ColumnFlex>
+        <Preview markdownContent={markdownContent} />
+      </RowFlex>
+    </div>
+  );
 }
