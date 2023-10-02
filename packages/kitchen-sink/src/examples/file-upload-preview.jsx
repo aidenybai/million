a5871acd22276718dae1c313239072cc122c7e02
darshbaxi <baxidarsh@gmail.com>
2023-10-02 21:06:12 +0530

    file upload and preview

--- packages/kitchen-sink/src/examples/file-upload-preview.jsx
+++ packages/kitchen-sink/src/examples/file-upload-preview.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 import { block } from 'million/react';
-const FileUpload=block(({file})=>{
-  return(
-    <div className='preview' >
-    <img src={file} alt='file-uploaded'/>
-    </div>
-  )
-})
+
 export default function File() {
   const [file, setFile] = useState();
   function getFile(event) {
     setFile(URL.createObjectURL(event.target.files[0]));
   }
+
+  const FileUpload=block(({file})=>{
+    console.log(file)
+    return(
+      <div className='preview' >
+      <img src={file} />
+      </div>
+    )
+  })
   return (
     <div >
         <div>
          <input type="file" onChange={getFile}></input> 
         </div>
          <div style={{height:200,width:200,padding:10}}>
-         {file && <FileUpload file={file}/>}
+         <FileUpload file={file}/>
          </div>
     </div>
   );
