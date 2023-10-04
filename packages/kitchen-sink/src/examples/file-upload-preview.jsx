579d7e96b19a1afaa27ccccdb5452f8fae7dbc21
Darsh Baxi <119887723+darshbaxi@users.noreply.github.com>
2023-10-05 03:38:51 +0530

    wsBcBAABCAAQBQJlHeJzCRBK7hj4Ov3rIwAAlWUIAENZRDsv2KN2dLcAlHwYHnGJ  uTkPT+FCv4oa9iIj8HoYzaZS7mDJdJkxjcyEZfek14glLBum8/ygqpCFvY9fHT8N  BKsd9bEDMsYaKBB8BIocgQgj1E+M8eqCefgz6Bkm1lKk/RlwGkdq3vPTZKNl/0/c  Wx/Q2vFi8ePAP/4FfLJC7G31XTeQsFPZ1FFo9tFkWx5iEhU//z8jIYP+OavxmrHI  SmouTV/nJ3kQJy04GY6TGEMkMIvdFUWxafPYzHnftKbo0gsLHMQ/+uEldE3Bqic0  GsOUu6K2d8Ze2oD1fYxLfAXaEnNQrZn3yGkBBKDbp+BQwql2JBcVEkZlqpwwb1Y=  =4URs  -----END PGP SIGNATURE-----
    File Upload and Preview (#637)
    
    * file upload and preview
    
    * fixes alt attribute and logging to console
    
    * requirement to run cleanup command satisfy
    
    * bug fixes

---
+++ packages/kitchen-sink/src/examples/file-upload-preview.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { block } from 'million/react';
+const FileUpload=block(({file})=>{
+  return(
+    <div className='preview' >
+    <img src={file} alt='file-uploaded'/>
+    </div>
+  )
+})
+export default function File() {
+  const [file, setFile] = useState();
+  function getFile(event) {
+    setFile(URL.createObjectURL(event.target.files[0]));
+  }
+  return (
+    <div >
+        <div>
+         <input type="file" onChange={getFile}></input> 
+        </div>
+         <div style={{height:200,width:200,padding:10}}>
+         {file && <FileUpload file={file}/>}
+         </div>
+    </div>
+  );
+}
