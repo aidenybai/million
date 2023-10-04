5b29e400d06e4e4b7056821bf4ddc6527f8b008e
Darsh Baxi <119887723+darshbaxi@users.noreply.github.com>
2023-10-05 03:38:51 +0530

    File Upload and Preview (#637)
    * file upload and preview
    
    * fixes alt attribute and logging to console
    
    * requirement to run cleanup command satisfy
    
    * bug fixes

--- website/components/million-library/jsx-runtime.d.ts
+++ website/components/million-library/jsx-runtime.d.ts
@@ -1,5 +1,9 @@
 import { P as Props, V as VNode } from './types-35702ad2.js';
 
-declare const h: (type: string, props?: Props | null, ...children: VNode[]) => VNode;
+declare const h: (
+  type: string,
+  props?: Props | null,
+  ...children: VNode[]
+) => VNode;
 
 export { h as createElement, h, h as jsx, h as jsxs };
