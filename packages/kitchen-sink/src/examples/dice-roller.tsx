09968aaf40985b6c3d0ab2e1b303c8ec22764fec
Uzair <s.uzairnoman@gmail.com>
2023-10-06 00:58:39 +0200

    feat: prettier

--- packages/kitchen-sink/src/examples/dice-roller.tsx
+++ packages/kitchen-sink/src/examples/dice-roller.tsx
@@ -2,31 +2,31 @@ import React, { useState } from 'react';
 import { block } from 'million/react';
 
 const DiceRoller: React.FC = block(() => {
-    const [result, setResult] = useState<number | null>(null);
-    const [rolling, setRolling] = useState<boolean>(false);
-  
-    const rollDice = () => {
-      if (!rolling) {
-        setRolling(true);
-  
-        const min = 1;
-        const max = 6;
-        const randomResult = Math.floor(Math.random() * (max - min + 1)) + min;
-  
-        setTimeout(() => {
-          setResult(randomResult);
-          setRolling(false);
-        }, 1000); // Adjust the timeout duration if needed
-      }
-    };
-  
-    return (
-      <div className={`dice-roller ${rolling ? 'roll-animation' : ''}`}>
-        <h1>Virtual Dice Roller</h1>
-        <button onClick={rollDice}>Roll Dice</button>
-        {result !== null && <p>Result: {result}</p>}
-      </div>
-    );
-  });
-  
-  export default DiceRoller;
+  const [result, setResult] = useState<number | null>(null);
+  const [rolling, setRolling] = useState<boolean>(false);
+
+  const rollDice = () => {
+    if (!rolling) {
+      setRolling(true);
+
+      const min = 1;
+      const max = 6;
+      const randomResult = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      setTimeout(() => {
+        setResult(randomResult);
+        setRolling(false);
+      }, 1000); // Adjust the timeout duration if needed
+    }
+  };
+
+  return (
+    <div className={`dice-roller ${rolling ? 'roll-animation' : ''}`}>
+      <h1>Virtual Dice Roller</h1>
+      <button onClick={rollDice}>Roll Dice</button>
+      {result !== null && <p>Result: {result}</p>}
+    </div>
+  );
+});
+
+export default DiceRoller;
