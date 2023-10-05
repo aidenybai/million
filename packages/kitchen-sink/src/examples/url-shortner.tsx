4178ab354366da7d0453d3a859a8475c515a15bf
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-10-06 02:35:24 +0530

    Modified url-shortner component

--- packages/kitchen-sink/src/examples/url-shortner.tsx
+++ packages/kitchen-sink/src/examples/url-shortner.tsx
@@ -1,49 +1,52 @@
+import { block } from 'million/react';
 import { useState } from 'react';
 
-function App() {
-  const [url, setUrl] = useState()
-  const [shortendUrl, setShortenedUrl] = useState('')
+const UrlShortener = block(() => {
+  const [url, setUrl] = useState('');
+  const [shortenedUrl, setShortenedUrl] = useState('');
 
   const shortenUrl = async (e) => {
     e.preventDefault();
     try {
       const response = await fetch(
         `https://api.shrtco.de/v2/shorten?url=${url}`
-      )
-      const data = await response.json()
+      );
+      const data = await response.json();
       setShortenedUrl(data.result.full_short_link);
     } catch (e) {
       alert(e);
     }
   };
+
   const redirectToUrl = () => {
-    if (shortendUrl) {
-      window.open(shortendUrl, '_blank');
+    if (shortenedUrl) {
+      window.open(shortenedUrl, '_blank');
     }
   };
-  
+
   return (
     <div className="app">
-      <div className='shortener'>
-        <h2>URL shortener</h2>
+      <div className="shortener">
+        <h2>URL Shortener</h2>
         <form onSubmit={shortenUrl}>
           <input
-            placeholder='Enter URL'
+            placeholder="Enter URL"
             value={url}
-            onChange={(e) => setUrl(e.target.value)}/>
-          <button>Submit</button>
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button type="submit">Submit</button>
         </form>
-        {shortendUrl &&
-        <div>
-        <p>Shortened URL:</p>
-        <a href={shortendUrl} onClick={redirectToUrl}>
-          {shortendUrl}
-        </a>
-      </div>
-        }
+        {shortenedUrl && (
+          <div>
+            <p>Shortened URL:</p>
+            <a href={shortenedUrl} onClick={redirectToUrl}>
+              {shortenedUrl}
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+});
 
-export default App;  
+export default UrlShortener;
