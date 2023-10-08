c87f3e3e805711b9f518f9c04a751703563ec2f2
capsy14 <kartikbhattkg@gmail.com>
2023-10-08 19:10:05 +0530

    Added github user search

---
+++ packages/kitchen-sink/src/examples/GithubUserSearch.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const GithubUserSearch = () => {
+    const [userData, setUserData] = useState('');
+    const [user, setUser] = useState('');
+
+    const getUserDetails = (user) => {
+        const apiURL = 'https://api.github.com/users/' + user;
+        axios
+            .get(apiURL)
+            .then((response) => {
+                console.log(response.data);
+                setUserData(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const userNameHandler = (e) => {
+        setUser(e.target.value);
+    };
+
+    const search = () => {
+        getUserDetails(user);
+    };
+
+    useEffect(() => {
+        getUserDetails('capsy14');
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="kl">
+                <div className="input-container">
+                    <input
+                        type="text"
+                        className="input-field"
+                        onChange={userNameHandler}
+                        value={user}
+                        placeholder="Enter a GitHub username"
+                    />
+                    <button className="search-button" onClick={search}>
+                        Search
+                    </button>
+                </div>
+                <div className="user-details">
+                    <h2 className="username">UserName: {userData.login}</h2>
+                    <h2 className="name">Name: {userData.name}</h2>
+                    <h2 className="bio">{userData.bio}</h2>
+                    <h2 className="location">Location: {userData.location}</h2>
+                    <h3>Following: {userData.following}</h3>
+                    <h3>Followers: {userData.followers}</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GithubUserSearch;
