d157a47e2cb89acae999422ed2e92a3006087aff
Bharath-KumarReddy <127650446+Bharath-KumarReddy@users.noreply.github.com>
2023-10-10 18:13:26 +0530

    Update recognise_speech.jsx

--- packages/kitchen-sink/src/examples/recognise_speech.jsx
+++ packages/kitchen-sink/src/examples/recognise_speech.jsx
@@ -1,53 +1,58 @@
+import { block, Page } from 'million';
 import React, { useState } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import { block } from 'million/react';
 
-const App = () => {
-  const [textToCopy, setTextToCopy] = useState('');
+const SpeechRecognitionBlock = block(function SpeechRecognitionComponent() {
+  const [textToCopy, setTextToCopy] = useState();
   const [isCopied, setCopied] = useState(false);
 
   const startListening = () => {
+     SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
     
-    SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
   };
 
   const stopListening = () => {
-    
-    SpeechRecognition.stopListening();
+  SpeechRecognition.stopListening();
   };
 
- 
+  
   const transcript = 'This is a sample transcript';
 
-  
-  const { transcript: speechTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
-
-
-  if (speechTranscript !== textToCopy) {
-    setTextToCopy(speechTranscript);
-  }
-
   return (
-    <>
-      <div className="container">
-        <h2>Speech Detector</h2>
-        <br />
-        <p>Start speaking, and your speech is translated into words and displayed dynamically!!</p>
+    <div className="container">
+      <h2>Speech Detector</h2>
+      <br />
+      <p>Start speaking and your speech is translated into words and displayed dynamically!!</p>
 
-        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
-          {textToCopy}
-        </div>
-
-        <div className="btn-style">
-          <button onClick={() => setCopied(!isCopied)}>
-            {isCopied ? 'Copied!' : 'Copy to clipboard'}
-          </button>
-          <button onClick={startListening}>Start Speaking</button>
-          <button onClick={stopListening}>Stop Speaking</button>
-        </div>
+      <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+        {transcript}
       </div>
-    </>
-  );
-};
 
-export default block(App);
+      <div className="btn-style">
+        <button onClick={() => setCopied(!isCopied)}>
+          {isCopied ? 'Copied!' : 'Copy to clipboard'}
+        </button>
+        <button onClick={startListening}>Start Speaking</button>
+        <button onClick={stopListening}>Stop Speaking</button>
+      </div>
+    </div>
+  );
+});
+
+// Export your Million.js block component
+export default SpeechRecognitionBlock;
+
+
+import { render } from 'million';
+import SpeechRecognitionBlock from './SpeechRecognitionBlock'; 
+
+function App() {
+  return (
+    <Page>
+      <h1>Welcome to My Million.js App</h1>
+      <SpeechRecognitionBlock /> {/* Use the SpeechRecognitionBlock component */}
+    </Page>
+  );
+}
+
+
+render(<App />);
