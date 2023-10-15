9e802aa2989c4bb0b13558990a575877a9928539
Travis Bennett <t_bennett_a@protonmail.com>
2023-10-15 15:29:33 -0400

    Functionality for all modes complete

--- packages/kitchen-sink/src/examples/virtualWhiteboard.tsx
+++ packages/kitchen-sink/src/examples/virtualWhiteboard.tsx
@@ -1,18 +1,28 @@
 import React, { useRef, useEffect, useState } from 'react';
 
 const VirtualBoard: React.FC = () => {
-    const [coordinates, setCoordinates] = useState<
+
+// Enum responsible for defining modes
+enum Mode {
+    Draw = 'draw',
+    Text = 'text',
+    Line = 'line',
+    Erase = 'erase',
+    Clear = 'clear'
+}
+
+type CoordinateType = Array<{points:{ x: number, y: number}[]; color: string}>;
+  
+const [coordinates, setCoordinates] = useState<
     Array<{ points: { x: number; y: number }[]; color: string }>
   >([]);
-  
+
   // Maintains the current lines state with x, y coordinates
   const [currentLine, setCurrentLine] = useState<{ x: number; y: number }[]>(
     [],
   );
   const [isDrawing, setIsDrawing] = useState(false); // New state to track drawing mode
-  const [mode, setMode] = useState<
-    'draw' | 'text' | 'line' | 'erase' | 'clear'
-  >('draw');
+  const [mode, setMode] = useState< Mode | null>(Mode.Draw)
   const [color, setColor] = useState<string>('#000000');
   // Responsible for controlling position and visibility of the text input field
   const [textInput, setTextInput] = useState<{
@@ -30,13 +40,51 @@ const VirtualBoard: React.FC = () => {
   const [startPoint, setStartPoint] = useState<{ x: number; y: number } | null>(
     null,
   );
+  const [mousePosition, setMousePosition] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
+  const [showEraserPreview, setShowEraserPreview] = useState(false);
+  const [undoStack, setUndoStack] = useState<CoordinateType[]>([]);
+  const [redoStack, setRedoStack] = useState<CoordinateType[]>([]);
 
-  const colorInputRef = useRef<HTMLInputElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const textInputRef = useRef<HTMLInputElement>(null);
 
+  const handleChange = () => {
+    setUndoStack([...undoStack, coordinates]);
+    setRedoStack([]);
+  };
+
+  const handleUndo = () => {
+    if (undoStack.length > 0) {
+      const newUndoStack = [...undoStack];
+      const lastState = newUndoStack.pop();
+      
+      const newRedoStack = [...redoStack, coordinates];
+      
+      setCoordinates(lastState || []);
+      setUndoStack(newUndoStack);
+      setRedoStack(newRedoStack);
+    }
+  };
+  
+  const handleRedo = () => {
+    if (redoStack.length > 0) {
+      const newRedoStack = [...redoStack];
+      const nextState = newRedoStack.pop();
+      
+      const newUndoStack = [...undoStack, coordinates];
+      
+      setCoordinates(nextState || []);
+      setUndoStack(newUndoStack);
+      setRedoStack(newRedoStack);
+    }
+  };
+  
+
   const handleModeChange = (
-    newMode: 'draw' | 'text' | 'line' | 'erase' | 'clear',
+    newMode: Mode
   ) => {
     setMode(newMode);
   };
@@ -60,7 +108,10 @@ const VirtualBoard: React.FC = () => {
         setStartPoint({ x, y });
         setIsDrawing(true);
       } else {
-        setCoordinates([...coordinates, { points: [startPoint, { x, y }], color }]);
+        setCoordinates([
+          ...coordinates,
+          { points: [startPoint, { x, y }], color },
+        ]);
 
         setStartPoint(null); // Reset the starting point immediately
         setIsDrawing(false); // Stop drawing the preview line
@@ -69,21 +120,17 @@ const VirtualBoard: React.FC = () => {
       setIsDrawing(true);
     }
     setCurrentLine([]);
-    console.log('MouseDown:', startPoint, isDrawing);
   };
 
-  const handleMouseUp = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  const handleMouseUp = (e:React.MouseEvent<HTMLCanvasElement>) => {
     const newLine = { points: currentLine, color: color };
     const newCoordinates = [...coordinates, newLine];
     if (mode !== 'line') {
       setIsDrawing(false);
       setCoordinates(newCoordinates);
       setCurrentLine([]);
+      handleChange();
     }
-    console.log('MouseUp:', startPoint, isDrawing);
-    console.log('MouseUp: Coordinates', coordinates);
-    console.log('MouseUp: newCoordinates', newCoordinates);
-    console.log('MouseUp: Current Line', currentLine);
   };
 
   const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
@@ -92,7 +139,6 @@ const VirtualBoard: React.FC = () => {
       const x = e.clientX - rect.left;
       const y = e.clientY - rect.top;
       setCurrentLine([{ x, y }]);
-      console.log('handleMouseMove:', currentLine);
     }
   };
 
@@ -102,13 +148,25 @@ const VirtualBoard: React.FC = () => {
     }
     if (mode === 'erase') {
       handleErase(e);
+      handleEraserPreview(e);
     }
     if (mode === 'line') {
       handleMouseMove(e);
     }
   };
 
+  const handleEraserPreview = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setMousePosition({ x, y });
+  };
+
   const handleText = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (mode === 'erase') {
+      setMode(null);
+      return;
+    }
     if (mode !== 'text') return; // Only add text when the mode is 'text'
 
     const rect = e.currentTarget.getBoundingClientRect();
@@ -146,6 +204,7 @@ const VirtualBoard: React.FC = () => {
 
   const handleErase = (e: React.MouseEvent<HTMLCanvasElement>) => {
     if (mode !== 'erase') return;
+    setShowEraserPreview(true);
 
     const distance = (x1: number, y1: number, x2: number, y2: number) =>
       Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
@@ -175,7 +234,7 @@ const VirtualBoard: React.FC = () => {
           const secondHalf = lineData.points.slice(closestIndex + 1);
           return [
             { points: firstHalf, color: lineData.color },
-            { points: secondHalf, color: lineData.color }
+            { points: secondHalf, color: lineData.color },
           ];
         }
 
@@ -212,29 +271,20 @@ const VirtualBoard: React.FC = () => {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw eraser preview if mode is 'erase'
-    if (mode === 'erase' && currentLine.length > 0) {
-      const lastPoint = currentLine[currentLine.length - 1];
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'; // semi-transparent black
-      ctx.beginPath();
-      ctx.arc(lastPoint.x, lastPoint.y, 20 /* radius */, 0, Math.PI * 2);
-      ctx.fill();
-    }
-
     // Draw the lines from the coordinates state
     coordinates.forEach((lineData) => {
-        ctx.strokeStyle = lineData.color;
-        ctx.beginPath();
-        lineData.points.forEach((point, i) => {
-          if (i === 0) {
-            ctx.moveTo(point.x, point.y);
-          } else {
-            ctx.lineTo(point.x, point.y);
-          }
-        });
-        ctx.stroke();
-        ctx.closePath();
+      ctx.strokeStyle = lineData.color;
+      ctx.beginPath();
+      lineData.points.forEach((point, i) => {
+        if (i === 0) {
+          ctx.moveTo(point.x, point.y);
+        } else {
+          ctx.lineTo(point.x, point.y);
+        }
       });
+      ctx.stroke();
+      ctx.closePath();
+    });
 
     // Draw text elements
     textElements.forEach((textElement) => {
@@ -244,6 +294,7 @@ const VirtualBoard: React.FC = () => {
     });
 
     // Draw the current line
+    ctx.strokeStyle = color;
     ctx.beginPath();
     currentLine.forEach((point, i) => {
       if (i === 0) {
@@ -272,6 +323,19 @@ const VirtualBoard: React.FC = () => {
       ctx.stroke();
       ctx.closePath();
     }
+    // Draw eraser preview if mode is 'erase'
+    if (mode === 'erase' && mousePosition && showEraserPreview) {
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'; // semi-transparent black
+      ctx.beginPath();
+      ctx.arc(
+        mousePosition.x,
+        mousePosition.y,
+        20 /* radius */,
+        0,
+        Math.PI * 2,
+      );
+      ctx.fill();
+    }
   }, [
     coordinates,
     textElements,
@@ -280,17 +344,36 @@ const VirtualBoard: React.FC = () => {
     startPoint,
     isDrawing,
     color,
+    mousePosition,
+    showEraserPreview,
   ]);
+  useEffect(() => {
+    const onClickOutsideCanvas = (e: MouseEvent) => {
+      const canvas = canvasRef.current;
+      if (canvas && !canvas.contains(e.target as Node)) {
+        setShowEraserPreview(false);
+      }
+    };
+
+    document.addEventListener('click', onClickOutsideCanvas);
+
+    return () => {
+      document.removeEventListener('click', onClickOutsideCanvas);
+    };
+  }, []);
 
   return (
     <>
       <div className="toolBar" style={{ marginBottom: '10px' }}>
-        <button onClick={() => handleModeChange('draw')}> Draw </button>
-        <button onClick={() => handleModeChange('text')}> Text </button>
-        <button onClick={() => handleModeChange('line')}> Line </button>
-        <button onClick={() => handleModeChange('erase')}> Erase </button>
+      <input type="color" value={color} onChange={handleColorChange} />
+        <button onClick={() => handleModeChange(Mode.Draw)}> Draw </button>
+        <button onClick={() => handleModeChange(Mode.Text)}> Text </button>
+        <button onClick={() => handleModeChange(Mode.Line)}> Line </button>
+        <button onClick={() => handleModeChange(Mode.Erase)}> Erase </button>
         <button onClick={handleClear}> Clear </button>
-        <input type="color" value={color} onChange={handleColorChange} />
+        
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleRedo}>Redo</button>
       </div>
       <div className="canvas" style={{ position: 'relative' }}>
         <canvas
