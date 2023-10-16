3a73c4903edf89b831cc243c34e0dc19e83a0913
Sheikh Salman <sksalmanmiah@gmail.com>
2023-10-16 18:03:21 +0600

    add mac-widgets example

---
+++ packages/kitchen-sink/src/examples/mac-widgets.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MacWidgets = () => {
+    return (
+    //source: https://github.com/IamSAL/goriberMac
+      <div className='flex justify-center align-middle w-100'><iframe src='https://gmac.vercel.app/' width="100%" height="600px"></iframe></div>
+  )
+}
+
+export default MacWidgets
