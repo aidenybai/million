5cfde3760c90a0c0b86010d6a84313c7db419c08
Drex <davidokunoye003@gmail.com>
2023-10-28 21:33:53 +0100

    completed the revamp

--- packages/kitchen-sink/src/main.tsx
+++ packages/kitchen-sink/src/main.tsx
@@ -6,9 +6,12 @@ import {
   lazy,
   Suspense,
   LazyExoticComponent,
+  useEffect,
 } from 'react';
 import './style.css';
 import { ErrorBoundary } from 'react-error-boundary';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineClose } from 'react-icons/ai';
 
 type Module = { default: ComponentType<any> };
 
@@ -38,12 +41,46 @@ type Module = { default: ComponentType<any> };
 
   function App() {
     const [selected, setSelected] = useState<number>(-1);
+
+    const [sidebarOpened, setSidebarOpened] = useState(false);
+
+    const [isMobile, setIsMobile] = useState(true);
+
+    const [screen, setScreen] = useState<number | null>(null);
+
+    // Setting the Screen Size State whenever the screen is resized
+    useEffect(() => {
+      const handleResize = () => setScreen(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+      handleResize();
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    // Toggling the isMobile State whenever the screensize changes
+    useEffect(() => {
+      if (screen! <= 768) {
+        setIsMobile(true);
+        setSidebarOpened(false);
+      } else {
+        setIsMobile(false);
+        setSidebarOpened(true);
+      }
+    }, [screen]);
+
     const hasSelected = selected >= 0;
 
     const Mod = hasSelected ? loadedModules[selected]! : null;
 
     return (
       <div>
+        {isMobile && (
+          <button
+            className="kitchen_sink-layout-icon"
+            onClick={() => setSidebarOpened(true)}
+          >
+            <RxHamburgerMenu />
+          </button>
+        )}
         <h1>Million.js Kitchen Sink</h1>
         <p>
           Hey! We're actively recruiting cooks 🧑‍🍳 to help assemble a list of
@@ -53,71 +90,90 @@ type Module = { default: ComponentType<any> };
           </a>{' '}
           for details.
         </p>
-        <div
-          style={{
-            display: 'flex',
-            gap: '0.5rem',
-            flexWrap: 'wrap',
-          }}
-        >
-          {modules.map(([key], index) => {
-            return (
-              <button
-                onClick={async () => {
-                  setSelected(index);
-                  loadedModules[index] = lazy(() => modules[index]![1]());
-                }}
-                key={key}
-                disabled={hasSelected && selected === index}
-              >
-                {key
-                  .replace('./examples/', '')
-                  .replace('.tsx', '')
-                  .replace('.jsx', '')}
-              </button>
-            );
-          })}{' '}
-        </div>
-        <details open={hasSelected}>
-          <summary>
-            Currently Selected:{' '}
-            <strong>{hasSelected ? modules[selected]![0] : 'None'}</strong>
-          </summary>
-          <div style={{ padding: '1rem' }}>
-            <ErrorBoundary
-              fallbackRender={({ error }) => (
-                <div
-                  style={{
-                    color: '#ff8080',
-                    background: '#290000',
-                    padding: '1rem',
-                    borderRadius: '1rem',
-                  }}
+
+        <div className="kitchen_sink-layout">
+          <div
+            className={`kitchen_sink-sidebar ${
+              sidebarOpened
+                ? 'kitchen_sink-sidebar_open'
+                : 'kitchen_sink-sidebar_close'
+            }`}
+          >
+            <div className="kitchen_sink-sidebar_container">
+              {isMobile && (
+                <button
+                  className="kitchen_sink-layout-icon"
+                  onClick={() => setSidebarOpened(false)}
                 >
-                  <h2>💥 {error.message}</h2>
-                  <pre>{error.stack}</pre>
-                </div>
+                  <AiOutlineClose />
+                </button>
               )}
-            >
-              {hasSelected && Mod && (
-                <Suspense
-                  fallback={
-                    <div
-                      style={{
-                        opacity: 0.5,
-                      }}
-                    >
-                      <p className="spinner"></p>
-                      Loading module...
-                    </div>
-                  }
-                >
-                  <Mod />
-                </Suspense>
-              )}
-            </ErrorBoundary>
+              {modules.map(([key], index) => {
+                return (
+                  <button
+                    onClick={async () => {
+                      setSelected(index);
+                      loadedModules[index] = lazy(() => modules[index]![1]());
+                    }}
+                    key={key}
+                    disabled={hasSelected && selected === index}
+                  >
+                    {key
+                      .replace('./examples/', '')
+                      .replace('.tsx', '')
+                      .replace('.jsx', '')}
+                  </button>
+                );
+              })}{' '}
+            </div>
           </div>
-        </details>
+          <div className="kitchen_sink-content">
+            <div className="kitchen_sink-content_container">
+              <details open={hasSelected}>
+                <summary>
+                  Currently Selected:{' '}
+                  <strong>
+                    {hasSelected ? modules[selected]![0] : 'None'}
+                  </strong>
+                </summary>
+                <div style={{ padding: '1rem' }}>
+                  <ErrorBoundary
+                    fallbackRender={({ error }) => (
+                      <div
+                        style={{
+                          color: '#ff8080',
+                          background: '#290000',
+                          padding: '1rem',
+                          borderRadius: '1rem',
+                        }}
+                      >
+                        <h2>💥 {error.message}</h2>
+                        <pre>{error.stack}</pre>
+                      </div>
+                    )}
+                  >
+                    {hasSelected && Mod && (
+                      <Suspense
+                        fallback={
+                          <div
+                            style={{
+                              opacity: 0.5,
+                            }}
+                          >
+                            <p className="spinner"></p>
+                            Loading module...
+                          </div>
+                        }
+                      >
+                        <Mod />
+                      </Suspense>
+                    )}
+                  </ErrorBoundary>
+                </div>
+              </details>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
