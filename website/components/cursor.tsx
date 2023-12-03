318a40f702c1bfb863089497739fc8d5652d0fa0
Nisarg Patel <awesomenisarg@gmail.com>
2023-12-03 19:35:03 +0000

    fix: improved cursor (transition/cleanup)

--- website/components/cursor.tsx
+++ website/components/cursor.tsx
@@ -1,28 +1,21 @@
-export function Cursor({
-  color,
-  x,
-  y,
-}: {
-  color: string;
+interface CursorProps {
   x: number;
   y: number;
-}) {
+  color: string;
+}
+
+export function Cursor({ x, y, color }: CursorProps) {
   return (
     <svg
-      style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        transform: `translateX(${x}px) translateY(${y}px)`,
-        zIndex: 1000,
-        pointerEvents: 'none'
-
-      }}
       width="24"
       height="36"
       viewBox="0 0 24 36"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      style={{
+        transform: `translateX(${x}px) translateY(${y}px)`,
+      }}
+      className="left-0 top-0 absolute z-[1000] pointer-events-none transition-transform duration-100 ease-linear motion-reduce:transition-none"
     >
       <path
         d="M5.65376 12.3673H5.46026L5.31717 12.4976L0.500002 16.8829L0.500002 1.19841L11.7841 12.3673H5.65376Z"
