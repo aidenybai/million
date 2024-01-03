18e4db8a6e7c28e292306c6390f1d205d7065821
Aiden Bai <aiden.bai05@gmail.com>
2024-01-02 23:11:36 -0800

    chore: remove unnecessary imports

--- website/components/home/index.tsx
+++ website/components/home/index.tsx
@@ -1,7 +1,5 @@
 import { About } from './about';
-import { Community } from './community';
 import { CTA } from './cta';
-import { FAQ } from './faq';
 import { Hero } from './hero';
 import { Showcase } from './showcase';
 
