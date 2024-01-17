d64eaca59e3fd713427dd9180f00021c9429a981
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-17 16:15:08 +0800

    Create get-descriptive-name.ts

---
+++ packages/compiler/utils/get-descriptive-name.ts
@@ -0,0 +1,39 @@
+import type { NodePath } from "@babel/core";
+
+export function getDescriptiveName(path: NodePath, defaultName: string): string {
+  let current: babel.NodePath | null = path;
+  while (current) {
+    switch (current.node.type) {
+      case 'FunctionDeclaration':
+      case 'FunctionExpression': {
+        if (current.node.id) {
+          return current.node.id.name;
+        }
+        break;
+      }
+      case 'VariableDeclarator': {
+        if (current.node.id.type === 'Identifier') {
+          return current.node.id.name;
+        }
+        break;
+      }
+      case 'ClassPrivateMethod':
+      case 'ClassMethod':
+      case 'ObjectMethod': {
+        switch (current.node.key.type) {
+          case 'Identifier':
+            return current.node.key.name;
+          case 'PrivateName':
+            return current.node.key.id.name;
+          default:
+            break;
+        }
+        break;
+      }
+      default:
+        break;
+    }
+    current = current.parentPath;
+  }
+  return defaultName;
+}
