04aad63999825457735dec91530a42a72fd24810
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-17 16:23:59 +0800

    Create checks.ts

---
+++ packages/compiler/utils/checks.ts
@@ -0,0 +1,50 @@
+import type * as t from '@babel/types';
+
+type TypeFilter<V extends t.Node> = (node: t.Node) => node is V;
+
+export function isPathValid<V extends t.Node>(
+  path: unknown,
+  key: TypeFilter<V>,
+): path is babel.NodePath<V> {
+  return key((path as babel.NodePath).node);
+}
+
+export type NestedExpression =
+  | t.ParenthesizedExpression
+  | t.TypeCastExpression
+  | t.TSAsExpression
+  | t.TSSatisfiesExpression
+  | t.TSNonNullExpression
+  | t.TSInstantiationExpression
+  | t.TSTypeAssertion;
+
+export function isNestedExpression(node: t.Node): node is NestedExpression {
+  switch (node.type) {
+    case 'ParenthesizedExpression':
+    case 'TypeCastExpression':
+    case 'TSAsExpression':
+    case 'TSSatisfiesExpression':
+    case 'TSNonNullExpression':
+    case 'TSTypeAssertion':
+    case 'TSInstantiationExpression':
+      return true;
+    default:
+      return false;
+  }
+}
+
+export type ComponentNode =
+  | t.ArrowFunctionExpression
+  | t.FunctionExpression
+  | t.FunctionDeclaration;
+
+export function isComponent(node: t.Node): node is ComponentNode {
+  switch (node.type) {
+    case 'ArrowFunctionExpression':
+    case 'FunctionExpression':
+    case 'FunctionDeclaration':
+      return true;
+    default:
+      return false;
+  }
+}
