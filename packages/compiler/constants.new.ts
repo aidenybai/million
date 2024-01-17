7f043dd4202e103cd0247df6abca5419d0fd36be
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-17 16:23:55 +0800

    Create constants.new.ts

---
+++ packages/compiler/constants.new.ts
@@ -0,0 +1,95 @@
+import type { ImportDefinition } from "./types";
+
+export const RENDER_SCOPE = 'slot';
+export const SKIP_ANNOTATION = '@million skip';
+export const SVG_ELEMENTS = [
+  'circle',
+  'ellipse',
+  'foreignObject',
+  'image',
+  'line',
+  'path',
+  'polygon',
+  'polyline',
+  'rect',
+  'text',
+  'textPath',
+  'tspan',
+  'svg',
+  'g',
+];
+export const NO_PX_PROPERTIES = [
+  'animationIterationCount',
+  'boxFlex',
+  'boxFlexGroup',
+  'boxOrdinalGroup',
+  'columnCount',
+  'fillOpacity',
+  'flex',
+  'flexGrow',
+  'flexPositive',
+  'flexShrink',
+  'flexNegative',
+  'flexOrder',
+  'fontWeight',
+  'lineClamp',
+  'lineHeight',
+  'opacity',
+  'order',
+  'orphans',
+  'stopOpacity',
+  'strokeDashoffset',
+  'strokeOpacity',
+  'strokeWidth',
+  'tabSize',
+  'widows',
+  'zIndex',
+  'zoom',
+  // SVG-related properties
+  'fillOpacity',
+  'floodOpacity',
+  'stopOpacity',
+  'strokeDasharray',
+  'strokeDashoffset',
+  'strokeMiterlimit',
+  'strokeOpacity',
+  'strokeWidth',
+];
+
+interface Imports {
+  block: {
+    client: ImportDefinition;
+    server: ImportDefinition;
+  };
+  For: {
+    client: ImportDefinition;
+    server: ImportDefinition;
+  };
+}
+
+export const IMPORTS: Imports = {
+  block: {
+    client: {
+      kind: 'named',
+      name: 'block',
+      source: 'million/react',
+    },
+    server: {
+      kind: 'named',
+      name: 'block',
+      source: 'million/react-server',
+    },
+  },
+  For: {
+    client: {
+      kind: 'named',
+      name: 'For',
+      source: 'million/react',
+    },
+    server: {
+      kind: 'named',
+      name: 'For',
+      source: 'million/react-server',
+    },
+  }
+};
