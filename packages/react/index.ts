0c63c41467978d6831e7e83b3202615b7a84eaba
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-20 22:21:59 +0800

    Add internal `areCompiledBlockPropsEqual`

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,7 +2,7 @@ import { block as internalBlock, patch, mount } from '../million';
 
 export { block } from './block';
 export { For } from './for';
-export { renderReactScope, unwrap } from './utils';
+export { renderReactScope, unwrap, areCompiledBlockPropsEqual } from './utils';
 export { REGISTRY } from './constants';
 export const INTERNALS = {
   block: internalBlock,
