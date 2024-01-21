557eaec2800d1432e840692bfc1e2af5be979ea8
tobySolutions <adedejitobiloba7@gmail.com>
2024-01-21 12:31:28 +0100

    chore(i18n): fix 404 for AI page

--- website/middleware.js
+++ website/middleware.js
@@ -4,9 +4,9 @@ export const middleware = (request) => {
   const { nextUrl } = request;
 
   // The middleware must not handle dynamic routes.
-  if (nextUrl.pathname.startsWith('/ai')) {
-    return;
-  }
+  // if (nextUrl.pathname.startsWith('/ai')) {
+  //   return;
+  // }
 
   return locales(request);
 };
