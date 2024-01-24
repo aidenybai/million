aa52addcbae505a0212706fc10e6dec529850a37
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-01-24 20:36:01 +0330

    final version (maybe)

--- website/components/wrapped/index.tsx
+++ website/components/wrapped/index.tsx
@@ -95,50 +95,30 @@ rounded-lg text-center bg-gradient-to-b from-zinc-200 to-white dark:from-zinc-70
                   </>
                 )}{' '}
               </h1>
+              <p className="mt-8 text-xl text-zinc-600 dark:text-zinc-300 leading-8">
+                Ping{' '}
+                <Link href="https://twitter.com/milliondotjs">
+                  <span className="gradient-text inline-block">
+                    @milliondotjs
+                  </span>{' '}
+                </Link>{' '}
+                on{' '}
+                <span className="font-medium dark:text-zinc-100">
+                  X / Twitter
+                </span>{' '}
+                and share your gains! We'd love to quote you.
+              </p>
 
               <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
                 <ShimmerButton
                   className="relative w-full sm:w-max flex items-center justify-center transition-all hover:shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset]"
                   background="radial-gradient(ellipse 80% 70% at 50% 120%, #b28ce2, #892fda)"
-                  onClick={async () => {
-                    (await navigator.clipboard.read()).forEach(async (m) => {
-                      const blob = await m.getType('text/plain');
-                      console.log(blob);
-                      console.log(await blob.text());
-                      console.log(await blob.arrayBuffer());
-                    });
-                    const vid = await fetch(videoUrl);
-                    const blob = await vid.blob();
-
-                    var a = new FileReader();
-                    a.onload = async function (e) {
-                      // @ts-ignore
-                      const str: string = e.target.result;
-
-                      // console.log(e.target.result);
-                      await navigator.clipboard.write([
-                        new ClipboardItem({
-                          ['text/plain']: new Blob([str], {
-                            type: 'text/plain',
-                          }),
-                        }),
-                      ]);
-                    };
-                    a.readAsDataURL(blob);
-                  }}
+                  onClick={() => window.open(videoUrl, '_blank')}
                 >
                   <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight text-white z-10">
-                    Share on X
-                  </span>
-                </ShimmerButton>
-                <button
-                  onClick={() => window.open(videoUrl, '_blank')}
-                  className="pointer-click relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-purple-600/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-zinc-700 dark:before:bg-zinc-800 sm:w-max"
-                >
-                  <span className="relative text-base font-semibold text-purple-600 dark:text-white">
                     Download
                   </span>
-                </button>
+                </ShimmerButton>
               </div>
             </div>
           </div>
