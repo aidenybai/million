da7d854c3035539052b54ea4916ffbcc9a6a4fac
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-01-24 13:30:37 +0330

    custom styles

--- website/components/wrapped/index.tsx
+++ website/components/wrapped/index.tsx
@@ -1,3 +1,4 @@
+// import '../../styles/wrapper.css';
 import Link from 'next/link';
 import { About } from '../../components/home/about';
 import { CTA } from '../../components/home/cta';
@@ -9,20 +10,46 @@ import Tilt from 'react-parallax-tilt';
 import CountUp from 'react-countup';
 import { useRouter } from 'next/router';
 import { useMockProgress } from 'mock-progress-react';
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { BasicEvaluatedExpression } from 'next/dist/compiled/webpack/webpack';
+import Head from 'next/head';
 
 export default function Wrapped(props) {
   const { progress, finish, start } = useMockProgress({ timeInterval: 1000 });
-  const { query } = useRouter();
-  const id = query.id;
+  const id = props.id;
   const videoUrl = `https://telemetry.million.dev/api/v1/wrapped/${id}.mp4`;
-
-  useEffect(() => {
-    start();
-  }, []);
+  useEffect(start, []);
+  // start();
 
   return (
     <main className="space-y-40 mb-40">
+      <Head>
+        <style
+          type="text/css"
+          dangerouslySetInnerHTML={{
+            __html: `
+.nextra-sidebar-container, .nextra-breadcrumb, .nextra-toc {
+  display: none!important;
+}
+#__next > main > div > div:nth-child(5) {
+  max-width: 100%!important;
+}
+
+article {
+  max-width: 100%!important;
+}
+
+article > main {
+  max-width: 100%!important;
+  padding: 0!important;
+}
+#__next > main > div > div:nth-child(5) > article > main > :nth-child(3) {
+  display: none!important;
+}
+      `,
+          }}
+        ></style>
+      </Head>
       <div className="relative pb-10 border-b border-b-[#ffffff1a]">
         <Blur />
         <RetroGrid className="opacity-20 " />
@@ -47,6 +74,7 @@ rounded-lg text-center bg-gradient-to-b from-zinc-200 to-white dark:from-zinc-70
                   autoPlay={true}
                   loop={true}
                   onCanPlay={() => {
+                    console.log('can play');
                     finish();
                   }}
                 >
@@ -71,16 +99,39 @@ rounded-lg text-center bg-gradient-to-b from-zinc-200 to-white dark:from-zinc-70
               </h1>
 
               <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
-                <Link href="/docs" className="w-full sm:w-max">
-                  <ShimmerButton
-                    className="relative w-full sm:w-max flex items-center justify-center transition-all hover:shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset]"
-                    background="radial-gradient(ellipse 80% 70% at 50% 120%, #b28ce2, #892fda)"
-                  >
-                    <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight text-white z-10">
-                      Share on X
-                    </span>
-                  </ShimmerButton>
-                </Link>
+                <ShimmerButton
+                  className="relative w-full sm:w-max flex items-center justify-center transition-all hover:shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset]"
+                  background="radial-gradient(ellipse 80% 70% at 50% 120%, #b28ce2, #892fda)"
+                  onClick={async () => {
+                    (await navigator.clipboard.read()).forEach(async (m) => {
+                      const blob = await m.getType('text/plain');
+                      console.log(blob);
+                      console.log(await blob.text());
+                      console.log(await blob.arrayBuffer());
+                    });
+                    const vid = await fetch(videoUrl);
+                    const blob = await vid.blob();
+
+                    var a = new FileReader();
+                    a.onload = async function (e) {
+                      const str: string = e.target.result;
+
+                      // console.log(e.target.result);
+                      await navigator.clipboard.write([
+                        new ClipboardItem({
+                          ['text/plain']: new Blob([str], {
+                            type: 'text/plain',
+                          }),
+                        }),
+                      ]);
+                    };
+                    a.readAsDataURL(blob);
+                  }}
+                >
+                  <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight text-white z-10">
+                    Share on X
+                  </span>
+                </ShimmerButton>
                 <button
                   onClick={() => window.open(videoUrl, '_blank')}
                   className="pointer-click relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-purple-600/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-zinc-700 dark:before:bg-zinc-800 sm:w-max"
