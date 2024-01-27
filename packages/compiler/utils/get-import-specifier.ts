2c612c91f11f9fcd84da3941beaaa125fddd998d
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-27 18:01:53 +0800

    Update get-import-specifier.ts

--- packages/compiler/utils/get-import-specifier.ts
+++ packages/compiler/utils/get-import-specifier.ts
@@ -1,23 +1,36 @@
-import { addNamed, addDefault } from "@babel/helper-module-imports";
-import type * as t from '@babel/types'
-import type { StateContext, ImportDefinition } from "../types";
+import type * as babel from '@babel/core';
+import * as t from '@babel/types';
+import type { ImportDefinition, StateContext } from '../types';
 
 export function getImportIdentifier(
   state: StateContext,
   path: babel.NodePath,
   registration: ImportDefinition,
 ): t.Identifier {
-  const name = registration.kind === 'named'
-    ? registration.name
-    : 'default';
+  const name = registration.kind === 'named' ? registration.name : 'default';
   const target = `${registration.source}[${name}]`;
   const current = state.imports.get(target);
   if (current) {
     return current;
   }
-  const newID = (registration.kind === 'named')
-    ? addNamed(path, registration.name, registration.source)
-    : addDefault(path, registration.source);
-  state.imports.set(target, newID);
-  return newID;
+  const programParent = path.scope.getProgramParent();
+  const uid = programParent.generateUidIdentifier(
+    registration.kind === 'named' ? registration.name : 'default',
+  );
+  const newPath = (
+    programParent.path as babel.NodePath<t.Program>
+  ).unshiftContainer(
+    'body',
+    t.importDeclaration(
+      [
+        registration.kind === 'named'
+          ? t.importSpecifier(uid, t.identifier(registration.name))
+          : t.importDefaultSpecifier(uid),
+      ],
+      t.stringLiteral(registration.source),
+    ),
+  )[0];
+  programParent.registerDeclaration(newPath);
+  state.imports.set(target, uid);
+  return uid;
 }
