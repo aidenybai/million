981c457de2f5a71c75e4c3e7cfa92f62305e0930
tobySolutions <adedejitobiloba7@gmail.com>
2024-01-29 17:12:42 +0100

    chore: pulling

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -13,11 +13,6 @@ const withNextra = nextra({
 const nextConfig = {
   transpilePackages: ['react-tweet'],
   i18n: {
-<<<<<<< HEAD
-    locales: ['en-US', 'zh-CN', 'de-DE'],
-    defaultLocale: 'en-US',
-  },
-=======
     locales: ['en-US', 'zh-CN', 'es-ES', 'fr-FR'],
     defaultLocale: 'en-US',
   },
@@ -35,7 +30,6 @@ const nextConfig = {
       },
     ];
   },
->>>>>>> 36d1913569efff3399cde30dbda145ffd761115e
 };
 
 // eslint-disable-next-line import/no-default-export  -- Next.js requires default export
