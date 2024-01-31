417fc6705f6413dd4183aa8d1efd084b5016cc3a
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-31 22:10:18 +0800

    Update vite.config.ts

--- packages/kitchen-sink/vite.config.ts
+++ packages/kitchen-sink/vite.config.ts
@@ -1,16 +1,16 @@
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 // @ts-ignore
-import million from '../compiler';
 import Inspect from 'vite-plugin-inspect';
+import million from '../compiler';
 
 import data from '../../package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // @ts-ignore
-  plugins: [million.vite({ mute: 'info' }), react(), Inspect()],
+  plugins: [million.vite({ mute: 'info', auto: true }), react(), Inspect()],
   resolve: {
     alias: {
       'million/react': path.resolve(__dirname, '../react'),
