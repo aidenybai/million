aa36566fa9d3e72996b687b30b4e0c88d1a0407a
Nisarg Patel <awesomenisarg@gmail.com>
2024-02-01 00:12:42 -0800

    feat: share on twitter

--- website/components/wrapped/index.tsx
+++ website/components/wrapped/index.tsx
@@ -101,14 +101,31 @@ export default function Wrapped(props) {
                 and share your gains! We'd love to quote you.
               </p>
 
-              <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+              <div className="mt-8 flex flex-wrap justify-center items-center gap-y-4 gap-x-6">
+              <button
+                  className="relative w-full sm:w-max flex gap-1 items-center justify-center transition-all shadow-[0_0_0_1px_rgba(255,255,255,0.2)_inset] bg-black rounded-[100px] px-8 py-[11.5px] w-[165px] transition-all  hover:scale-105 duration-300"
+                  onClick={() => window.open(`https://twitter.com/intent/tweet?text=I just wrapped my website with Million.js and it's amazing! Check it out at https://million.dev/wrapped/${id}.mp4`, '_blank')}
+                >
+                <span className ="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight text-white z-10">Share on</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="white"
+                  className="feather feather-twitter pt-1"
+                >
+                  <path d="M16.99 0H20.298L13.071 8.26L21.573 19.5H14.916L9.702 12.683L3.736 19.5H0.426L8.156 10.665L0 0H6.826L11.539 6.231L16.99 0ZM15.829 17.52H17.662L5.83 1.876H3.863L15.829 17.52Z"></path>
+                </svg>
+                </button>
+              
                 <ShimmerButton
                   className="relative w-full sm:w-max flex items-center justify-center transition-all hover:shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset]"
                   background="radial-gradient(ellipse 80% 70% at 50% 120%, #b28ce2, #892fda)"
                   onClick={() => window.open(videoUrl, '_blank')}
                 >
                   <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight text-white z-10">
-                    Download
+                    Download Video
                   </span>
                 </ShimmerButton>
               </div>
