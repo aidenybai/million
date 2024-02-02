451c3721f7575dea0dc86b67944cb39b48bd0414
Aiden Bai <aiden.bai05@gmail.com>
2024-02-02 09:59:53 -0800

    hotfix: version process

--- build.config.ts
+++ build.config.ts
@@ -2,9 +2,10 @@ import { defineBuildConfig } from 'unbuild';
 import banner from 'rollup-plugin-banner2';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
+import replace from '@rollup/plugin-replace';
 
 const version = JSON.parse(
-  readFileSync(join(__dirname, 'package.json'), 'utf-8')
+  readFileSync(join(__dirname, 'package.json'), 'utf-8'),
 ).version;
 
 export default defineBuildConfig({
@@ -22,24 +23,18 @@ export default defineBuildConfig({
   rollup: {
     emitCJS: true,
     inlineDependencies: true,
-    replace: {
-      'process.env.VERSION': JSON.stringify(version),
-      preventAssignment: true,
-    }
   },
   hooks: {
     'rollup:options'(_ctx, options) {
       if (Array.isArray(options?.plugins)) {
         options.plugins.push(banner(() => `'use client';\n`) as any);
+        options.plugins.push(
+          replace({
+            'process.env.VERSION': JSON.stringify(version),
+          }),
+        );
       }
     },
   },
-  externals: [
-    'react',
-    'react-dom',
-    'million',
-    'vite',
-    'esbuild',
-    'rollup',
-  ],
+  externals: ['react', 'react-dom', 'million', 'vite', 'esbuild', 'rollup'],
 });
