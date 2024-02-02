edd8492f1ddafcbfb1019c67bdd3857ba53e5d39
Aiden Bai <aiden.bai05@gmail.com>
2024-02-02 06:40:56 -0800

    hotfix: remove outdated compiler tests

--- test/react-compiler.test.ts
+++ test/react-compiler.test.ts
@@ -1,23 +1,23 @@
-import { it, describe } from 'vitest';
-import { babel } from '../packages/compiler';
-import { pluginTester } from 'babel-plugin-tester';
+// import { it, describe } from 'vitest';
+// import { babel } from '../packages/compiler';
+// import { pluginTester } from 'babel-plugin-tester';
 
-(globalThis as any).it = it;
-(globalThis as any).describe = describe;
+// (globalThis as any).it = it;
+// (globalThis as any).describe = describe;
 
-pluginTester({
-  plugin: babel,
-  filepath: __filename,
-  babelOptions: {
-    parserOpts: {
-      plugins: ['jsx', 'typescript'],
-    },
-    generatorOpts: {},
-    babelrc: false,
-    configFile: false,
-  },
-  pluginOptions: {
-    mode: 'react',
-  },
-  tests: {},
-});
+// pluginTester({
+//   plugin: babel,
+//   filepath: __filename,
+//   babelOptions: {
+//     parserOpts: {
+//       plugins: ['jsx', 'typescript'],
+//     },
+//     generatorOpts: {},
+//     babelrc: false,
+//     configFile: false,
+//   },
+//   pluginOptions: {
+//     mode: 'react',
+//   },
+//   tests: {},
+// });
